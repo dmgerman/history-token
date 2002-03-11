2bdefb1169100f0dae46b00e1850a7903651127e
David Mosberger <davidm@hpl.hp.com>
2002-03-11 08:15:10 -0800

    [PATCH] Compile fix for kernel/sched.c
    Fix init_idle() to initialize preempt_count only if CONFIG_PREEMPT is set.

--- kernel/sched.c
+++ kernel/sched.c
@@ -5194,6 +5194,7 @@ c_func
 id|flags
 )paren
 suffix:semicolon
+macro_line|#ifdef CONFIG_PREEMPT
 multiline_comment|/* Set the preempt count _outside_ the spinlocks! */
 id|idle-&gt;thread_info-&gt;preempt_count
 op_assign
@@ -5203,6 +5204,7 @@ op_ge
 l_int|0
 )paren
 suffix:semicolon
+macro_line|#endif
 )brace
 r_extern
 r_void
