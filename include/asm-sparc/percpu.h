86870c18f013a9cd986cdbacf5f930695573a9cb
David S. Miller <davem@nuts.ninka.net>
2002-04-16 02:17:31 -0700

    Add asm-sparc{,64}/percpu.h

---
+++ include/asm-sparc/percpu.h
@@ -0,0 +1,6 @@
+macro_line|#ifndef __ARCH_SPARC_PERCPU__
+DECL|macro|__ARCH_SPARC_PERCPU__
+mdefine_line|#define __ARCH_SPARC_PERCPU__
+macro_line|#include &lt;asm-generic/percpu.h&gt;
+macro_line|#endif /* __ARCH_SPARC_PERCPU__ */
+eof
