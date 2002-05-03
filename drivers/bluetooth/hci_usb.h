8f9e7aac40a32117a3d98172eceef1ac65c1074d
Maksim Krasnyanskiy <maxk@qualcomm.com>
2002-05-03 01:11:54 -0700

    [PATCH] Bluetooth subsystem sync up
    This updates 2.5.x Bluetooth subsystem and removes the EXPERIMENTAL
    status of Bluetooth support.
    
             BlueZ Core:
                     New generic HCI connection manager.
                     Complete role switch and link policy support.
                     Security mode 1 and 3 support.
                     L2CAP service level security support.
                     HCI filter support.
                     HCI frame time-stamps.
                     SCO (voice links) support.
                     Improved HCI device unregistration (device destructors).
                     Support for L2CAP signalling frame fragmentation.
                     Improved L2CAP timeout handling.
                     New HCI ioctls for changing ACL and SCO MTU.
                     Killed HCI_MAX_DEV limit.
                     Security fixes.
    
             HCI USB driver:
                     Performance improvements.
                     Firmware loading support.
                     Stability fixes. URB and disconnect handling rewrite.
    
             HCI UART driver:
                     Support for multiple UART protocols.
    
             HCI PCMCIA driver:
                     Support for Nokia Bluetooth PC Cards.
                     Support for Anycom Bluetooth PC/CF Cards.

--- drivers/bluetooth/hci_usb.h
+++ drivers/bluetooth/hci_usb.h
@@ -1,5 +1,5 @@
 multiline_comment|/* &n;   BlueZ - Bluetooth protocol stack for Linux&n;   Copyright (C) 2000-2001 Qualcomm Incorporated&n;&n;   Written 2000,2001 by Maxim Krasnyansky &lt;maxk@qualcomm.com&gt;&n;&n;   This program is free software; you can redistribute it and/or modify&n;   it under the terms of the GNU General Public License version 2 as&n;   published by the Free Software Foundation;&n;&n;   THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS&n;   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,&n;   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS.&n;   IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) AND AUTHOR(S) BE LIABLE FOR ANY&n;   CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES &n;   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN &n;   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF &n;   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.&n;&n;   ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS, &n;   COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS &n;   SOFTWARE IS DISCLAIMED.&n;*/
-multiline_comment|/*&n; * $Id: hci_usb.h,v 1.3 2001/06/02 01:40:08 maxk Exp $&n; */
+multiline_comment|/*&n; * $Id: hci_usb.h,v 1.2 2002/03/18 19:10:04 maxk Exp $&n; */
 macro_line|#ifdef __KERNEL__
 multiline_comment|/* Class, SubClass, and Protocol codes that describe a Bluetooth device */
 DECL|macro|HCI_DEV_CLASS
@@ -10,26 +10,61 @@ DECL|macro|HCI_DEV_PROTOCOL
 mdefine_line|#define HCI_DEV_PROTOCOL     0x01&t;/* Bluetooth programming protocol */
 DECL|macro|HCI_CTRL_REQ
 mdefine_line|#define HCI_CTRL_REQ&t;     0x20
+DECL|macro|HCI_MAX_IFACE_NUM
+mdefine_line|#define HCI_MAX_IFACE_NUM&t;3 
+DECL|macro|HCI_MAX_BULK_TX
+mdefine_line|#define HCI_MAX_BULK_TX     &t;4
+DECL|macro|HCI_MAX_BULK_RX
+mdefine_line|#define HCI_MAX_BULK_RX     &t;1
 DECL|struct|hci_usb
 r_struct
 id|hci_usb
 (brace
+DECL|member|hdev
+r_struct
+id|hci_dev
+id|hdev
+suffix:semicolon
+DECL|member|state
+r_int
+r_int
+id|state
+suffix:semicolon
 DECL|member|udev
 r_struct
 id|usb_device
 op_star
 id|udev
 suffix:semicolon
-DECL|member|dev_req
+DECL|member|isoc_iface
 r_struct
-id|usb_ctrlrequest
-id|dev_req
-suffix:semicolon
-DECL|member|ctrl_urb
-r_struct
-id|urb
+id|usb_interface
 op_star
-id|ctrl_urb
+id|isoc_iface
+suffix:semicolon
+DECL|member|bulk_out_ep
+id|__u8
+id|bulk_out_ep
+suffix:semicolon
+DECL|member|bulk_in_ep
+id|__u8
+id|bulk_in_ep
+suffix:semicolon
+DECL|member|isoc_out_ep
+id|__u8
+id|isoc_out_ep
+suffix:semicolon
+DECL|member|isoc_in_ep
+id|__u8
+id|isoc_in_ep
+suffix:semicolon
+DECL|member|intr_ep
+id|__u8
+id|intr_ep
+suffix:semicolon
+DECL|member|intr_interval
+id|__u8
+id|intr_interval
 suffix:semicolon
 DECL|member|intr_urb
 r_struct
@@ -37,80 +72,64 @@ id|urb
 op_star
 id|intr_urb
 suffix:semicolon
-DECL|member|read_urb
-r_struct
-id|urb
-op_star
-id|read_urb
-suffix:semicolon
-DECL|member|write_urb
-r_struct
-id|urb
-op_star
-id|write_urb
-suffix:semicolon
-DECL|member|read_buf
-id|__u8
-op_star
-id|read_buf
-suffix:semicolon
-DECL|member|intr_buf
-id|__u8
-op_star
-id|intr_buf
-suffix:semicolon
 DECL|member|intr_skb
 r_struct
 id|sk_buff
 op_star
 id|intr_skb
 suffix:semicolon
-DECL|member|intr_count
-r_int
-id|intr_count
+DECL|member|completion_lock
+id|rwlock_t
+id|completion_lock
 suffix:semicolon
-DECL|member|bulk_out_ep_addr
-id|__u8
-id|bulk_out_ep_addr
-suffix:semicolon
-DECL|member|bulk_in_ep_addr
-id|__u8
-id|bulk_in_ep_addr
-suffix:semicolon
-DECL|member|intr_in_ep_addr
-id|__u8
-id|intr_in_ep_addr
-suffix:semicolon
-DECL|member|intr_in_interval
-id|__u8
-id|intr_in_interval
-suffix:semicolon
-DECL|member|hdev
-r_struct
-id|hci_dev
-id|hdev
-suffix:semicolon
-DECL|member|tx_state
-r_int
-r_int
-id|tx_state
-suffix:semicolon
-DECL|member|tx_ctrl_q
+DECL|member|cmd_q
 r_struct
 id|sk_buff_head
-id|tx_ctrl_q
+id|cmd_q
 suffix:semicolon
-DECL|member|tx_write_q
+singleline_comment|// TX Commands
+DECL|member|acl_q
 r_struct
 id|sk_buff_head
-id|tx_write_q
+id|acl_q
+suffix:semicolon
+singleline_comment|// TX ACLs
+DECL|member|pending_q
+r_struct
+id|sk_buff_head
+id|pending_q
+suffix:semicolon
+singleline_comment|// Pending requests
+DECL|member|completed_q
+r_struct
+id|sk_buff_head
+id|completed_q
+suffix:semicolon
+singleline_comment|// Completed requests
+)brace
+suffix:semicolon
+DECL|struct|hci_usb_scb
+r_struct
+id|hci_usb_scb
+(brace
+DECL|member|urb
+r_struct
+id|urb
+op_star
+id|urb
+suffix:semicolon
+DECL|member|intr_len
+r_int
+id|intr_len
 suffix:semicolon
 )brace
 suffix:semicolon
-multiline_comment|/* Transmit states  */
-DECL|macro|HCI_TX_CTRL
-mdefine_line|#define HCI_TX_CTRL&t;1
-DECL|macro|HCI_TX_WRITE
-mdefine_line|#define HCI_TX_WRITE&t;2
+multiline_comment|/* States  */
+DECL|macro|HCI_USB_TX_PROCESS
+mdefine_line|#define HCI_USB_TX_PROCESS&t;1
+DECL|macro|HCI_USB_TX_WAKEUP
+mdefine_line|#define HCI_USB_TX_WAKEUP&t;2
+DECL|macro|HCI_USB_CTRL_TX
+mdefine_line|#define HCI_USB_CTRL_TX&t;&t;3
 macro_line|#endif /* __KERNEL__ */
 eof
