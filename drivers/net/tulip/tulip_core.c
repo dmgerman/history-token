5501e7571314d33d4f2c6a610ef8e8778c03dbda
Juan Quintela <quintela@mandrakesoft.com>
2002-05-10 21:11:26 -0400

    tulip net driver 2114x phy init fix

--- drivers/net/tulip/tulip_core.c
+++ drivers/net/tulip/tulip_core.c
@@ -3,9 +3,9 @@ multiline_comment|/*&n;&t;Maintained by Jeff Garzik &lt;jgarzik@mandrakesoft.com
 DECL|macro|DRV_NAME
 mdefine_line|#define DRV_NAME&t;&quot;tulip&quot;
 DECL|macro|DRV_VERSION
-mdefine_line|#define DRV_VERSION&t;&quot;1.1.12&quot;
+mdefine_line|#define DRV_VERSION&t;&quot;1.1.13&quot;
 DECL|macro|DRV_RELDATE
-mdefine_line|#define DRV_RELDATE&t;&quot;Mar 07, 2002&quot;
+mdefine_line|#define DRV_RELDATE&t;&quot;May 11, 2002&quot;
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &quot;tulip.h&quot;
