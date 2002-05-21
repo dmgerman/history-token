7ce34e7128bd285bc8605854ee268898aa826c00
Rusty Russell <rusty@rustcorp.com.au>
2002-05-20 21:36:50 -0700

    [PATCH] Tasklet cleanup
    This makes tasklet_vec and tasklet_hi_vec static inside softirq.c, and
    makes them __per_cpu_data.

--- kernel/ksyms.c
+++ kernel/ksyms.c
@@ -3035,20 +3035,6 @@ id|strsep
 )paren
 suffix:semicolon
 multiline_comment|/* software interrupts */
-DECL|variable|tasklet_hi_vec
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|tasklet_hi_vec
-)paren
-suffix:semicolon
-DECL|variable|tasklet_vec
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|tasklet_vec
-)paren
-suffix:semicolon
 DECL|variable|bh_task_vec
 id|EXPORT_SYMBOL
 c_func
