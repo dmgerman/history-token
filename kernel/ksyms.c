2883faedd6808a0ba482db034f39ab195b097243
Christoph Hellwig <hch@infradead.org>
2002-05-22 22:52:56 -0700

    [PATCH] include buffer_head.h in actual users instead of fs.h (8/10)
    Make kernel/ksyms.c include buffer_head.h directly.

--- kernel/ksyms.c
+++ kernel/ksyms.c
@@ -38,6 +38,7 @@ macro_line|#include &lt;linux/completion.h&gt;
 macro_line|#include &lt;linux/seq_file.h&gt;
 macro_line|#include &lt;linux/binfmts.h&gt;
 macro_line|#include &lt;linux/namei.h&gt;
+macro_line|#include &lt;linux/buffer_head.h&gt;
 macro_line|#include &lt;asm/checksum.h&gt;
 macro_line|#if defined(CONFIG_PROC_FS)
 macro_line|#include &lt;linux/proc_fs.h&gt;
