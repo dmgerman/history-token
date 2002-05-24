69978bb797704d1c77e2231db6d68299d81e8541
Linus Torvalds <torvalds@penguin.transmeta.com>
2002-05-23 21:25:44 -0700

    Fix up header file

--- include/linux/buffer_head.h
+++ include/linux/buffer_head.h
@@ -4,6 +4,7 @@ DECL|macro|_LINUX_BUFFER_HEAD_H
 mdefine_line|#define _LINUX_BUFFER_HEAD_H
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/fs.h&gt;
+macro_line|#include &lt;linux/linkage.h&gt;
 macro_line|#include &lt;asm/atomic.h&gt;
 DECL|enum|bh_state_bits
 r_enum
