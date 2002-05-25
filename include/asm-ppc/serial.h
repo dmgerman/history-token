6954ca3b09245f29945847a866c26cb6a590b0f1
Paul Mackerras <paulus@samba.org>
2002-05-25 16:30:21 +1000

    PPC32: Updates for PPC405 processors.  A lot of stuff that was under CONFIG_4xx but which won't apply on the new 440GP has been changed to depend on CONFIG_40x instead.  Also a few other fixes and updates for 405GP-based systems from the linuxppc_2_4_devel tree.

--- include/asm-ppc/serial.h
+++ include/asm-ppc/serial.h
@@ -27,7 +27,7 @@ macro_line|#elif defined(CONFIG_SPRUCE)
 macro_line|#include &lt;platforms/spruce_serial.h&gt;
 macro_line|#elif defined(CONFIG_ZX4500)
 macro_line|#include &lt;platforms/zx4500_serial.h&gt;
-macro_line|#elif defined(CONFIG_4xx)
+macro_line|#elif defined(CONFIG_40x)
 macro_line|#include &lt;asm/ibm4xx.h&gt;
 macro_line|#else
 multiline_comment|/*&n; * XXX Assume for now it has PC-style ISA serial ports.&n; * This is true for PReP and CHRP at least.&n; */
