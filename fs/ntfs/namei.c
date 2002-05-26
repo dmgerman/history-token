9287cef07d5daec5a1bf872ceca6b2df84629588
Anton Altaparmakov <aia21@cantab.net>
2002-05-26 20:02:46 +0100

    NTFS: The beginning of 2.0.8. - Major updates for handling of case sensitivity. - Remove unused source file fs/ntfs/attraops.c. - Remove show_inodes mount option(s), thus dropping support for   displaying of short file names. - Remove deprecated mount option posix. - Restore show_sys_files mount option. - Add new mount option case_sensitive, to determine if the driver treats   file names as case sensitive or not. If case sensitive, create file names   in the POSIX namespace. Otherwise create file names in the WIN32   namespace. By default, or when case_sensitive is set to FALSE, files   remain accessible via their short file name, if it exists. - Add additional argument to ntfs_lookup_inode_by_name() in which we   return information about the matching file name if the case is not   matching or the match is a short file name. See comments above the   function definition for details.

--- fs/ntfs/namei.c
+++ fs/ntfs/namei.c
@@ -1,6 +1,7 @@
 multiline_comment|/*&n; * namei.c - NTFS kernel directory inode operations. Part of the Linux-NTFS&n; * &t;     project.&n; *&n; * Copyright (c) 2001,2002 Anton Altaparmakov.&n; *&n; * This program/include file is free software; you can redistribute it and/or&n; * modify it under the terms of the GNU General Public License as published&n; * by the Free Software Foundation; either version 2 of the License, or&n; * (at your option) any later version.&n; *&n; * This program/include file is distributed in the hope that it will be &n; * useful, but WITHOUT ANY WARRANTY; without even the implied warranty &n; * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n; * GNU General Public License for more details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program (in the main directory of the Linux-NTFS &n; * distribution in the file COPYING); if not, write to the Free Software&n; * Foundation,Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA&n; */
 macro_line|#include &quot;ntfs.h&quot;
-multiline_comment|/**&n; * ntfs_lookup - find the inode represented by a dentry in a directory inode&n; * @dir_ino:&t;directory inode in which to look for the inode&n; * @dent:&t;dentry representing the inode to look for&n; *&n; * In short, ntfs_lookup() looks for the inode represented by the dentry @dent&n; * in the directory inode @dir_ino and if found attaches the inode to the&n; * dentry @dent.&n; *&n; * In more detail, the dentry @dent specifies which inode to look for by&n; * supplying the name of the inode in @dent-&gt;d_name.name. ntfs_lookup()&n; * converts the name to Unicode and walks the contents of the directory inode&n; * @dir_ino looking for the converted Unicode name. If the name is found in the&n; * directory, the corresponding inode is loaded by calling iget() on its inode&n; * number and the inode is associated with the dentry @dent via a call to&n; * d_add().&n; *&n; * If the name is not found in the directory, a NULL inode is inserted into the&n; * dentry @dent. The dentry is then termed a negative dentry.&n; *&n; * Only if an actual error occurs, do we return an error via ERR_PTR().&n; */
+macro_line|#include &quot;dir.h&quot;
+multiline_comment|/**&n; * ntfs_lookup - find the inode represented by a dentry in a directory inode&n; * @dir_ino:&t;directory inode in which to look for the inode&n; * @dent:&t;dentry representing the inode to look for&n; *&n; * In short, ntfs_lookup() looks for the inode represented by the dentry @dent&n; * in the directory inode @dir_ino and if found attaches the inode to the&n; * dentry @dent.&n; *&n; * In more detail, the dentry @dent specifies which inode to look for by&n; * supplying the name of the inode in @dent-&gt;d_name.name. ntfs_lookup()&n; * converts the name to Unicode and walks the contents of the directory inode&n; * @dir_ino looking for the converted Unicode name. If the name is found in the&n; * directory, the corresponding inode is loaded by calling iget() on its inode&n; * number and the inode is associated with the dentry @dent via a call to&n; * d_add().&n; *&n; * If the name is not found in the directory, a NULL inode is inserted into the&n; * dentry @dent. The dentry is then termed a negative dentry.&n; *&n; * Only if an actual error occurs, do we return an error via ERR_PTR().&n; *&n; * TODO: Implement the below! (AIA)&n; *&n; * In order to handle the case insensitivity issues of NTFS with regards to the&n; * dcache and the dcache requiring only one dentry per directory, we deal with&n; * dentry aliases that only differ in case in -&gt;ntfs_lookup() while maintining&n; * a case sensitive dcache. This means that we get the full benefit of dcache&n; * speed when the file/directory is looked up with the same case as returned by&n; * -&gt;ntfs_readdir() but that a lookup for any other case (or for the short file&n; * name) will not find anything in dcache and will enter -&gt;ntfs_lookup()&n; * instead, where we search the directory for a fully matching file name&n; * (including case) and if that is not found, we search for a file name that&n; * matches with different case and if that has non-POSIX semantics we return&n; * that. We actually do only one search (case sensitive) and keep tabs on&n; * whether we have found a case insensitive match in the process.&n; *&n; * To simplify matters for us, we do not treat the short vs long filenames as&n; * two hard links but instead if the lookup matches a short filename, we&n; * return the dentry for the corresponding long filename instead.&n; *&n; * There are three cases we need to distinguish here:&n; *&n; * 1) @dent perfectly matches (i.e. including case) a directory entry with a&n; *    file name in the WIN32 or POSIX namespaces. In this case&n; *    ntfs_lookup_inode_by_name() will return with name set to NULL and we&n; *    just d_add() @dent.&n; * 2) @dent matches (not including case) a directory entry with a file name in&n; *    the WIN32 or POSIX namespaces. In this case ntfs_lookup_inode_by_name()&n; *    will return with name set to point to a kmalloc()ed ntfs_name structure&n; *    containing the properly cased little endian Unicode name. We convert the&n; *    name to the current NLS code page, search if a dentry with this name&n; *    already exists and if so return that instead of @dent. The VFS will then&n; *    destroy the old @dent and use the one we returned. If a dentry is not&n; *    found, we allocate a new one, d_add() it, and return it as above.&n; * 3) @dent matches either perfectly or not (i.e. we don&squot;t care about case) a&n; *    directory entry with a file name in the DOS namespace. In this case&n; *    ntfs_lookup_inode_by_name() will return with name set to point to a&n; *    kmalloc()ed ntfs_name structure containing the mft reference (cpu endian)&n; *    of the inode. We use the mft reference to read the inode and to find the&n; *    file name in the WIN32 namespace corresponding to the matched short file&n; *    name. We then convert the name to the current NLS code page, and proceed&n; *    searching for a dentry with this name, etc, as in case 2), above.&n; */
 DECL|function|ntfs_lookup
 r_static
 r_struct
@@ -48,6 +49,12 @@ id|uname
 suffix:semicolon
 r_int
 id|uname_len
+suffix:semicolon
+id|ntfs_name
+op_star
+id|name
+op_assign
+l_int|NULL
 suffix:semicolon
 id|ntfs_debug
 c_func
@@ -113,6 +120,9 @@ comma
 id|uname
 comma
 id|uname_len
+comma
+op_amp
+id|name
 )paren
 suffix:semicolon
 id|kmem_cache_free
@@ -121,6 +131,18 @@ c_func
 id|ntfs_name_cache
 comma
 id|uname
+)paren
+suffix:semicolon
+singleline_comment|// TODO: Handle name. (AIA)
+r_if
+c_cond
+(paren
+id|name
+)paren
+id|kfree
+c_func
+(paren
+id|name
 )paren
 suffix:semicolon
 r_if
@@ -350,6 +372,7 @@ id|mref
 )paren
 suffix:semicolon
 )brace
+multiline_comment|/*&n; * Inode operations for directories.&n; */
 DECL|variable|ntfs_dir_inode_ops
 r_struct
 id|inode_operations
@@ -360,7 +383,7 @@ id|lookup
 suffix:colon
 id|ntfs_lookup
 comma
-multiline_comment|/* lookup directory. */
+multiline_comment|/* VFS: Lookup directory. */
 )brace
 suffix:semicolon
 eof
