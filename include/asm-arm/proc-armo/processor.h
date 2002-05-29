54a0b07b17ad1d250b566e34ef757f5aba4bc985
Russell King <rmk@flint.arm.linux.org.uk>
2002-05-29 18:06:54 +0100

    [ARM] Context switch improvements  - We don't need to save the CPSR.  - Rearrange thread_info members so we can pull the fields out of    thread_info more efficiently.  - Allocate a couple of extra words for CPU specific context saving    (eg, for Xscale ACC registers)  - Always leave 8 bytes free at the top of the kernel stack.  This    prevents the stack becoming completely empty when do_exit() is    called from an exiting nfsd() thread, and causing the wrong    pointer to be returned from current_thread_info()

--- include/asm-arm/proc-armo/processor.h
+++ include/asm-arm/proc-armo/processor.h
@@ -3,92 +3,8 @@ macro_line|#ifndef __ASM_PROC_PROCESSOR_H
 DECL|macro|__ASM_PROC_PROCESSOR_H
 mdefine_line|#define __ASM_PROC_PROCESSOR_H
 macro_line|#include &lt;linux/string.h&gt;
-macro_line|#include &lt;asm/proc/ptrace.h&gt;
 DECL|macro|KERNEL_STACK_SIZE
 mdefine_line|#define KERNEL_STACK_SIZE 4096
-DECL|struct|cpu_context_save
-r_struct
-id|cpu_context_save
-(brace
-DECL|member|r4
-r_int
-r_int
-id|r4
-suffix:semicolon
-DECL|member|r5
-r_int
-r_int
-id|r5
-suffix:semicolon
-DECL|member|r6
-r_int
-r_int
-id|r6
-suffix:semicolon
-DECL|member|r7
-r_int
-r_int
-id|r7
-suffix:semicolon
-DECL|member|r8
-r_int
-r_int
-id|r8
-suffix:semicolon
-DECL|member|r9
-r_int
-r_int
-id|r9
-suffix:semicolon
-DECL|member|sl
-r_int
-r_int
-id|sl
-suffix:semicolon
-DECL|member|fp
-r_int
-r_int
-id|fp
-suffix:semicolon
-DECL|member|pc
-r_int
-r_int
-id|pc
-suffix:semicolon
-)brace
-suffix:semicolon
-DECL|function|init_pc_psr
-r_static
-r_inline
-r_void
-id|init_pc_psr
-c_func
-(paren
-r_struct
-id|cpu_context_save
-op_star
-id|s
-comma
-r_void
-op_star
-id|fn
-)paren
-(brace
-id|s-&gt;pc
-op_assign
-(paren
-(paren
-r_int
-r_int
-)paren
-id|fn
-)paren
-op_or
-id|PSR_I_BIT
-op_or
-id|SVC26_MODE
-suffix:semicolon
-)brace
 r_typedef
 r_struct
 (brace
@@ -275,8 +191,8 @@ mdefine_line|#define EXTRA_THREAD_STRUCT_INIT&t;&t;&bslash;&n;&t;uaccess:&t;&amp
 DECL|macro|start_thread
 mdefine_line|#define start_thread(regs,pc,sp)&t;&t;&t;&t;&t;&bslash;&n;({&t;&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;unsigned long *stack = (unsigned long *)sp;&t;&t;&t;&bslash;&n;&t;set_fs(USER_DS);&t;&t;&t;&t;&t;&t;&bslash;&n;&t;memzero(regs-&gt;uregs, sizeof (regs-&gt;uregs));&t;&t;&t;&bslash;&n;&t;regs-&gt;ARM_pc = pc;&t;&t;/* pc */&t;&t;&t;&bslash;&n;&t;regs-&gt;ARM_sp = sp;&t;&t;/* sp */&t;&t;&t;&bslash;&n;&t;regs-&gt;ARM_r2 = stack[2];&t;/* r2 (envp) */&t;&t;&t;&bslash;&n;&t;regs-&gt;ARM_r1 = stack[1];&t;/* r1 (argv) */&t;&t;&t;&bslash;&n;&t;regs-&gt;ARM_r0 = stack[0];&t;/* r0 (argc) */&t;&t;&t;&bslash;&n;})
 DECL|macro|KSTK_EIP
-mdefine_line|#define KSTK_EIP(tsk)&t;(((unsigned long *)(4096+(unsigned long)(tsk)))[1022])
+mdefine_line|#define KSTK_EIP(tsk)&t;(((unsigned long *)(4096+(unsigned long)(tsk)))[1020])
 DECL|macro|KSTK_ESP
-mdefine_line|#define KSTK_ESP(tsk)&t;(((unsigned long *)(4096+(unsigned long)(tsk)))[1020])
+mdefine_line|#define KSTK_ESP(tsk)&t;(((unsigned long *)(4096+(unsigned long)(tsk)))[1018])
 macro_line|#endif
 eof
