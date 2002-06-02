e081dbdeab692e703a5592249f81d8dd9e086d93
Andrew Morton <akpm@zip.com.au>
2002-06-02 03:23:42 -0700

    [PATCH] swapcache bugfixes
    Fixes a few lock ranking bugs (and deadlocks) related to
    swap_list_lock(), swap_device_lock(), mapping->page_lock and
    mapping->private_lock.
    
    - Cannot call block_flushpage->try_to_free_buffers() inside
      mapping->page_lock.  Because __set_page_dirty_buffers() takes
      ->page_lock inside ->private-lock.
    
    - Cannot call swap_free->swap_list_lock/swap_device_lock inside
      mapping->page_lock because exclusive_swap_page() takes ->page_lock
      inside swap_info_get().
    
    The patch also removes all the block_flushpage() calls from the swap
    code in favour of a direct call to try_to_free_buffers().
    
    The theory is that the page is locked, there is no I/O underway, nobody
    else has access to the buffers so they MUST be freeable.  A bunch of
    BUG() checks have been added, and unless someone manages to trigger
    one, the "block_flushpage() inside spinlock" problem is fixed.

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -10,7 +10,7 @@ macro_line|#include &lt;linux/pagemap.h&gt;
 macro_line|#include &lt;linux/namei.h&gt;
 macro_line|#include &lt;linux/shm.h&gt;
 macro_line|#include &lt;linux/blkdev.h&gt;
-macro_line|#include &lt;linux/buffer_head.h&gt;&t;&t;/* for block_flushpage() */
+macro_line|#include &lt;linux/buffer_head.h&gt;&t;&t;/* for try_to_free_buffers() */
 macro_line|#include &lt;asm/pgtable.h&gt;
 macro_line|#include &lt;linux/swapops.h&gt;
 DECL|variable|swaplock
@@ -1296,12 +1296,35 @@ c_cond
 id|retval
 )paren
 (brace
-id|block_flushpage
+id|BUG_ON
+c_func
+(paren
+id|PageWriteback
 c_func
 (paren
 id|page
-comma
-l_int|0
+)paren
+)paren
+suffix:semicolon
+r_if
+c_cond
+(paren
+id|page_has_buffers
+c_func
+(paren
+id|page
+)paren
+op_logical_and
+op_logical_neg
+id|try_to_free_buffers
+c_func
+(paren
+id|page
+)paren
+)paren
+id|BUG
+c_func
+(paren
 )paren
 suffix:semicolon
 id|swap_free
