72c751a6ffe0410427568fd1b8567234c60b6379
Greg Kroah-Hartman <greg@kroah.com>
2002-06-04 22:17:20 -0700

    USB: emi26.c small fix to enable the driver to build properly

--- drivers/usb/misc/emi26.c
+++ drivers/usb/misc/emi26.c
@@ -3,6 +3,7 @@ macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/errno.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
+macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/usb.h&gt;
 DECL|macro|MAX_INTEL_HEX_RECORD_LENGTH
 mdefine_line|#define MAX_INTEL_HEX_RECORD_LENGTH 16
