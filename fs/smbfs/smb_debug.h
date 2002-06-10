085b8d31651dfa8a68408227da1ffd64b651df2c
Martin Dalecki <dalecki@evision-ventures.com>
2002-06-09 19:23:26 -0700

    [PATCH] kill warnings 6/19
    - Fix compilation warnings in cfbimgblt.c.
    
    - Fix improper __FUNCTION__ usage in smb_debug.h.
    
    It's all about giving the compiler a chance to coalesce
    equal string constants - a good reason for the
    C language standard to depricate the old semantics.

--- fs/smbfs/smb_debug.h
+++ fs/smbfs/smb_debug.h
@@ -2,21 +2,21 @@ multiline_comment|/*&n; * Defines some debug macros for smbfs.&n; */
 multiline_comment|/* This makes a dentry parent/child name pair. Useful for debugging printk&squot;s */
 DECL|macro|DENTRY_PATH
 mdefine_line|#define DENTRY_PATH(dentry) &bslash;&n;&t;(dentry)-&gt;d_parent-&gt;d_name.name,(dentry)-&gt;d_name.name
-multiline_comment|/*&n; * safety checks that should never happen ??? &n; * these are normally enabled.&n; */
+multiline_comment|/*&n; * safety checks that should never happen ???&n; * these are normally enabled.&n; */
 macro_line|#ifdef SMBFS_PARANOIA
 DECL|macro|PARANOIA
-mdefine_line|#define PARANOIA(x...) printk(KERN_NOTICE __FUNCTION__ &quot;: &quot; x)
+macro_line|# define PARANOIA(f, a...) printk(KERN_NOTICE &quot;%s: &quot; f, __FUNCTION__, ## a)
 macro_line|#else
 DECL|macro|PARANOIA
-mdefine_line|#define PARANOIA(x...) do { ; } while(0)
+macro_line|# define PARANOIA(f, a...) do { ; } while(0)
 macro_line|#endif
 multiline_comment|/* lots of debug messages */
 macro_line|#ifdef SMBFS_DEBUG_VERBOSE
 DECL|macro|VERBOSE
-mdefine_line|#define VERBOSE(x...) printk(KERN_DEBUG __FUNCTION__ &quot;: &quot; x)
+macro_line|# define VERBOSE(f, a...) printk(KERN_DEBUG &quot;%s: &quot; f, __FUNCTION__, ## a)
 macro_line|#else
 DECL|macro|VERBOSE
-mdefine_line|#define VERBOSE(x...) do { ; } while(0)
+macro_line|# define VERBOSE(f, a...) do { ; } while(0)
 macro_line|#endif
 multiline_comment|/*&n; * &quot;normal&quot; debug messages, but not with a normal DEBUG define ... way&n; * too common name.&n; */
 macro_line|#ifdef SMBFS_DEBUG
