d49159c38fb6bda2975028a91981ce2f0b1ec559
Kai Germaschewski <kai@tp1.ruhr-uni-bochum.de>
2002-06-15 09:21:36 -0500

    Make net_dev_init() an __initcall
    If you were looking to find where net_dev_init() is called, you wouldn't
    have guessed it's in drivers/block/genhd.c, would you?
    
    Nothing should break if the __initcall net_dev_init is called too late
    now, since register_netdevice() will call it for us in that case.

--- drivers/block/genhd.c
+++ drivers/block/genhd.c
@@ -645,14 +645,6 @@ suffix:semicolon
 macro_line|#endif
 r_extern
 r_int
-id|net_dev_init
-c_func
-(paren
-r_void
-)paren
-suffix:semicolon
-r_extern
-r_int
 id|soc_probe
 c_func
 (paren
@@ -733,13 +725,6 @@ suffix:semicolon
 macro_line|#endif
 macro_line|#ifdef CONFIG_BLK_CPQ_DA
 id|cpqarray_init
-c_func
-(paren
-)paren
-suffix:semicolon
-macro_line|#endif
-macro_line|#ifdef CONFIG_NET
-id|net_dev_init
 c_func
 (paren
 )paren
