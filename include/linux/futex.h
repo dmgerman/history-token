23c71c8ffa2be1d066498ac566ab1cb49e15c839
Rusty Russell <rusty@rustcorp.com.au>
2002-06-19 22:42:08 -0700

    [PATCH] Async Futex
    This patch adds a FUTEX_FD call, for opening a file descriptor
    attached to a futex, which can be used with poll, select or SIGIO.
    Rewritten so that closing fd unpins page, as per Linus's suggestion,
    and fixed not to call poll_wait with a spinlock as per Martin Wirth's
    sharp eyes.

--- include/linux/futex.h
+++ include/linux/futex.h
@@ -6,5 +6,7 @@ DECL|macro|FUTEX_WAIT
 mdefine_line|#define FUTEX_WAIT (0)
 DECL|macro|FUTEX_WAKE
 mdefine_line|#define FUTEX_WAKE (1)
+DECL|macro|FUTEX_FD
+mdefine_line|#define FUTEX_FD (2)
 macro_line|#endif
 eof
