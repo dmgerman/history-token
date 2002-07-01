ad286e4b3821f19455e4abdd1d071fa9cf2ab3dc
Linus Torvalds <torvalds@home.transmeta.com>
2002-07-01 00:41:38 -0700

    Make in-kernel HZ be 1000 on x86, retaining user-level 100 HZ clock_t.
    Stop using "struct tms" internally - always use timer ticks (or one of
    the sane timeval/timespec types) instead.
    
    Explicitly convert to clock_t when copying to user space for the old
    broken interfaces that still use "clock_t".
    
    Clean up and unify jiffies<->timeval conversion.

--- include/asm-i386/param.h
+++ include/asm-i386/param.h
@@ -1,6 +1,14 @@
 macro_line|#ifndef _ASMi386_PARAM_H
 DECL|macro|_ASMi386_PARAM_H
 mdefine_line|#define _ASMi386_PARAM_H
+macro_line|#ifdef __KERNEL__
+DECL|macro|HZ
+macro_line|# define HZ&t;&t;1000&t;&t;/* Internal kernel timer frequency */
+DECL|macro|USER_HZ
+macro_line|# define USER_HZ&t;100&t;&t;/* .. some user interfaces are in &quot;ticks&quot; */
+DECL|macro|CLOCKS_PER_SEC
+macro_line|# define CLOCKS_PER_SEC&t;(USER_HZ)&t;/* like times() */
+macro_line|#endif
 macro_line|#ifndef HZ
 DECL|macro|HZ
 mdefine_line|#define HZ 100
@@ -17,9 +25,5 @@ mdefine_line|#define NOGROUP&t;&t;(-1)
 macro_line|#endif
 DECL|macro|MAXHOSTNAMELEN
 mdefine_line|#define MAXHOSTNAMELEN&t;64&t;/* max length of hostname */
-macro_line|#ifdef __KERNEL__
-DECL|macro|CLOCKS_PER_SEC
-macro_line|# define CLOCKS_PER_SEC&t;100&t;/* frequency at which times() counts */
-macro_line|#endif
 macro_line|#endif
 eof
