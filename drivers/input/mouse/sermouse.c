fa9386a02b7d63b0a83900fc883c8cfd0b400791
Vojtech Pavlik <vojtech@twilight.ucw.cz>
2002-07-03 20:32:10 +0200

    Use time_after() where applicable.

--- drivers/input/mouse/sermouse.c
+++ drivers/input/mouse/sermouse.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * $Id: sermouse.c,v 1.15 2001/10/09 22:34:17 jsimmons Exp $&n; *&n; *  Copyright (c) 1999-2001 Vojtech Pavlik&n; */
+multiline_comment|/*&n; * $Id: sermouse.c,v 1.17 2002/03/13 10:03:43 vojtech Exp $&n; *&n; *  Copyright (c) 1999-2001 Vojtech Pavlik&n; */
 multiline_comment|/*&n; *  Serial mouse driver for Linux&n; */
 multiline_comment|/*&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or &n; * (at your option) any later version.&n; * &n; * This program is distributed in the hope that it will be useful,&n; * but WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n; * GNU General Public License for more details.&n; * &n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA&n; * &n; * Should you need to contact me, the author, you can do so either by&n; * e-mail - mail your message to &lt;vojtech@ucw.cz&gt;, or by paper mail:&n; * Vojtech Pavlik, Simunkova 1594, Prague 8, 182 00 Czech Republic&n; */
 macro_line|#include &lt;linux/delay.h&gt;
@@ -914,11 +914,17 @@ suffix:semicolon
 r_if
 c_cond
 (paren
+id|time_after
+c_func
+(paren
 id|jiffies
-op_minus
+comma
 id|sermouse-&gt;last
-OG
-l_int|2
+op_plus
+id|HZ
+op_div
+l_int|20
+)paren
 )paren
 id|sermouse-&gt;count
 op_assign
