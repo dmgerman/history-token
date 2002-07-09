9c12820a96a69d2911e0610af7b57c4b1f1aa45b
Russell King <rmk@flint.arm.linux.org.uk>
2002-07-09 22:06:00 +0100

    [ARM] Fix up include/asm-arm/param.h (and sub-architecture param.h) for 2.5.25 HZ/USER_HZ changes.

--- include/asm-arm/arch-tbox/param.h
+++ include/asm-arm/arch-tbox/param.h
@@ -1,3 +1,4 @@
-DECL|macro|HZ
-mdefine_line|#define HZ 1000
+multiline_comment|/*&n; *  linux/include/asm-arm/arch-tbox/param.h&n; */
+DECL|macro|__KERNEL_HZ
+mdefine_line|#define __KERNEL_HZ 1000
 eof
