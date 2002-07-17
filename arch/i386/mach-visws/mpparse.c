7faad6c1157237db0ea8ee0c0ab636b1df0d7c81
James Bottomley <jejb@mulgrave.(none)>
2002-07-16 23:59:25 -0500

    Add comment change back to visws

--- arch/i386/mach-visws/mpparse.c
+++ arch/i386/mach-visws/mpparse.c
@@ -130,7 +130,7 @@ r_int
 r_int
 id|phys_cpu_present_map
 suffix:semicolon
-multiline_comment|/*&n; * The Visual Workstation is Intel MP compliant in the hardware&n; * sense, but it doesnt have a BIOS(-configuration table).&n; * No problem for Linux.&n; */
+multiline_comment|/*&n; * The Visual Workstation is Intel MP compliant in the hardware&n; * sense, but it doesn&squot;t have a BIOS(-configuration table).&n; * No problem for Linux.&n; */
 DECL|function|find_smp_config
 r_void
 id|__init
