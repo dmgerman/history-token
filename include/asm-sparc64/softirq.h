eb4fd2c0350794d94db0754c1e8c4b7013f8aa1d
David S. Miller <davem@nuts.ninka.net>
2002-07-22 07:56:30 -0700

    include/asm-sparc64/softirq.h: Fix typo in local_bh_enable.

--- include/asm-sparc64/softirq.h
+++ include/asm-sparc64/softirq.h
@@ -10,7 +10,7 @@ mdefine_line|#define local_bh_disable()&t;do { preempt_count() += IRQ_OFFSET; ba
 DECL|macro|__local_bh_enable
 mdefine_line|#define __local_bh_enable()&t;do { barrier(); preempt_count() -= IRQ_OFFSET; } while (0)
 DECL|macro|local_bh_enable
-mdefine_line|#define local_bh_enable()&t;&t;&t;&t;&bslash;&n;do { if (unlikely((preempt_count() == IRQ_OFFSET) &amp;&amp;&t;&bslash;&n;&t; softirq_pending(smp_processor_id())) {   &t;&bslash;&n;&t;&t;__local_bh_enable();&t;&t;&t;&bslash;&n;&t;&t;do_softirq();&t;&t;&t;  &t;&bslash;&n;&t;&t;preempt_check_resched();&t;&t;&bslash;&n;     } else {&t;&t;&t;&t;&t;&t;&bslash;&n;&t;&t;__local_bh_enable();&t;&t;&t;&bslash;&n;&t;&t;preempt_check_resched();&t;&t;&bslash;&n;     }&t;&t;&t;&t;&t;&t;&t;&bslash;&n;} while (0)
+mdefine_line|#define local_bh_enable()&t;&t;&t;&t;&bslash;&n;do { if (unlikely((preempt_count() == IRQ_OFFSET) &amp;&amp;&t;&bslash;&n;&t; softirq_pending(smp_processor_id()))) {   &t;&bslash;&n;&t;&t;__local_bh_enable();&t;&t;&t;&bslash;&n;&t;&t;do_softirq();&t;&t;&t;  &t;&bslash;&n;&t;&t;preempt_check_resched();&t;&t;&bslash;&n;     } else {&t;&t;&t;&t;&t;&t;&bslash;&n;&t;&t;__local_bh_enable();&t;&t;&t;&bslash;&n;&t;&t;preempt_check_resched();&t;&t;&bslash;&n;     }&t;&t;&t;&t;&t;&t;&t;&bslash;&n;} while (0)
 DECL|macro|in_softirq
 mdefine_line|#define in_softirq() in_interrupt()
 macro_line|#endif /* !(__SPARC64_SOFTIRQ_H) */
