0f21919758dc4daf5bc00675d5959239913979f8
Patrick Mochel <mochel@osdl.org>
2002-08-01 21:57:14 -0700

    driverfs: Move driverfs calls from drivers/base/*.c to drivers/base/fs/*.c
    This cleans up the drivers/base/ files, so they deal mainly with registration.
    
    It also provides a good place to put the glue needed for bus and driver files in driverfs.

---
+++ drivers/base/fs/fs.h
@@ -0,0 +1,17 @@
+r_extern
+r_int
+id|device_create_dir
+c_func
+(paren
+r_struct
+id|driver_dir_entry
+op_star
+id|dir
+comma
+r_struct
+id|driver_dir_entry
+op_star
+id|parent
+)paren
+suffix:semicolon
+eof
