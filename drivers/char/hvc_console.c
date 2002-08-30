ac349643f17948ce7fcffa7ff2cc01c80416fb79
Anton Blanchard <anton@samba.org>
2002-08-30 11:58:38 +1000

    hvc_console: stop HVC console while xmon is running

--- drivers/char/hvc_console.c
+++ drivers/char/hvc_console.c
@@ -949,6 +949,11 @@ id|flags
 )paren
 suffix:semicolon
 )brace
+r_extern
+r_int
+r_int
+id|cpus_in_xmon
+suffix:semicolon
 DECL|function|khvcd
 r_int
 id|khvcd
@@ -994,6 +999,13 @@ suffix:semicolon
 suffix:semicolon
 )paren
 (brace
+r_if
+c_cond
+(paren
+op_logical_neg
+id|cpus_in_xmon
+)paren
+(brace
 r_for
 c_loop
 (paren
@@ -1014,6 +1026,7 @@ c_func
 id|i
 )paren
 suffix:semicolon
+)brace
 id|set_current_state
 c_func
 (paren
