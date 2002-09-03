d5edc4b5bf0e1647dcf44c691e3ac62a91c10821
Andrew Morton <akpm@zip.com.au>
2002-09-03 05:33:56 -0700

    [PATCH] reorganise setup_arch() for ia32 discontigmem
    This restructures setup_arch() for i386 to make it easier to include the
    i386 numa changes (for CONFIG_DISCONTIGMEM) I've been working on.  It
    also makes setup_arch() easier to read.  A version of this patch is the
    in 2.4 aa tree.
    
    This does not depend on the other patches I'm submitting today, but my
    discontigmem patch does depend on this one.
    
    I've tested this patch on the following configurations: UP, SMP, SMP
    PAE, multiquad, multiquad PAE.

--- include/asm-i386/setup.h
+++ include/asm-i386/setup.h
@@ -2,5 +2,16 @@ multiline_comment|/*&n; *&t;Just a place holder. We don&squot;t want to have to 
 macro_line|#ifndef _i386_SETUP_H
 DECL|macro|_i386_SETUP_H
 mdefine_line|#define _i386_SETUP_H
+DECL|macro|PFN_UP
+mdefine_line|#define PFN_UP(x)&t;(((x) + PAGE_SIZE-1) &gt;&gt; PAGE_SHIFT)
+DECL|macro|PFN_DOWN
+mdefine_line|#define PFN_DOWN(x)&t;((x) &gt;&gt; PAGE_SHIFT)
+DECL|macro|PFN_PHYS
+mdefine_line|#define PFN_PHYS(x)&t;((x) &lt;&lt; PAGE_SHIFT)
+multiline_comment|/*&n; * Reserved space for vmalloc and iomap - defined in asm/page.h&n; */
+DECL|macro|MAXMEM_PFN
+mdefine_line|#define MAXMEM_PFN&t;PFN_DOWN(MAXMEM)
+DECL|macro|MAX_NONPAE_PFN
+mdefine_line|#define MAX_NONPAE_PFN&t;(1 &lt;&lt; 20)
 macro_line|#endif /* _i386_SETUP_H */
 eof
