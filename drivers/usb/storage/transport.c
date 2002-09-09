c96816653fdbbfb783681aa8b1a15550224d876e
Alan Stern <stern@rowland.harvard.edu>
2002-09-08 23:58:32 -0700

    [PATCH] USB storage: abort bug fix
    Also, have you sent in the one-line fix I found for the abort bug?
    Andries found that it cured his BUG_ON problem.  In case you didn't save a
    copy of it, I've included it below.

--- drivers/usb/storage/transport.c
+++ drivers/usb/storage/transport.c
@@ -2407,6 +2407,14 @@ id|us-&gt;irq_urb
 )paren
 suffix:semicolon
 )brace
+multiline_comment|/* Wait for the aborted command to finish */
+id|wait_for_completion
+c_func
+(paren
+op_amp
+id|us-&gt;notify
+)paren
+suffix:semicolon
 multiline_comment|/* Reacquire the lock */
 id|scsi_lock
 c_func
