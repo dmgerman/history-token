de48a64a5d4fc33f945a8f27952bf86ccd6b012f
David S. Miller <davem@nuts.ninka.net>
2002-09-22 06:14:25 -0700

    [BRIDGE]: Missing headers from ebtables merge.

---
+++ include/linux/netfilter_bridge/ebt_nat.h
@@ -0,0 +1,28 @@
+macro_line|#ifndef __LINUX_BRIDGE_EBT_NAT_H
+DECL|macro|__LINUX_BRIDGE_EBT_NAT_H
+mdefine_line|#define __LINUX_BRIDGE_EBT_NAT_H
+DECL|struct|ebt_nat_info
+r_struct
+id|ebt_nat_info
+(brace
+DECL|member|mac
+r_int
+r_char
+id|mac
+(braket
+id|ETH_ALEN
+)braket
+suffix:semicolon
+singleline_comment|// EBT_ACCEPT, EBT_DROP, EBT_CONTINUE or EBT_RETURN
+DECL|member|target
+r_int
+id|target
+suffix:semicolon
+)brace
+suffix:semicolon
+DECL|macro|EBT_SNAT_TARGET
+mdefine_line|#define EBT_SNAT_TARGET &quot;snat&quot;
+DECL|macro|EBT_DNAT_TARGET
+mdefine_line|#define EBT_DNAT_TARGET &quot;dnat&quot;
+macro_line|#endif
+eof
