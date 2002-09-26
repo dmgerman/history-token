0543aa3fdf448286b5555e18037e64910feeb1e7
Brian Hall <brihall@pcisys.net>
2002-09-26 09:01:20 -0700

    [PATCH] Update for JMTek USBDrive
    Attached is a patch against the 2.4.19 linux kernel. It adds an entry
    for another version of the JMTek USBDrive (driverless), and also updates
    my email address.

--- drivers/usb/storage/unusual_devs.h
+++ drivers/usb/storage/unusual_devs.h
@@ -1835,7 +1835,7 @@ op_or
 id|US_FL_FIX_INQUIRY
 )paren
 comma
-multiline_comment|/* Submitted by Brian Hall &lt;brihall@bigfoot.com&gt;&n; * Needed for START_STOP flag */
+multiline_comment|/* Submitted by Brian Hall &lt;brihall@pcisys.net&gt;&n; * Needed for START_STOP flag */
 id|UNUSUAL_DEV
 c_func
 (paren
@@ -1860,7 +1860,31 @@ comma
 id|US_FL_START_STOP
 )paren
 comma
-multiline_comment|/* Reported by Dan Pilone &lt;pilone@slac.com&gt;&n; * The device needs the flags only.&n; * Also reported by Brian Hall &lt;brihall@bigfoot.com&gt;, again for flags.&n; * I also suspect this device may have a broken serial number.&n; */
+id|UNUSUAL_DEV
+c_func
+(paren
+l_int|0x0c76
+comma
+l_int|0x0005
+comma
+l_int|0x0100
+comma
+l_int|0x0100
+comma
+l_string|&quot;JMTek&quot;
+comma
+l_string|&quot;USBDrive&quot;
+comma
+id|US_SC_SCSI
+comma
+id|US_PR_BULK
+comma
+l_int|NULL
+comma
+id|US_FL_START_STOP
+)paren
+comma
+multiline_comment|/* Reported by Dan Pilone &lt;pilone@slac.com&gt;&n; * The device needs the flags only.&n; * Also reported by Brian Hall &lt;brihall@pcisys.net&gt;, again for flags.&n; * I also suspect this device may have a broken serial number.&n; */
 id|UNUSUAL_DEV
 c_func
 (paren
