7bfcf5d3d7d30c03daae332a3feb3e04cdd683d5
Russell King <rmk@flint.arm.linux.org.uk>
2002-09-29 19:42:34 +0100

    [ARM] Remove keyboard.h includes and some generic ARM keyboard bits. This keeps ARM in line with the continued transition to the input layer.

--- include/asm-arm/arch-rpc/keyboard.h
+++
@@ -1,4 +0,0 @@
-multiline_comment|/*&n; *  linux/include/asm-arm/arch-rpc/keyboard.h&n; *&n; *  Copyright (C) 1998-2001 Russell King&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License version 2 as&n; * published by the Free Software Foundation.&n; *&n; *  Keyboard driver definitions for RiscPC architecture&n; *  Now using the input subsystem...&n; */
-DECL|macro|kbd_init_hw
-mdefine_line|#define kbd_init_hw()&t;&t;do { } while (0)
-eof
