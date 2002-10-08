14d13a2625b6301afcfa25381857e594ab52b100
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-10-08 00:57:47 -0700

    [PATCH] first pass at seagate st-02 for 2.5

--- drivers/scsi/seagate.h
+++ drivers/scsi/seagate.h
@@ -2,8 +2,7 @@ multiline_comment|/*&n; *&t;seagate.h Copyright (C) 1992 Drew Eckhardt &n; *&t;l
 macro_line|#ifndef _SEAGATE_H
 DECL|macro|SEAGATE_H
 mdefine_line|#define SEAGATE_H
-multiline_comment|/*&n;&t;$Header&n;*/
-macro_line|#ifndef ASM
+r_static
 r_int
 id|seagate_st0x_detect
 c_func
@@ -12,6 +11,7 @@ id|Scsi_Host_Template
 op_star
 )paren
 suffix:semicolon
+r_static
 r_int
 id|seagate_st0x_command
 c_func
@@ -20,6 +20,7 @@ id|Scsi_Cmnd
 op_star
 )paren
 suffix:semicolon
+r_static
 r_int
 id|seagate_st0x_queue_command
 c_func
@@ -47,6 +48,7 @@ id|Scsi_Cmnd
 op_star
 )paren
 suffix:semicolon
+r_static
 r_const
 r_char
 op_star
@@ -60,18 +62,32 @@ op_star
 suffix:semicolon
 r_static
 r_int
-id|seagate_st0x_reset
+id|seagate_st0x_bus_reset
 c_func
 (paren
 id|Scsi_Cmnd
 op_star
-comma
+)paren
+suffix:semicolon
+r_static
 r_int
+id|seagate_st0x_device_reset
+c_func
+(paren
+id|Scsi_Cmnd
+op_star
+)paren
+suffix:semicolon
+r_static
 r_int
+id|seagate_st0x_host_reset
+c_func
+(paren
+id|Scsi_Cmnd
+op_star
 )paren
 suffix:semicolon
 DECL|macro|SEAGATE_ST0X
-mdefine_line|#define SEAGATE_ST0X  {  detect:         seagate_st0x_detect,&t;&t;&bslash;&n;&t;&t;&t; info:           seagate_st0x_info,&t;&t;&bslash;&n;&t;&t;&t; command:        seagate_st0x_command,&t;&t;&bslash;&n;&t;&t;&t; queuecommand:   seagate_st0x_queue_command,&t;&bslash;&n;&t;&t;&t; abort:          seagate_st0x_abort,&t;&t;&bslash;&n;&t;&t;&t; reset:          seagate_st0x_reset,&t;&t;&bslash;&n;&t;&t;&t; can_queue:      1,&t;&t;&t;&t;&bslash;&n;&t;&t;&t; this_id:        7,&t;&t;&t;&t;&bslash;&n;&t;&t;&t; sg_tablesize:   SG_ALL,&t;&t;&t;&bslash;&n;&t;&t;&t; cmd_per_lun:    1,&t;&t;&t;&t;&bslash;&n;&t;&t;&t; use_clustering: DISABLE_CLUSTERING}
-macro_line|#endif /* ASM */
+mdefine_line|#define SEAGATE_ST0X  {  detect:         seagate_st0x_detect,&t;&t;&t;&bslash;&n;&t;&t;&t; info:           seagate_st0x_info,&t;&t;&t;&bslash;&n;&t;&t;&t; command:        seagate_st0x_command,&t;&t;&t;&bslash;&n;&t;&t;&t; queuecommand:   seagate_st0x_queue_command,&t;&t;&bslash;&n;&t;&t;&t; eh_abort_handler:&t;seagate_st0x_abort,&t;&t;&bslash;&n;&t;&t;&t; eh_bus_reset_handler:  seagate_st0x_bus_reset,&t;&t;&bslash;&n;&t;&t;&t; eh_host_reset_handler: seagate_st0x_host_reset,&t;&bslash;&n;&t;&t;&t; eh_device_reset_handler:seagate_st0x_device_reset,&t;&bslash;&n;&t;&t;&t; can_queue:      1,&t;&t;&t;&t;&t;&bslash;&n;&t;&t;&t; this_id:        7,&t;&t;&t;&t;&t;&bslash;&n;&t;&t;&t; sg_tablesize:   SG_ALL,&t;&t;&t;&t;&bslash;&n;&t;&t;&t; cmd_per_lun:    1,&t;&t;&t;&t;&t;&bslash;&n;&t;&t;&t; use_clustering: DISABLE_CLUSTERING}
 macro_line|#endif /* _SEAGATE_H */
 eof
