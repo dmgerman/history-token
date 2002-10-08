a6cc51abb3fc7b11ac4721963dc6b679b219d0e4
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-10-08 00:58:08 -0700

    [PATCH] Fix 2.5 signal handling in jffs/jffs2
    Ingo chamged it again 8)

--- fs/jffs2/background.c
+++ fs/jffs2/background.c
@@ -466,9 +466,6 @@ op_amp
 id|current-&gt;sig-&gt;shared_pending
 comma
 op_amp
-id|current-&gt;blocked
-comma
-op_amp
 id|info
 )paren
 suffix:semicolon
@@ -485,9 +482,6 @@ c_func
 (paren
 op_amp
 id|current-&gt;pending
-comma
-op_amp
-id|current-&gt;blocked
 comma
 op_amp
 id|info
