a386e948b139c9c1ecd492170cf696f32fc67508
John Stultz <johnstul@us.ibm.com>
2002-10-09 21:37:48 -0700

    [PATCH] linux-2.5.41_timer-changes_A4 (1/3 - infrastructure)
            The i386 time.c code is turning into a mess. We've got multiple
    functions that do the same thing, only with different hardware, all
    surrounded #ifdefs and even more difficult to follow #ifndefs. George
    Anzinger is introducing a new ACPIpm time source, I'm going to attempt
    to add the cyclone counter as a time source, and in the future there
    will be HPET to deal with. These will not go in cleanly together as
    things are now.
    
            Inspired by suggestions from Alan, this collection of patches
    tries to clean up time.c by breaking out the PIT and TSC specific parts
    into their own files. Additionally the patch creates an abstract
    interface to use these existing time soruces, as well as make it easier
    to add future time sources.
    
            It introduces "struct timer_ops" which gives the time code a
    clear interface to use these different time sources. It also allows for
    clearer conditional compilation of these various time sources.
    
            This first patch (part 1 of 3) provides the infrastructure
    needed via the timer_ops structure, as well as the select_timer()
    function for choosing the best available timer.

---
+++ arch/i386/kernel/timers/timer.c
@@ -0,0 +1,81 @@
+macro_line|#include &lt;linux/kernel.h&gt;
+macro_line|#include &lt;asm/timer.h&gt;
+multiline_comment|/* list of externed timers */
+multiline_comment|/* eg: extern struct timer_opts timer_XXX*/
+suffix:semicolon
+multiline_comment|/* list of timers, ordered by preference */
+DECL|variable|timers
+r_struct
+id|timer_opts
+op_star
+id|timers
+(braket
+)braket
+op_assign
+(brace
+multiline_comment|/* eg: &amp;timer_XXX */
+)brace
+suffix:semicolon
+DECL|macro|NR_TIMERS
+mdefine_line|#define NR_TIMERS (sizeof(timers)/sizeof(timers[0]))
+multiline_comment|/* iterates through the list of timers, returning the first &n; * one that initializes successfully.&n; */
+DECL|function|select_timer
+r_struct
+id|timer_opts
+op_star
+id|select_timer
+c_func
+(paren
+r_void
+)paren
+(brace
+r_int
+id|i
+suffix:semicolon
+multiline_comment|/* find most preferred working timer */
+r_for
+c_loop
+(paren
+id|i
+op_assign
+l_int|0
+suffix:semicolon
+id|i
+OL
+id|NR_TIMERS
+suffix:semicolon
+id|i
+op_increment
+)paren
+r_if
+c_cond
+(paren
+id|timers
+(braket
+id|i
+)braket
+op_member_access_from_pointer
+id|init
+c_func
+(paren
+)paren
+)paren
+(brace
+r_return
+id|timers
+(braket
+id|i
+)braket
+suffix:semicolon
+)brace
+id|panic
+c_func
+(paren
+l_string|&quot;select_timer: Cannot find a suitable timer&bslash;n&quot;
+)paren
+suffix:semicolon
+r_return
+l_int|0
+suffix:semicolon
+)brace
+eof
---
+++ include/asm-i386/timer.h
@@ -0,0 +1,54 @@
+macro_line|#ifndef _ASMi386_TIMER_H
+DECL|macro|_ASMi386_TIMER_H
+mdefine_line|#define _ASMi386_TIMER_H
+DECL|struct|timer_opts
+r_struct
+id|timer_opts
+(brace
+multiline_comment|/* probes and initializes timer. returns 1 on sucess, 0 on failure */
+DECL|member|init
+r_int
+(paren
+op_star
+id|init
+)paren
+(paren
+r_void
+)paren
+suffix:semicolon
+multiline_comment|/* called by the timer interrupt */
+DECL|member|mark_offset
+r_void
+(paren
+op_star
+id|mark_offset
+)paren
+(paren
+r_void
+)paren
+suffix:semicolon
+multiline_comment|/* called by gettimeofday. returns # ms since the last timer interrupt */
+DECL|member|get_offset
+r_int
+r_int
+(paren
+op_star
+id|get_offset
+)paren
+(paren
+r_void
+)paren
+suffix:semicolon
+)brace
+suffix:semicolon
+r_struct
+id|timer_opts
+op_star
+id|select_timer
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
+macro_line|#endif
+eof
