64c90a21adf80fc991d538407f016b63a38a3313
Alan Cox <alan@lxorguk.ukuu.org.uk>
2002-10-12 03:18:54 -0700

    [PATCH] __ret is deprecated

--- include/asm-cris/uaccess.h
+++ include/asm-cris/uaccess.h
@@ -109,15 +109,6 @@ DECL|macro|__get_user
 mdefine_line|#define __get_user(x,ptr) &bslash;&n;  __get_user_nocheck((x),(ptr),sizeof(*(ptr)))
 DECL|macro|__put_user
 mdefine_line|#define __put_user(x,ptr) &bslash;&n;  __put_user_nocheck((__typeof__(*(ptr)))(x),(ptr),sizeof(*(ptr)))
-multiline_comment|/*&n; * The &quot;xxx_ret&quot; versions return constant specified in third argument, if&n; * something bad happens. These macros can be optimized for the&n; * case of just returning from the function xxx_ret is used.&n; */
-DECL|macro|put_user_ret
-mdefine_line|#define put_user_ret(x,ptr,ret) &bslash;&n;&t;do { if (put_user(x,ptr)) return ret; } while (0)
-DECL|macro|get_user_ret
-mdefine_line|#define get_user_ret(x,ptr,ret) &bslash;&n;&t;do { if (get_user(x,ptr)) return ret; } while (0)
-DECL|macro|__put_user_ret
-mdefine_line|#define __put_user_ret(x,ptr,ret) &bslash;&n;&t;do { if (__put_user(x,ptr)) return ret; } while (0)
-DECL|macro|__get_user_ret
-mdefine_line|#define __get_user_ret(x,ptr,ret) &bslash;&n;&t;do { if (__get_user(x,ptr)) return ret; } while (0)
 r_extern
 r_int
 id|__put_user_bad
@@ -1667,10 +1658,6 @@ DECL|macro|copy_from_user
 mdefine_line|#define copy_from_user(to, from, n)&t;&t;&bslash;&n;(__builtin_constant_p(n) ?&t;&t;&t;&bslash;&n; __constant_copy_from_user(to, from, n) :&t;&bslash;&n; __generic_copy_from_user(to, from, n))
 DECL|macro|copy_to_user
 mdefine_line|#define copy_to_user(to, from, n)&t;&t;&bslash;&n;(__builtin_constant_p(n) ?&t;&t;&t;&bslash;&n; __constant_copy_to_user(to, from, n) :&t;&t;&bslash;&n; __generic_copy_to_user(to, from, n))
-DECL|macro|copy_to_user_ret
-mdefine_line|#define copy_to_user_ret(to,from,n,retval) &bslash;&n;&t;do { if (copy_to_user(to,from,n)) return retval; } while (0)
-DECL|macro|copy_from_user_ret
-mdefine_line|#define copy_from_user_ret(to,from,n,retval) &bslash;&n;&t;do { if (copy_from_user(to,from,n)) return retval; } while (0)
 multiline_comment|/* We let the __ versions of copy_from/to_user inline, because they&squot;re often&n; * used in fast paths and have only a small space overhead.&n; */
 r_static
 r_inline
