75d50608010f0aeff5a9dcc745078ac2ce3d5ef1
Andrew Morton <akpm@digeo.com>
2002-10-12 19:32:50 -0700

    [PATCH] page freeing function for swsusp
    Software suspend needs a way of forcing page reclaim, up to the point
    where 50% of memory is free.
    
    This patch implements a function to do that:
    
    	int shrink_all_memory(int nr_pages);
    
    Will attempt to reclaim <BACKTICK>nr_pages' pages and return them to the free
    pages pool.  It returns the number of pages which it actually freed.
    
    If called with a "large" number of pages it will only free up to a few
    hundred, so the caller needs to loop on it.
    
    If it returns zero then there is no point in calling it again.

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -471,6 +471,14 @@ r_int
 r_int
 )paren
 suffix:semicolon
+r_int
+id|shrink_all_memory
+c_func
+(paren
+r_int
+id|nr_pages
+)paren
+suffix:semicolon
 multiline_comment|/* linux/mm/page_io.c */
 r_int
 id|swap_readpage
