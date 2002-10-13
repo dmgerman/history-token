c826e90bb588b05c3e8f9b6e41a99c578723e43d
Russell King <rmk@flint.arm.linux.org.uk>
2002-10-13 17:57:10 +0100

    [ARM] Fix fas216 use of __FUNCTION__ macro.

--- drivers/acorn/scsi/fas216.c
+++ drivers/acorn/scsi/fas216.c
@@ -8737,15 +8737,15 @@ suffix:semicolon
 id|printk
 c_func
 (paren
-l_string|&quot;scsi%d.%c: &quot;
-id|__FUNCTION__
-l_string|&quot;: called&bslash;n&quot;
+l_string|&quot;scsi%d.%c: %s: called&bslash;n&quot;
 comma
 id|info-&gt;host-&gt;host_no
 comma
 l_char|&squot;0&squot;
 op_plus
 id|SCpnt-&gt;target
+comma
+id|__FUNCTION__
 )paren
 suffix:semicolon
 r_return
@@ -8791,15 +8791,15 @@ suffix:semicolon
 id|printk
 c_func
 (paren
-l_string|&quot;scsi%d.%c: &quot;
-id|__FUNCTION__
-l_string|&quot;: resetting bus&bslash;n&quot;
+l_string|&quot;scsi%d.%c: %s: resetting bus&bslash;n&quot;
 comma
 id|info-&gt;host-&gt;host_no
 comma
 l_char|&squot;0&squot;
 op_plus
 id|SCpnt-&gt;target
+comma
+id|__FUNCTION__
 )paren
 suffix:semicolon
 multiline_comment|/*&n;&t; * Attempt to stop all activity on this interface.&n;&t; */
@@ -9066,15 +9066,15 @@ suffix:semicolon
 id|printk
 c_func
 (paren
-l_string|&quot;scsi%d.%c: &quot;
-id|__FUNCTION__
-l_string|&quot;: resetting host&bslash;n&quot;
+l_string|&quot;scsi%d.%c: %s: resetting host&bslash;n&quot;
 comma
 id|info-&gt;host-&gt;host_no
 comma
 l_char|&squot;0&squot;
 op_plus
 id|SCpnt-&gt;target
+comma
+id|__FUNCTION__
 )paren
 suffix:semicolon
 multiline_comment|/*&n;&t; * Reset the SCSI chip.&n;&t; */
