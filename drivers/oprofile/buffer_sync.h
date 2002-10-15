8a3f0ba195229bb1dffc323aed9095eafa1f0b9c
John Levon <levon@movementarian.org>
2002-10-15 04:30:56 -0700

    [PATCH] oprofile - core
    Add the oprofile core.  The core design is very similar to that we
    discussed in private mail.  The nasty details should be documented in
    the patch below.

---
+++ drivers/oprofile/buffer_sync.h
@@ -0,0 +1,22 @@
+multiline_comment|/**&n; * @file buffer_sync.h&n; *&n; * @remark Copyright 2002 OProfile authors&n; * @remark Read the file COPYING&n; *&n; * @author John Levon &lt;levon@movementarian.org&gt;&n; */
+macro_line|#ifndef OPROFILE_BUFFER_SYNC_H
+DECL|macro|OPROFILE_BUFFER_SYNC_H
+mdefine_line|#define OPROFILE_BUFFER_SYNC_H
+multiline_comment|/* add the necessary profiling hooks */
+r_int
+id|sync_start
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
+multiline_comment|/* remove the hooks */
+r_void
+id|sync_stop
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
+macro_line|#endif /* OPROFILE_BUFFER_SYNC_H */
+eof
---
+++ drivers/oprofile/oprofile_stats.h
@@ -0,0 +1,56 @@
+multiline_comment|/**&n; * @file oprofile_stats.h&n; *&n; * @remark Copyright 2002 OProfile authors&n; * @remark Read the file COPYING&n; *&n; * @author John Levon&n; */
+macro_line|#ifndef OPROFILE_STATS_H
+DECL|macro|OPROFILE_STATS_H
+mdefine_line|#define OPROFILE_STATS_H
+macro_line|#include &lt;asm/atomic.h&gt;
+DECL|struct|oprofile_stat_struct
+r_struct
+id|oprofile_stat_struct
+(brace
+DECL|member|sample_lost_mmap_sem
+id|atomic_t
+id|sample_lost_mmap_sem
+suffix:semicolon
+DECL|member|event_lost_overflow
+id|atomic_t
+id|event_lost_overflow
+suffix:semicolon
+)brace
+suffix:semicolon
+r_extern
+r_struct
+id|oprofile_stat_struct
+id|oprofile_stats
+suffix:semicolon
+multiline_comment|/* reset all stats to zero */
+r_void
+id|oprofile_reset_stats
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
+r_struct
+id|super_block
+suffix:semicolon
+r_struct
+id|dentry
+suffix:semicolon
+multiline_comment|/* create the stats/ dir */
+r_void
+id|oprofile_create_stats_files
+c_func
+(paren
+r_struct
+id|super_block
+op_star
+id|sb
+comma
+r_struct
+id|dentry
+op_star
+id|root
+)paren
+suffix:semicolon
+macro_line|#endif /* OPROFILE_STATS_H */
+eof
