61e75ae6ac05888fd79ff4a34ea997ceb2502c43
Andrew Morton <akpm@digeo.com>
2002-10-17 08:32:42 -0700

    [PATCH] ramdisk fix
    The ramdisk address_space needs to have a non-zero
    mapping->backing_dev_info.memory_backed, so it doesn't contribute to
    global memory accounting.
    
    At present it is confusing memory accounting and causing
    balance_dirty_pages to go into an infinite loop.

--- drivers/block/rd.c
+++ drivers/block/rd.c
@@ -8,6 +8,7 @@ macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/devfs_fs_kernel.h&gt;
 macro_line|#include &lt;linux/buffer_head.h&gt;&t;&t;/* for invalidate_bdev() */
+macro_line|#include &lt;linux/backing-dev.h&gt;
 macro_line|#include &lt;asm/uaccess.h&gt;
 multiline_comment|/*&n; * 35 has been officially registered as the RAMDISK major number, but&n; * so is the original MAJOR number of 1.  We&squot;re using 1 in&n; * include/linux/major.h for now&n; */
 DECL|macro|MAJOR_NR
@@ -1231,6 +1232,27 @@ comma
 )brace
 suffix:semicolon
 macro_line|#endif
+DECL|variable|rd_backing_dev_info
+r_static
+r_struct
+id|backing_dev_info
+id|rd_backing_dev_info
+op_assign
+(brace
+dot
+id|ra_pages
+op_assign
+l_int|0
+comma
+multiline_comment|/* No readahead */
+dot
+id|memory_backed
+op_assign
+l_int|1
+comma
+multiline_comment|/* Does not contribute to dirty memory */
+)brace
+suffix:semicolon
 DECL|function|rd_open
 r_static
 r_int
@@ -1367,6 +1389,16 @@ id|bd_inode-&gt;i_mapping-&gt;a_ops
 op_assign
 op_amp
 id|ramdisk_aops
+suffix:semicolon
+id|rd_bdev
+(braket
+id|unit
+)braket
+op_member_access_from_pointer
+id|bd_inode-&gt;i_mapping-&gt;backing_dev_info
+op_assign
+op_amp
+id|rd_backing_dev_info
 suffix:semicolon
 id|rd_bdev
 (braket
