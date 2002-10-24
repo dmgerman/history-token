99bcdb12db2b62467085916637eb06c202c80291
David Mosberger <davidm@tiger.hpl.hp.com>
2002-10-24 07:57:18 -0700

    ia64: Clean up ia64 version of topology.h.

--- include/asm-ia64/topology.h
+++ include/asm-ia64/topology.h
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * linux/include/asm-ia64/topology.h&n; *&n; * Copyright (C) 2002, Erich Focht, NEC&n; *&n; * All rights reserved.          &n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or&n; * (at your option) any later version.&n; */
+multiline_comment|/*&n; * linux/include/asm-ia64/topology.h&n; *&n; * Copyright (C) 2002, Erich Focht, NEC&n; *&n; * All rights reserved.&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or&n; * (at your option) any later version.&n; */
 macro_line|#ifndef _ASM_IA64_TOPOLOGY_H
 DECL|macro|_ASM_IA64_TOPOLOGY_H
 mdefine_line|#define _ASM_IA64_TOPOLOGY_H
@@ -20,10 +20,10 @@ macro_line|#else
 DECL|macro|__memblk_to_node
 mdefine_line|#define __memblk_to_node(memblk) (memblk)
 macro_line|#endif
-multiline_comment|/* &n; * Returns the number of the node containing Node &squot;nid&squot;.&n; * Not implemented here. Multi-level hierarchies detected with&n; * the help of node_distance().&n; */
+multiline_comment|/*&n; * Returns the number of the node containing Node &squot;nid&squot;.&n; * Not implemented here. Multi-level hierarchies detected with&n; * the help of node_distance().&n; */
 DECL|macro|__parent_node
 mdefine_line|#define __parent_node(nid) (nid)
-multiline_comment|/* &n; * Returns the number of the first CPU on Node &squot;node&squot;.&n; * Slow in the current implementation.&n; * Who needs this?&n; */
+multiline_comment|/*&n; * Returns the number of the first CPU on Node &squot;node&squot;.&n; * Slow in the current implementation.&n; * Who needs this?&n; */
 multiline_comment|/* #define __node_to_first_cpu(node) pool_cpus[pool_ptr[node]] */
 DECL|function|__node_to_first_cpu
 r_static
@@ -38,8 +38,6 @@ id|node
 (brace
 r_int
 id|i
-comma
-id|cpu
 suffix:semicolon
 r_for
 c_loop
@@ -80,7 +78,7 @@ op_minus
 l_int|1
 suffix:semicolon
 )brace
-multiline_comment|/* &n; * Returns a bitmask of CPUs on Node &squot;node&squot;.&n; */
+multiline_comment|/*&n; * Returns a bitmask of CPUs on Node &squot;node&squot;.&n; */
 DECL|function|__node_to_cpu_mask
 r_static
 r_inline
@@ -140,10 +138,5 @@ suffix:semicolon
 multiline_comment|/*&n; * Returns the number of the first MemBlk on Node &squot;node&squot;&n; * Should be fixed when IA64 discontigmem goes in.&n; */
 DECL|macro|__node_to_memblk
 mdefine_line|#define __node_to_memblk(node) (node)
-macro_line|#endif /* _ASM_IA64_TOPOLOGY_H */
-macro_line|#ifndef _ASM_IA64_TOPOLOGY_H
-DECL|macro|_ASM_IA64_TOPOLOGY_H
-mdefine_line|#define _ASM_IA64_TOPOLOGY_H
-macro_line|#include &lt;asm-generic/topology.h&gt;
 macro_line|#endif /* _ASM_IA64_TOPOLOGY_H */
 eof
