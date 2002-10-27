c27412a5cf2c7369e28845403b63522b538716b5
Mark-André Hopf <mhopf@innominate.com>
2002-10-27 12:58:41 -0800

    [EBTABLES]: Add tcp/udp port checking.

--- include/linux/netfilter_bridge/ebt_ip.h
+++ include/linux/netfilter_bridge/ebt_ip.h
@@ -1,3 +1,4 @@
+multiline_comment|/*&n; *  ebt_ip&n; *&n; *&t;Authors:&n; *&t;Bart De Schuymer &lt;bart.de.schuymer@pandora.be&gt;&n; *&n; *  April, 2002&n; *&n; *  Changes:&n; *    added ip-sport and ip-dport&n; *    Innominate Security Technologies AG &lt;mhopf@innominate.com&gt;&n; *    September, 2002&n; */
 macro_line|#ifndef __LINUX_BRIDGE_EBT_IP_H
 DECL|macro|__LINUX_BRIDGE_EBT_IP_H
 mdefine_line|#define __LINUX_BRIDGE_EBT_IP_H
@@ -9,8 +10,12 @@ DECL|macro|EBT_IP_TOS
 mdefine_line|#define EBT_IP_TOS 0x04
 DECL|macro|EBT_IP_PROTO
 mdefine_line|#define EBT_IP_PROTO 0x08
+DECL|macro|EBT_IP_SPORT
+mdefine_line|#define EBT_IP_SPORT 0x10
+DECL|macro|EBT_IP_DPORT
+mdefine_line|#define EBT_IP_DPORT 0x20
 DECL|macro|EBT_IP_MASK
-mdefine_line|#define EBT_IP_MASK (EBT_IP_SOURCE | EBT_IP_DEST | EBT_IP_TOS | EBT_IP_PROTO)
+mdefine_line|#define EBT_IP_MASK (EBT_IP_SOURCE | EBT_IP_DEST | EBT_IP_TOS | EBT_IP_PROTO |&bslash;&n; EBT_IP_SPORT | EBT_IP_DPORT )
 DECL|macro|EBT_IP_MATCH
 mdefine_line|#define EBT_IP_MATCH &quot;ip&quot;
 singleline_comment|// the same values are used for the invflags
@@ -49,6 +54,20 @@ suffix:semicolon
 DECL|member|invflags
 r_uint8
 id|invflags
+suffix:semicolon
+DECL|member|sport
+r_uint16
+id|sport
+(braket
+l_int|2
+)braket
+suffix:semicolon
+DECL|member|dport
+r_uint16
+id|dport
+(braket
+l_int|2
+)braket
 suffix:semicolon
 )brace
 suffix:semicolon
