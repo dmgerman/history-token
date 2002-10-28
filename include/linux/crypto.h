7ec0b2724257660621fa6ee3dbc8cffa80cd089f
David S. Miller <davem@nuts.ninka.net>
2002-10-28 11:37:18 -0800

    include/linux/crypto.h: Include linux/string.h

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -5,6 +5,7 @@ mdefine_line|#define _LINUX_CRYPTO_H
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/list.h&gt;
+macro_line|#include &lt;linux/string.h&gt;
 multiline_comment|/*&n; * Algorithm masks and types.&n; */
 DECL|macro|CRYPTO_ALG_TYPE_MASK
 mdefine_line|#define CRYPTO_ALG_TYPE_MASK&t;&t;0x000000ff
