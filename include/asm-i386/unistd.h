065ac7297adb946c84eb4143461494001fc7dab9
Davide Libenzi <davidel@xmailserver.org>
2002-10-29 22:35:31 -0800

    [PATCH] sys_epoll 0.15
    Latest version of the epoll interfaces.

--- include/asm-i386/unistd.h
+++ include/asm-i386/unistd.h
@@ -508,6 +508,12 @@ DECL|macro|__NR_exit_group
 mdefine_line|#define __NR_exit_group&t;&t;252
 DECL|macro|__NR_lookup_dcookie
 mdefine_line|#define __NR_lookup_dcookie&t;253
+DECL|macro|__NR_sys_epoll_create
+mdefine_line|#define __NR_sys_epoll_create&t;254
+DECL|macro|__NR_sys_epoll_ctl
+mdefine_line|#define __NR_sys_epoll_ctl&t;255
+DECL|macro|__NR_sys_epoll_wait
+mdefine_line|#define __NR_sys_epoll_wait&t;256
 multiline_comment|/* user-visible error numbers are in the range -1 - -124: see &lt;asm-i386/errno.h&gt; */
 DECL|macro|__syscall_return
 mdefine_line|#define __syscall_return(type, res) &bslash;&n;do { &bslash;&n;&t;if ((unsigned long)(res) &gt;= (unsigned long)(-125)) { &bslash;&n;&t;&t;errno = -(res); &bslash;&n;&t;&t;res = -1; &bslash;&n;&t;} &bslash;&n;&t;return (type) (res); &bslash;&n;} while (0)
