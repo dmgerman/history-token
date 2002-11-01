732286aa00fcbd9b501246a85ddbedb6ff19b0f5
Andrew Morton <akpm@digeo.com>
2002-10-31 20:01:48 -0800

    [PATCH] Update/Create core files for DriverFS Topology.
    From Matthew Dobson.
    
    Update/Create core files for DriverFS Topology.
    
    This patch creates the generic structures that are (will be) embedded in
    the per-arch structures.  Also creates calls to register these generic
    structures (CPUs, MemBlks, & Nodes).
    
    Note that without arch-specific structures in which to embed these
    structures, and an arch-specific initialization routine, these
    functions/structures remain unused.

--- drivers/base/memblk.c
+++ drivers/base/memblk.c
@@ -1,13 +1,14 @@
-multiline_comment|/*&n; * drivers/base/cpu.c - basic CPU class support&n; */
+multiline_comment|/*&n; * drivers/base/memblk.c - basic Memory Block class support&n; */
 macro_line|#include &lt;linux/device.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
-macro_line|#include &lt;linux/cpu.h&gt;
+macro_line|#include &lt;linux/memblk.h&gt;
+macro_line|#include &lt;linux/node.h&gt;
 macro_line|#include &lt;asm/topology.h&gt;
-DECL|function|cpu_add_device
+DECL|function|memblk_add_device
 r_static
 r_int
-id|cpu_add_device
+id|memblk_add_device
 c_func
 (paren
 r_struct
@@ -20,34 +21,34 @@ r_return
 l_int|0
 suffix:semicolon
 )brace
-DECL|variable|cpu_devclass
+DECL|variable|memblk_devclass
 r_struct
 id|device_class
-id|cpu_devclass
+id|memblk_devclass
 op_assign
 (brace
 dot
 id|name
 op_assign
-l_string|&quot;cpu&quot;
+l_string|&quot;memblk&quot;
 comma
 dot
 id|add_device
 op_assign
-id|cpu_add_device
+id|memblk_add_device
 comma
 )brace
 suffix:semicolon
-DECL|variable|cpu_driver
+DECL|variable|memblk_driver
 r_struct
 id|device_driver
-id|cpu_driver
+id|memblk_driver
 op_assign
 (brace
 dot
 id|name
 op_assign
-l_string|&quot;cpu&quot;
+l_string|&quot;memblk&quot;
 comma
 dot
 id|bus
@@ -59,21 +60,21 @@ dot
 id|devclass
 op_assign
 op_amp
-id|cpu_devclass
+id|memblk_devclass
 comma
 )brace
 suffix:semicolon
-multiline_comment|/*&n; * register_cpu - Setup a driverfs device for a CPU.&n; * @num - CPU number to use when creating the device.&n; *&n; * Initialize and register the CPU device.&n; */
-DECL|function|register_cpu
+multiline_comment|/*&n; * register_memblk - Setup a driverfs device for a MemBlk&n; * @num - MemBlk number to use when creating the device.&n; *&n; * Initialize and register the MemBlk device.&n; */
+DECL|function|register_memblk
 r_void
 id|__init
-id|register_cpu
+id|register_memblk
 c_func
 (paren
 r_struct
-id|cpu
+id|memblk
 op_star
-id|cpu
+id|memblk
 comma
 r_int
 id|num
@@ -84,19 +85,19 @@ op_star
 id|root
 )paren
 (brace
-id|cpu-&gt;node_id
+id|memblk-&gt;node_id
 op_assign
-id|__cpu_to_node
+id|__memblk_to_node
 c_func
 (paren
 id|num
 )paren
 suffix:semicolon
-id|cpu-&gt;sysdev.name
+id|memblk-&gt;sysdev.name
 op_assign
-l_string|&quot;cpu&quot;
+l_string|&quot;memblk&quot;
 suffix:semicolon
-id|cpu-&gt;sysdev.id
+id|memblk-&gt;sysdev.id
 op_assign
 id|num
 suffix:semicolon
@@ -105,7 +106,7 @@ c_cond
 (paren
 id|root
 )paren
-id|cpu-&gt;sysdev.root
+id|memblk-&gt;sysdev.root
 op_assign
 op_amp
 id|root-&gt;sysroot
@@ -113,33 +114,33 @@ suffix:semicolon
 id|snprintf
 c_func
 (paren
-id|cpu-&gt;sysdev.dev.name
+id|memblk-&gt;sysdev.dev.name
 comma
 id|DEVICE_NAME_SIZE
 comma
-l_string|&quot;CPU %u&quot;
+l_string|&quot;Memory Block %u&quot;
 comma
 id|num
 )paren
 suffix:semicolon
-id|cpu-&gt;sysdev.dev.driver
+id|memblk-&gt;sysdev.dev.driver
 op_assign
 op_amp
-id|cpu_driver
+id|memblk_driver
 suffix:semicolon
 id|sys_device_register
 c_func
 (paren
 op_amp
-id|cpu-&gt;sysdev
+id|memblk-&gt;sysdev
 )paren
 suffix:semicolon
 )brace
-DECL|function|register_cpu_type
+DECL|function|register_memblk_type
 r_static
 r_int
 id|__init
-id|register_cpu_type
+id|register_memblk_type
 c_func
 (paren
 r_void
@@ -149,7 +150,7 @@ id|driver_register
 c_func
 (paren
 op_amp
-id|cpu_driver
+id|memblk_driver
 )paren
 suffix:semicolon
 r_return
@@ -157,15 +158,15 @@ id|devclass_register
 c_func
 (paren
 op_amp
-id|cpu_devclass
+id|memblk_devclass
 )paren
 suffix:semicolon
 )brace
-DECL|variable|register_cpu_type
+DECL|variable|register_memblk_type
 id|postcore_initcall
 c_func
 (paren
-id|register_cpu_type
+id|register_memblk_type
 )paren
 suffix:semicolon
 eof
