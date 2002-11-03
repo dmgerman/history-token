d58b2a96fefe979a279b36c71286f672a0ccfe3a
Davide Libenzi <davidel@xmailserver.org>
2002-11-02 19:40:57 -0800

    [PATCH] epoll update r3
     - EP_CTL_MOD drops an event if conditions events are met
     - The source file eventpoll.c moved from drivers/char to fs
     - Fixed a weirdness with tty's
    
    Missing: system calls for arch != i386 ...

--- include/linux/eventpoll.h
+++ include/linux/eventpoll.h
@@ -1,61 +1,29 @@
-multiline_comment|/*&n; *  include/linux/eventpoll.h ( Efficent event polling implementation )&n; *  Copyright (C) 2001,...,2002  Davide Libenzi&n; *&n; *  This program is free software; you can redistribute it and/or modify&n; *  it under the terms of the GNU General Public License as published by&n; *  the Free Software Foundation; either version 2 of the License, or&n; *  (at your option) any later version.&n; *&n; *  Davide Libenzi &lt;davidel@xmailserver.org&gt;&n; *&n; */
+multiline_comment|/*&n; *  include/linux/eventpoll.h ( Efficent event polling implementation )&n; *  Copyright (C) 2001,...,2002&t; Davide Libenzi&n; *&n; *  This program is free software; you can redistribute it and/or modify&n; *  it under the terms of the GNU General Public License as published by&n; *  the Free Software Foundation; either version 2 of the License, or&n; *  (at your option) any later version.&n; *&n; *  Davide Libenzi &lt;davidel@xmailserver.org&gt;&n; *&n; */
 macro_line|#ifndef _LINUX_EVENTPOLL_H
 DECL|macro|_LINUX_EVENTPOLL_H
 mdefine_line|#define _LINUX_EVENTPOLL_H
-DECL|macro|EVENTPOLL_MINOR
-mdefine_line|#define EVENTPOLL_MINOR&t;124
-DECL|macro|POLLFD_X_PAGE
-mdefine_line|#define POLLFD_X_PAGE (PAGE_SIZE / sizeof(struct pollfd))
-DECL|macro|MAX_FDS_IN_EVENTPOLL
-mdefine_line|#define MAX_FDS_IN_EVENTPOLL (1024 * 128)
-DECL|macro|MAX_EVENTPOLL_PAGES
-mdefine_line|#define MAX_EVENTPOLL_PAGES (MAX_FDS_IN_EVENTPOLL / POLLFD_X_PAGE)
-DECL|macro|EVENT_PAGE_INDEX
-mdefine_line|#define EVENT_PAGE_INDEX(n) ((n) / POLLFD_X_PAGE)
-DECL|macro|EVENT_PAGE_REM
-mdefine_line|#define EVENT_PAGE_REM(n) ((n) % POLLFD_X_PAGE)
-DECL|macro|EVENT_PAGE_OFFSET
-mdefine_line|#define EVENT_PAGE_OFFSET(n) (((n) % POLLFD_X_PAGE) * sizeof(struct pollfd))
-DECL|macro|EP_FDS_PAGES
-mdefine_line|#define EP_FDS_PAGES(n) (((n) + POLLFD_X_PAGE - 1) / POLLFD_X_PAGE)
-DECL|macro|EP_MAP_SIZE
-mdefine_line|#define EP_MAP_SIZE(n) (EP_FDS_PAGES(n) * PAGE_SIZE * 2)
-DECL|struct|evpoll
+multiline_comment|/* Forward declarations to avoid compiler errors */
 r_struct
-id|evpoll
-(brace
-DECL|member|ep_timeout
-r_int
-id|ep_timeout
+id|file
 suffix:semicolon
-DECL|member|ep_resoff
-r_int
-r_int
-id|ep_resoff
+r_struct
+id|pollfd
 suffix:semicolon
-)brace
-suffix:semicolon
-DECL|macro|EP_ALLOC
-mdefine_line|#define EP_ALLOC _IOR(&squot;P&squot;, 1, int)
-DECL|macro|EP_POLL
-mdefine_line|#define EP_POLL _IOWR(&squot;P&squot;, 2, struct evpoll)
-DECL|macro|EP_FREE
-mdefine_line|#define EP_FREE _IO(&squot;P&squot;, 3)
-DECL|macro|EP_ISPOLLED
-mdefine_line|#define EP_ISPOLLED _IOWR(&squot;P&squot;, 4, struct pollfd)
+multiline_comment|/* Valid opcodes to issue to sys_epoll_ctl() */
 DECL|macro|EP_CTL_ADD
 mdefine_line|#define EP_CTL_ADD 1
 DECL|macro|EP_CTL_DEL
 mdefine_line|#define EP_CTL_DEL 2
 DECL|macro|EP_CTL_MOD
 mdefine_line|#define EP_CTL_MOD 3
+multiline_comment|/* Kernel space functions implementing the user space &quot;epoll&quot; API */
 id|asmlinkage
 r_int
 id|sys_epoll_create
 c_func
 (paren
 r_int
-id|maxfds
+id|size
 )paren
 suffix:semicolon
 id|asmlinkage
@@ -87,13 +55,25 @@ id|epfd
 comma
 r_struct
 id|pollfd
-r_const
-op_star
 op_star
 id|events
 comma
 r_int
+id|maxevents
+comma
+r_int
 id|timeout
+)paren
+suffix:semicolon
+multiline_comment|/* Used in fs/file_table.c:__fput() to unlink files from the eventpoll interface */
+r_void
+id|ep_notify_file_close
+c_func
+(paren
+r_struct
+id|file
+op_star
+id|file
 )paren
 suffix:semicolon
 macro_line|#endif
