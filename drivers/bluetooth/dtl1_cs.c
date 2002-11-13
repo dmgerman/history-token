86fc056d7c5db0c950b46d3535a4653cac156221
Marcel Holtmann <marcel@holtmann.org>
2002-11-13 18:32:43 +0100

    [Bluetooth] Add the needed call of init_timer()
    This patch adds the missing init_timer() call for the PCMCIA
    release function.

--- drivers/bluetooth/dtl1_cs.c
+++ drivers/bluetooth/dtl1_cs.c
@@ -2083,6 +2083,13 @@ id|link-&gt;priv
 op_assign
 id|info
 suffix:semicolon
+id|init_timer
+c_func
+(paren
+op_amp
+id|link-&gt;release
+)paren
+suffix:semicolon
 id|link-&gt;release.function
 op_assign
 op_amp
