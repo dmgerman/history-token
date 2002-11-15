ca438a302fc48715141d1f98f1b3fe0a5f4e8f98
Davide Libenzi <davidel@xmailserver.org>
2002-11-14 16:17:23 -0800

    [PATCH] epoll bits 0.46 ...
    - A more uniform poll queueing interface with tips from Manfred
    
    - The f_op->poll() is done outside the irqlock to maintain compatibility
    	with existing drivers that assume to be called with irq enabled
    
    - Moved event mask setting inside ep_modify() with tips from John
    
    - Fixed locking to fit the new "poll() outside the lock" approach
    
    - Bufferd userspace event delivery to reduce irq_lock/irq_unlock switching
    	rate and to reduce the number of __copy_to_user()
    
    - Comments added

--- kernel/ksyms.c
+++ kernel/ksyms.c
@@ -1400,11 +1400,11 @@ c_func
 id|__pollwait
 )paren
 suffix:semicolon
-DECL|variable|poll_freewait
+DECL|variable|__pollfreewait
 id|EXPORT_SYMBOL
 c_func
 (paren
-id|poll_freewait
+id|__pollfreewait
 )paren
 suffix:semicolon
 DECL|variable|ROOT_DEV
