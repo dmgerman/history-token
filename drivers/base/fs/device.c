40792084541179124f013bd125d7039872290c4d
Patrick Mochel <mochel@osdl.org>
2002-11-18 06:30:12 -0600

    driver model: catch one last #define DEBUG 0
    Convert it to #undef DEBUG.

--- drivers/base/fs/device.c
+++ drivers/base/fs/device.c
@@ -1,6 +1,6 @@
 multiline_comment|/*&n; * drivers/base/fs.c - driver model interface to driverfs &n; *&n; * Copyright (c) 2002 Patrick Mochel&n; *&t;&t; 2002 Open Source Development Lab&n; */
 DECL|macro|DEBUG
-mdefine_line|#define DEBUG 0
+macro_line|#undef DEBUG
 macro_line|#include &lt;linux/device.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/string.h&gt;
