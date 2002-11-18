2e40e547cb05087234867c5e8f82fd428d760edf
David S. Miller <davem@nuts.ninka.net>
2002-11-17 20:11:29 -0800

    [SPARC64]: Missing linux/interrupt.h includes.

--- arch/sparc64/kernel/pci_sabre.c
+++ arch/sparc64/kernel/pci_sabre.c
@@ -4,6 +4,7 @@ macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
+macro_line|#include &lt;linux/interrupt.h&gt;
 macro_line|#include &lt;asm/apb.h&gt;
 macro_line|#include &lt;asm/pbm.h&gt;
 macro_line|#include &lt;asm/iommu.h&gt;
