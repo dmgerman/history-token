886dbf0648294eb6d4a543359a82445d420aa86e
David S. Miller <davem@nuts.ninka.net>
2002-11-17 22:23:53 -0800

    [SCSI]: drivers/scsi/qlogicisp.c needs interrupt.h

--- drivers/scsi/qlogicisp.c
+++ drivers/scsi/qlogicisp.c
@@ -10,6 +10,7 @@ macro_line|#include &lt;linux/pci.h&gt;
 macro_line|#include &lt;linux/delay.h&gt;
 macro_line|#include &lt;linux/unistd.h&gt;
 macro_line|#include &lt;linux/spinlock.h&gt;
+macro_line|#include &lt;linux/interrupt.h&gt;
 macro_line|#include &lt;asm/io.h&gt;
 macro_line|#include &lt;asm/irq.h&gt;
 macro_line|#include &lt;asm/byteorder.h&gt;
