9137a6973a7021739ed6ed4b36fdde67e93d6f67
Jeff Dike <jdike@uml.karaya.com>
2002-11-21 03:15:09 -0500

    Added mode.h, mk_constants_kern.c, mk_constants_user.c, and um_mmu.h

---
+++ arch/um/include/mode.h
@@ -0,0 +1,14 @@
+multiline_comment|/* &n; * Copyright (C) 2002 Jeff Dike (jdike@karaya.com)&n; * Licensed under the GPL&n; */
+macro_line|#ifndef __MODE_H__
+DECL|macro|__MODE_H__
+mdefine_line|#define __MODE_H__
+macro_line|#include &quot;uml-config.h&quot;
+macro_line|#ifdef CONFIG_MODE_TT
+macro_line|#include &quot;../kernel/tt/include/mode.h&quot;
+macro_line|#endif
+macro_line|#ifdef CONFIG_MODE_SKAS
+macro_line|#include &quot;../kernel/skas/include/mode.h&quot;
+macro_line|#endif
+macro_line|#endif
+multiline_comment|/*&n; * Overrides for Emacs so that we follow Linus&squot;s tabbing style.&n; * Emacs will notice this stuff at the end of the file and automatically&n; * adjust the settings for this buffer only.  This must remain at the end&n; * of the file.&n; * ---------------------------------------------------------------------------&n; * Local variables:&n; * c-file-style: &quot;linux&quot;&n; * End:&n; */
+eof
---
+++ arch/um/include/um_mmu.h
@@ -0,0 +1,36 @@
+multiline_comment|/* &n; * Copyright (C) 2002 Jeff Dike (jdike@karaya.com)&n; * Licensed under the GPL&n; */
+macro_line|#ifndef __ARCH_UM_MMU_H
+DECL|macro|__ARCH_UM_MMU_H
+mdefine_line|#define __ARCH_UM_MMU_H
+macro_line|#include &quot;linux/config.h&quot;
+macro_line|#include &quot;choose-mode.h&quot;
+macro_line|#ifdef CONFIG_MODE_TT
+macro_line|#include &quot;../kernel/tt/include/mmu.h&quot;
+macro_line|#endif
+macro_line|#ifdef CONFIG_MODE_SKAS
+macro_line|#include &quot;../kernel/skas/include/mmu.h&quot;
+macro_line|#endif
+r_typedef
+r_union
+(brace
+macro_line|#ifdef CONFIG_MODE_TT
+DECL|member|tt
+r_struct
+id|mmu_context_tt
+id|tt
+suffix:semicolon
+macro_line|#endif
+macro_line|#ifdef CONFIG_MODE_SKAS
+DECL|member|skas
+r_struct
+id|mmu_context_skas
+id|skas
+suffix:semicolon
+macro_line|#endif
+DECL|typedef|mm_context_t
+)brace
+id|mm_context_t
+suffix:semicolon
+macro_line|#endif
+multiline_comment|/*&n; * Overrides for Emacs so that we follow Linus&squot;s tabbing style.&n; * Emacs will notice this stuff at the end of the file and automatically&n; * adjust the settings for this buffer only.  This must remain at the end&n; * of the file.&n; * ---------------------------------------------------------------------------&n; * Local variables:&n; * c-file-style: &quot;linux&quot;&n; * End:&n; */
+eof
