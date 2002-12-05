bd3961a97e350a5a50646b61a751441329b541c6
Patrick Mansfield <patmans@us.ibm.com>
2002-12-05 04:58:53 -0600

    [PATCH] current scsi-misc-2.5 scsi_lib.c needs init.h
    The current scsi-misc-2.5 scsi_lib.c needs to include init.h
    to get the __init and __exit defined.

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -5,6 +5,7 @@ macro_line|#include &lt;linux/completion.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/mempool.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
+macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &quot;scsi.h&quot;
 macro_line|#include &quot;hosts.h&quot;
 DECL|macro|SG_MEMPOOL_NR
