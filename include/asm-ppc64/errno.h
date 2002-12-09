45777cb9376f53b7668dc088fc01f40b8355abfe
Anton Blanchard <anton@samba.org>
2002-12-09 11:00:33 +1100

    ppc64: Add sys_restart_syscall, from ppc32

--- include/asm-ppc64/errno.h
+++ include/asm-ppc64/errno.h
@@ -7,16 +7,7 @@ DECL|macro|EDEADLOCK
 macro_line|#undef&t;EDEADLOCK
 DECL|macro|EDEADLOCK
 mdefine_line|#define&t;EDEADLOCK&t;58&t;/* File locking deadlock error */
-multiline_comment|/* Should never be seen by user programs */
-DECL|macro|ERESTARTSYS
-mdefine_line|#define ERESTARTSYS&t;512
-DECL|macro|ERESTARTNOINTR
-mdefine_line|#define ERESTARTNOINTR&t;513
-DECL|macro|ERESTARTNOHAND
-mdefine_line|#define ERESTARTNOHAND&t;514&t;/* restart if no handler.. */
-DECL|macro|ENOIOCTLCMD
-mdefine_line|#define ENOIOCTLCMD&t;515&t;/* No ioctl command */
 DECL|macro|_LAST_ERRNO
-mdefine_line|#define _LAST_ERRNO&t;515
+mdefine_line|#define _LAST_ERRNO&t;516
 macro_line|#endif
 eof
