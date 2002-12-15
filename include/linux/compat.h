26ee8b12112bf1c181f8f586b16b25fed7939ff4
Stephen Rothwell <sfr@canb.auug.org.au>
2002-12-14 20:16:06 -0800

    [PATCH] consolidate sys32_times - architecture independent
    This patch creates compat_sys_times and a few more compability types.

--- include/linux/compat.h
+++ include/linux/compat.h
@@ -1,10 +1,12 @@
 macro_line|#ifndef _LINUX_COMPAT_H
 DECL|macro|_LINUX_COMPAT_H
 mdefine_line|#define _LINUX_COMPAT_H
-multiline_comment|/*&n; * These are the type definitions for the arhitecure sepcific&n; * compatibility layer.&n; */
+multiline_comment|/*&n; * These are the type definitions for the architecture specific&n; * syscall compatibility layer.&n; */
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#ifdef CONFIG_COMPAT
 macro_line|#include &lt;asm/compat.h&gt;
+DECL|macro|compat_jiffies_to_clock_t
+mdefine_line|#define compat_jiffies_to_clock_t(x)&t;((x) / (HZ / COMPAT_USER_HZ))
 DECL|struct|compat_utimbuf
 r_struct
 id|compat_utimbuf
@@ -32,6 +34,28 @@ DECL|member|it_value
 r_struct
 id|compat_timeval
 id|it_value
+suffix:semicolon
+)brace
+suffix:semicolon
+DECL|struct|compat_tms
+r_struct
+id|compat_tms
+(brace
+DECL|member|tms_utime
+id|compat_clock_t
+id|tms_utime
+suffix:semicolon
+DECL|member|tms_stime
+id|compat_clock_t
+id|tms_stime
+suffix:semicolon
+DECL|member|tms_cutime
+id|compat_clock_t
+id|tms_cutime
+suffix:semicolon
+DECL|member|tms_cstime
+id|compat_clock_t
+id|tms_cstime
 suffix:semicolon
 )brace
 suffix:semicolon
