7b7cb7a8c0b2603d4b70b2eeffe41ea9325b86b4
Anton Blanchard <anton@samba.org>
2002-12-17 10:17:45 +1100

    ppc64: remove xics_isa_init

--- arch/ppc64/kernel/xics.h
+++ arch/ppc64/kernel/xics.h
@@ -1,17 +1,7 @@
-multiline_comment|/* &n; * arch/ppc/kernel/xics.h&n; *&n; * Copyright 2000 IBM Corporation.&n; *&n; *  This program is free software; you can redistribute it and/or&n; *  modify it under the terms of the GNU General Public License&n; *  as published by the Free Software Foundation; either version&n; *  2 of the License, or (at your option) any later version.&n; */
-macro_line|#ifndef _PPC_KERNEL_XICS_H
-DECL|macro|_PPC_KERNEL_XICS_H
-mdefine_line|#define _PPC_KERNEL_XICS_H
-r_extern
-r_struct
-id|hw_interrupt_type
-id|xics_pic
-suffix:semicolon
-r_extern
-r_struct
-id|hw_interrupt_type
-id|xics_8259_pic
-suffix:semicolon
+multiline_comment|/* &n; * arch/ppc64/kernel/xics.h&n; *&n; * Copyright 2000 IBM Corporation.&n; *&n; *  This program is free software; you can redistribute it and/or&n; *  modify it under the terms of the GNU General Public License&n; *  as published by the Free Software Foundation; either version&n; *  2 of the License, or (at your option) any later version.&n; */
+macro_line|#ifndef _PPC64_KERNEL_XICS_H
+DECL|macro|_PPC64_KERNEL_XICS_H
+mdefine_line|#define _PPC64_KERNEL_XICS_H
 r_void
 id|xics_init_IRQ
 c_func
@@ -28,12 +18,5 @@ id|pt_regs
 op_star
 )paren
 suffix:semicolon
-r_void
-id|xics_isa_init
-c_func
-(paren
-r_void
-)paren
-suffix:semicolon
-macro_line|#endif /* _PPC_KERNEL_XICS_H */
+macro_line|#endif /* _PPC64_KERNEL_XICS_H */
 eof
