a448d45e4f85c032a0b31b94644e1b336636433b
Jeff Dike <jdike@uml.karaya.com>
2003-01-17 04:54:10 -0500

    Fixed asm/modules.h to update UML to 2.5.59.

--- include/asm-um/module.h
+++ include/asm-um/module.h
@@ -1,6 +1,18 @@
 macro_line|#ifndef __UM_MODULE_H
 DECL|macro|__UM_MODULE_H
 mdefine_line|#define __UM_MODULE_H
-macro_line|#include &quot;asm/arch/module.h&quot;
+multiline_comment|/* UML is simple */
+DECL|struct|mod_arch_specific
+r_struct
+id|mod_arch_specific
+(brace
+)brace
+suffix:semicolon
+DECL|macro|Elf_Shdr
+mdefine_line|#define Elf_Shdr Elf32_Shdr
+DECL|macro|Elf_Sym
+mdefine_line|#define Elf_Sym Elf32_Sym
+DECL|macro|Elf_Ehdr
+mdefine_line|#define Elf_Ehdr Elf32_Ehdr
 macro_line|#endif
 eof
