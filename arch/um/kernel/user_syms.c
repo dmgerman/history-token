088223469984b562a84f1e52dd32d9eb5b364f77
Jeff Dike <jdike@uml.karaya.com>
2003-01-18 10:23:13 -0500

    Changed some CONFIG_* symbols to UML_CONFIG_*.

--- arch/um/kernel/user_syms.c
+++ arch/um/kernel/user_syms.c
@@ -41,7 +41,7 @@ DECL|macro|EXPORT_SYMBOL
 mdefine_line|#define EXPORT_SYMBOL(var)&t;   error config_must_be_included_before_module
 DECL|macro|EXPORT_SYMBOL_NOVERS
 mdefine_line|#define EXPORT_SYMBOL_NOVERS(var)  error config_must_be_included_before_module
-macro_line|#elif !defined(__CONFIG_MODULES__)
+macro_line|#elif !defined(UML_CONFIG_MODULES)
 DECL|macro|__EXPORT_SYMBOL
 mdefine_line|#define __EXPORT_SYMBOL(sym,str)
 DECL|macro|EXPORT_SYMBOL
@@ -51,7 +51,7 @@ mdefine_line|#define EXPORT_SYMBOL_NOVERS(var)
 macro_line|#else
 DECL|macro|__EXPORT_SYMBOL
 mdefine_line|#define __EXPORT_SYMBOL(sym, str)&t;&t;&t;&bslash;&n;const char __kstrtab_##sym[]&t;&t;&t;&t;&bslash;&n;__attribute__((section(&quot;.kstrtab&quot;))) = str;&t;&t;&bslash;&n;const struct module_symbol __ksymtab_##sym &t;&t;&bslash;&n;__attribute__((section(&quot;__ksymtab&quot;))) =&t;&t;&t;&bslash;&n;{ (unsigned long)&amp;sym, __kstrtab_##sym }
-macro_line|#if defined(__MODVERSIONS__) || !defined(__CONFIG_MODVERSIONS__)
+macro_line|#if defined(__MODVERSIONS__) || !defined(UML_CONFIG_MODVERSIONS)
 DECL|macro|EXPORT_SYMBOL
 mdefine_line|#define EXPORT_SYMBOL(var)  __EXPORT_SYMBOL(var, __MODULE_STRING(var))
 macro_line|#else
