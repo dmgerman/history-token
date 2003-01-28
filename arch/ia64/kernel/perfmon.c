88c3cc5dfea356075338da3ac1412f8e4b6c2421
Stéphane Eranian <eranian@hpl.hp.com>
2003-01-27 23:52:35 -0800

    [PATCH] ia64: fix return type of sys_perfmonctl()

--- arch/ia64/kernel/perfmon.c
+++ arch/ia64/kernel/perfmon.c
@@ -13434,6 +13434,14 @@ c_func
 (paren
 )paren
 suffix:semicolon
+multiline_comment|/*&n;&t; * propagate kernel psr in new context (used for first ctxsw in&n;&t; */
+id|nctx-&gt;ctx_saved_psr
+op_assign
+id|pfm_get_psr
+c_func
+(paren
+)paren
+suffix:semicolon
 multiline_comment|/* link with new task */
 id|thread-&gt;pfm_context
 op_assign
