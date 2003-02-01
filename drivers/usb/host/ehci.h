9e4f2fc506b9f53ea1a39b37539b827320ef64f8
David Brownell <david-b@pacbell.net>
2003-02-01 13:25:48 +1100

    [PATCH] USB: ehci-hcd updates
    This should apply to 2.5.59 too.  It seems to get rid of some pesky
    hangs, on at least some hardware, but I won't have time to test it
    on either VIA version ... maybe someone else will make the time?  :)
    
          New QH state prevents a re-activation race
    	- nobody can un-halt a qh before its cleanup is done
    	- resubmit-from-completion had this race (some usbtest cases)
    	  as could some normal submit paths on busy endpoints (storage)
    	- faster controllers would trip on this more consistently
    
          Queues of qtds
    	- work harder to avoid ever modifing any qh in software
    	- short reads block queue advance much less often
    	- be more cautious with large (>~19KB) unaligned buffers
    
          Unlinking urbs
    	- if qtd unlinked is at queue head, use its latest status
    	  (main effect is reporting bytes from partial transfers)
    	- another new qh state:  defer qh unlink if IAA is busy
    	  (eliminates a busy-wait loop in a rare scenario)
    
          Enable features to improve bus utilization
    	- PCI MWI ... can produce better write throughput; and by
    	  using right cacheline size, sometimes read throughput too
    	- USB NAK throttle ... sometimes reduces PCI access rates
    
          Other
    	- async dump shows more funky qh+qtd states, and NAK count
    	- cope with with some of the sprintf wierdness
    	- periodic dump is usually smaller (so is that schedule)
          	- minor cleanups

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -504,7 +504,6 @@ id|list_head
 id|qtd_list
 suffix:semicolon
 multiline_comment|/* sw qtd list */
-multiline_comment|/* dma same in urb&squot;s qtds, except 1st control qtd (setup buffer) */
 DECL|member|urb
 r_struct
 id|urb
@@ -528,6 +527,8 @@ l_int|32
 )paren
 )paren
 suffix:semicolon
+DECL|macro|QTD_MASK
+mdefine_line|#define QTD_MASK cpu_to_le32 (~0x1f)&t;/* mask NakCnt+T in qh-&gt;hw_alt_next */
 multiline_comment|/*-------------------------------------------------------------------------*/
 multiline_comment|/* type tag from {qh,itd,sitd,fstn}-&gt;hw_next */
 DECL|macro|Q_NEXT_TYPE
@@ -667,6 +668,13 @@ id|ehci_qtd
 op_star
 id|dummy
 suffix:semicolon
+DECL|member|reclaim
+r_struct
+id|ehci_qh
+op_star
+id|reclaim
+suffix:semicolon
+multiline_comment|/* next to reclaim */
 DECL|member|refcount
 id|atomic_t
 id|refcount
@@ -685,6 +693,10 @@ DECL|macro|QH_STATE_UNLINK
 mdefine_line|#define&t;QH_STATE_UNLINK&t;&t;2&t;&t;/* HC may still see this */
 DECL|macro|QH_STATE_IDLE
 mdefine_line|#define&t;QH_STATE_IDLE&t;&t;3&t;&t;/* HC doesn&squot;t see this */
+DECL|macro|QH_STATE_UNLINK_WAIT
+mdefine_line|#define&t;QH_STATE_UNLINK_WAIT&t;4&t;&t;/* LINKED and on reclaim q */
+DECL|macro|QH_STATE_COMPLETING
+mdefine_line|#define&t;QH_STATE_COMPLETING&t;5&t;&t;/* don&squot;t touch token.HALT */
 multiline_comment|/* periodic schedule info */
 DECL|member|usecs
 id|u8
