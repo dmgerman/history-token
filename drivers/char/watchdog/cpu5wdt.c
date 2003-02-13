fd92e1775cd8a875a024ae4f7089e0a1d111f75c
Dave Jones <davej@codemonkey.org.uk>
2003-02-13 11:54:46 -0100

    [WATCHDOG] Remove unneeded includes & EXPORT_NO_SYMBOLS from cpu5wdt

--- drivers/char/watchdog/cpu5wdt.c
+++ drivers/char/watchdog/cpu5wdt.c
@@ -1,10 +1,8 @@
 multiline_comment|/*&n; * sma cpu5 watchdog driver&n; *&n; * Copyright (C) 2003 Heiko Ronsdorf &lt;hero@ihg.uni-duisburg.de&gt;&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or&n; * (at your option) any later version.&n; *&n; * This program is distributed in the hope that it will be useful,&n; * but WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n; * GNU General Public License for more details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n; *&n; */
-macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/errno.h&gt;
 macro_line|#include &lt;linux/miscdevice.h&gt;
-macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/fs.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/ioport.h&gt;
@@ -1140,7 +1138,5 @@ id|ticks
 comma
 l_string|&quot;count down ticks, default is 10000&quot;
 )paren
-suffix:semicolon
-id|EXPORT_NO_SYMBOLS
 suffix:semicolon
 eof
