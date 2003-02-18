cfe46a0d5b459d3049c6f3f1b0ec3f7926dc2c95
Greg Ungerer <gerg@snapgear.com>
2003-02-18 05:47:55 -0800

    [PATCH] create an architecture specific flat header for m68knommu
    Add a per-architecture flat.h flat format executable header. The idea is
    to support the different options required in a clean way. For starters
    we need to be able to configure the initial stack layout, and this
    differs for different CPU types.
    
    This first patch adds the header for the m68knommu architecture.

---
+++ include/asm-m68knommu/flat.h
@@ -0,0 +1,8 @@
+multiline_comment|/*&n; * include/asm-m68knommu/flat.h -- uClinux flat-format executables&n; */
+macro_line|#ifndef __M68KNOMMU_FLAT_H__
+DECL|macro|__M68KNOMMU_FLAT_H__
+mdefine_line|#define __M68KNOMMU_FLAT_H__
+DECL|macro|flat_argvp_envp_on_stack
+mdefine_line|#define flat_argvp_envp_on_stack()&t;1
+macro_line|#endif /* __M68KNOMMU_FLAT_H__ */
+eof
