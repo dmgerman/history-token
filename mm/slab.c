0fbd641f8a23f27f99cbb357f494e928f74fc000
Andrew Morton <akpm@digeo.com>
2003-02-24 17:05:22 -0800

    [PATCH] Fix slab batchcount limiting code
    Spotted by Anton Blanchard: Our attempt to limit the interrupts-off time in
    slab wasn't very effective.  #ifndef DEBUG is never true.

--- mm/slab.c
+++ mm/slab.c
@@ -7580,7 +7580,7 @@ id|limit
 op_assign
 l_int|248
 suffix:semicolon
-macro_line|#ifndef DEBUG
+macro_line|#if DEBUG
 multiline_comment|/* With debugging enabled, large batchcount lead to excessively&n;&t; * long periods with disabled local interrupts. Limit the &n;&t; * batchcount&n;&t; */
 r_if
 c_cond
