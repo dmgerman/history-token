c45ccd3a9285ef74965d60f94a24708102dfefd6
Alan Cox <alan@lxorguk.ukuu.org.uk>
2003-03-06 01:41:57 -0800

    [PATCH] add a new PCI quirk type for the ALi Magik series
    This chip needs latency and other setups to avoid a PCI/AGP deadlock
    with tv cards

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -3247,6 +3247,8 @@ DECL|macro|PCIPCI_VIAETBF
 mdefine_line|#define PCIPCI_VIAETBF&t;&t;8
 DECL|macro|PCIPCI_VSFX
 mdefine_line|#define PCIPCI_VSFX&t;&t;16
+DECL|macro|PCIPCI_ALIMAGIK
+mdefine_line|#define PCIPCI_ALIMAGIK&t;&t;32
 macro_line|#endif /* __KERNEL__ */
 macro_line|#endif /* LINUX_PCI_H */
 eof
