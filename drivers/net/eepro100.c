55ceac4a30916fe4ca3f57b12537243d83173c11
Andrew Morton <akpm@digeo.com>
2003-03-08 03:28:30 -0800

    [PATCH] fix SMP lockup in eepro100 with ethtool on unused
    Patch from Jason Lunz <lunz@falooley.org>
    
    When support for the GSET and SSET ethtool ioctls was added to eepro100.c in
    2.4.20, the tx lock was overloaded to serialize their use.  Unfortunately,
    this lock is only initialized in dev->open(), causing ethtool to deadlock the
    machine when used on an unconfigured eepro100 interface.
    
    The fix is to initialize the spinlock at probe time.

--- drivers/net/eepro100.c
+++ drivers/net/eepro100.c
@@ -3537,6 +3537,13 @@ id|sp-&gt;timer
 )paren
 suffix:semicolon
 multiline_comment|/* used in ioctl() */
+id|spin_lock_init
+c_func
+(paren
+op_amp
+id|sp-&gt;lock
+)paren
+suffix:semicolon
 id|sp-&gt;mii_if.full_duplex
 op_assign
 id|option
@@ -4404,13 +4411,6 @@ suffix:semicolon
 id|sp-&gt;tx_full
 op_assign
 l_int|0
-suffix:semicolon
-id|spin_lock_init
-c_func
-(paren
-op_amp
-id|sp-&gt;lock
-)paren
 suffix:semicolon
 id|sp-&gt;in_interrupt
 op_assign
