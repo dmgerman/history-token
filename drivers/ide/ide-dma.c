bffbc3e81bc49e46a769756584ab30d9997e56ca
Alan Cox <alan@lxorguk.ukuu.org.uk>
2003-03-21 02:32:40 -0800

    [PATCH] update ide-dma support

--- drivers/ide/ide-dma.c
+++ drivers/ide/ide-dma.c
@@ -2,8 +2,6 @@ multiline_comment|/*&n; *  linux/drivers/ide/ide-dma.c&t;&t;Version 4.10&t;June 
 multiline_comment|/*&n; *  Special Thanks to Mark for his Six years of work.&n; *&n; *  Copyright (c) 1995-1998  Mark Lord&n; *  May be copied or modified under the terms of the GNU General Public License&n; */
 multiline_comment|/*&n; * This module provides support for the bus-master IDE DMA functions&n; * of various PCI chipsets, including the Intel PIIX (i82371FB for&n; * the 430 FX chipset), the PIIX3 (i82371SB for the 430 HX/VX and &n; * 440 chipsets), and the PIIX4 (i82371AB for the 430 TX chipset)&n; * (&quot;PIIX&quot; stands for &quot;PCI ISA IDE Xcellerator&quot;).&n; *&n; * Pretty much the same code works for other IDE PCI bus-mastering chipsets.&n; *&n; * DMA is supported for all IDE devices (disk drives, cdroms, tapes, floppies).&n; *&n; * By default, DMA support is prepared for use, but is currently enabled only&n; * for drives which already have DMA enabled (UltraDMA or mode 2 multi/single),&n; * or which are recognized as &quot;good&quot; (see table below).  Drives with only mode0&n; * or mode1 (multi/single) DMA should also work with this chipset/driver&n; * (eg. MC2112A) but are not enabled by default.&n; *&n; * Use &quot;hdparm -i&quot; to view modes supported by a given drive.&n; *&n; * The hdparm-3.5 (or later) utility can be used for manually enabling/disabling&n; * DMA support, but must be (re-)compiled against this kernel version or later.&n; *&n; * To enable DMA, use &quot;hdparm -d1 /dev/hd?&quot; on a per-drive basis after booting.&n; * If problems arise, ide.c will disable DMA operation after a few retries.&n; * This error recovery mechanism works and has been extremely well exercised.&n; *&n; * IDE drives, depending on their vintage, may support several different modes&n; * of DMA operation.  The boot-time modes are indicated with a &quot;*&quot; in&n; * the &quot;hdparm -i&quot; listing, and can be changed with *knowledgeable* use of&n; * the &quot;hdparm -X&quot; feature.  There is seldom a need to do this, as drives&n; * normally power-up with their &quot;best&quot; PIO/DMA modes enabled.&n; *&n; * Testing has been done with a rather extensive number of drives,&n; * with Quantum &amp; Western Digital models generally outperforming the pack,&n; * and Fujitsu &amp; Conner (and some Seagate which are really Conner) drives&n; * showing more lackluster throughput.&n; *&n; * Keep an eye on /var/adm/messages for &quot;DMA disabled&quot; messages.&n; *&n; * Some people have reported trouble with Intel Zappa motherboards.&n; * This can be fixed by upgrading the AMI BIOS to version 1.00.04.BS0,&n; * available from ftp://ftp.intel.com/pub/bios/10004bs0.exe&n; * (thanks to Glen Morrell &lt;glen@spin.Stanford.edu&gt; for researching this).&n; *&n; * Thanks to &quot;Christopher J. Reimer&quot; &lt;reimer@doe.carleton.ca&gt; for&n; * fixing the problem with the BIOS on some Acer motherboards.&n; *&n; * Thanks to &quot;Benoit Poulot-Cazajous&quot; &lt;poulot@chorus.fr&gt; for testing&n; * &quot;TX&quot; chipset compatibility and for providing patches for the &quot;TX&quot; chipset.&n; *&n; * Thanks to Christian Brunner &lt;chb@muc.de&gt; for taking a good first crack&n; * at generic DMA -- his patches were referred to when preparing this code.&n; *&n; * Most importantly, thanks to Robert Bringman &lt;rob@mars.trion.com&gt;&n; * for supplying a Promise UDMA board &amp; WD UDMA drive for this work!&n; *&n; * And, yes, Intel Zappa boards really *do* use both PIIX IDE ports.&n; *&n; * ATA-66/100 and recovery functions, I forgot the rest......&n; *&n; */
 macro_line|#include &lt;linux/config.h&gt;
-DECL|macro|__NO_VERSION__
-mdefine_line|#define __NO_VERSION__
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
@@ -1474,8 +1472,6 @@ suffix:semicolon
 r_if
 c_cond
 (paren
-id|id
-op_logical_and
 (paren
 id|id-&gt;capability
 op_amp
@@ -3815,25 +3811,7 @@ suffix:semicolon
 r_if
 c_cond
 (paren
-id|check_mem_region
-c_func
-(paren
-id|base
-comma
-id|ports
-)paren
-)paren
-(brace
-id|printk
-c_func
-(paren
-l_string|&quot; -- Error, MMIO ports already in use.&bslash;n&quot;
-)paren
-suffix:semicolon
-r_return
-l_int|1
-suffix:semicolon
-)brace
+op_logical_neg
 id|request_mem_region
 c_func
 (paren
@@ -3843,6 +3821,9 @@ id|ports
 comma
 id|hwif-&gt;name
 )paren
+)paren
+r_goto
+id|fail
 suffix:semicolon
 id|hwif-&gt;dma_base
 op_assign
@@ -3862,6 +3843,10 @@ l_int|0
 )paren
 )paren
 (brace
+r_if
+c_cond
+(paren
+op_logical_neg
 id|request_region
 c_func
 (paren
@@ -3873,6 +3858,9 @@ id|hwif-&gt;cds-&gt;extra
 comma
 id|hwif-&gt;cds-&gt;name
 )paren
+)paren
+r_goto
+id|release_mem
 suffix:semicolon
 id|hwif-&gt;dma_extra
 op_assign
@@ -3912,14 +3900,6 @@ r_if
 c_cond
 (paren
 op_logical_neg
-id|check_mem_region
-c_func
-(paren
-id|hwif-&gt;dma_base2
-comma
-id|ports
-)paren
-)paren
 id|request_mem_region
 c_func
 (paren
@@ -3929,10 +3909,46 @@ id|ports
 comma
 id|hwif-&gt;name
 )paren
+)paren
+r_goto
+id|release_io
 suffix:semicolon
 )brace
 r_return
 l_int|0
+suffix:semicolon
+id|release_mem
+suffix:colon
+id|release_mem_region
+c_func
+(paren
+id|base
+comma
+id|ports
+)paren
+suffix:semicolon
+id|release_io
+suffix:colon
+id|release_region
+c_func
+(paren
+id|base
+op_plus
+l_int|16
+comma
+id|hwif-&gt;cds-&gt;extra
+)paren
+suffix:semicolon
+id|fail
+suffix:colon
+id|printk
+c_func
+(paren
+l_string|&quot; -- Error, MMIO ports already in use.&bslash;n&quot;
+)paren
+suffix:semicolon
+r_return
+l_int|1
 suffix:semicolon
 )brace
 DECL|function|ide_iomio_dma
