23883e06b98201cff98ab16ad06cb0c4dc901dc0
Neil Brown <neilb@cse.unsw.edu.au>
2003-03-26 18:57:10 -0800

    [PATCH] kNFSd: READ_BUF macro update
    Fix some misleading comments, and correct the test for requesting more
    than one page.

--- fs/nfsd/nfs4xdr.c
+++ fs/nfsd/nfs4xdr.c
@@ -1011,6 +1011,7 @@ DECL|macro|SAVEMEM
 mdefine_line|#define SAVEMEM(x,nbytes) do {&t;&t;&t;&bslash;&n;&t;if (!(x = (p==argp-&gt;tmp || p == argp-&gt;tmpp) ? &bslash;&n; &t;&t;savemem(argp, p, nbytes) :&t;&bslash;&n; &t;&t;(char *)p)) {&t;&t;&t;&bslash;&n;&t;&t;printk(KERN_NOTICE &quot;xdr error! (%s:%d)&bslash;n&quot;, __FILE__, __LINE__); &bslash;&n;&t;&t;goto xdr_error;&t;&t;&t;&bslash;&n;&t;&t;}&t;&t;&t;&t;&bslash;&n;&t;p += XDR_QUADLEN(nbytes);&t;&t;&bslash;&n;} while (0)
 DECL|macro|COPYMEM
 mdefine_line|#define COPYMEM(x,nbytes) do {&t;&t;&t;&bslash;&n;&t;memcpy((x), p, nbytes);&t;&t;&t;&bslash;&n;&t;p += XDR_QUADLEN(nbytes);&t;&t;&bslash;&n;} while (0)
+multiline_comment|/* READ_BUF, read_buf(): nbytes must be &lt;= PAGE_SIZE */
 DECL|macro|READ_BUF
 mdefine_line|#define READ_BUF(nbytes)  do {&t;&t;&t;&bslash;&n;&t;if (nbytes &lt;= (u32)((char *)argp-&gt;end - (char *)argp-&gt;p)) {&t;&bslash;&n;&t;&t;p = argp-&gt;p;&t;&t;&t;&bslash;&n;&t;&t;argp-&gt;p += XDR_QUADLEN(nbytes);&t;&bslash;&n;&t;} else if (!(p = read_buf(argp, nbytes))) { &bslash;&n;&t;&t;printk(KERN_NOTICE &quot;xdr error! (%s:%d)&bslash;n&quot;, __FILE__, __LINE__); &bslash;&n;&t;&t;goto xdr_error;&t;&t;&t;&bslash;&n;&t;}&t;&t;&t;&t;&t;&bslash;&n;} while (0)
 DECL|function|read_buf
@@ -1028,7 +1029,7 @@ r_int
 id|nbytes
 )paren
 (brace
-multiline_comment|/* We want more bytes than seem to be available.&n;&t; * Maybe we need a new page, may wehave just run out&n;&t; */
+multiline_comment|/* We want more bytes than seem to be available.&n;&t; * Maybe we need a new page, maybe we have just run out&n;&t; */
 r_int
 id|avail
 op_assign
@@ -1066,14 +1067,14 @@ c_cond
 id|avail
 op_plus
 id|PAGE_SIZE
-OG
+OL
 id|nbytes
 )paren
 multiline_comment|/* need more than a page !! */
 r_return
 l_int|NULL
 suffix:semicolon
-multiline_comment|/* ok, we can do it with the tail plus the next page */
+multiline_comment|/* ok, we can do it with the current plus the next page */
 r_if
 c_cond
 (paren
