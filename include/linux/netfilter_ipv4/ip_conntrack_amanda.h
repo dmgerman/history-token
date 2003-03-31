e6d5949c5229f3ee3607d8f852eaa03ff911adfa
Brian J. Murrell <netfilter@interlinx.bc.ca>
2003-03-30 23:40:07 -0800

    [NETFILTER]: Add amanda conntrack + NAT support.

---
+++ include/linux/netfilter_ipv4/ip_conntrack_amanda.h
@@ -0,0 +1,55 @@
+macro_line|#ifndef _IP_CONNTRACK_AMANDA_H
+DECL|macro|_IP_CONNTRACK_AMANDA_H
+mdefine_line|#define _IP_CONNTRACK_AMANDA_H
+multiline_comment|/* AMANDA tracking. */
+macro_line|#ifdef __KERNEL__
+macro_line|#include &lt;linux/netfilter_ipv4/lockhelp.h&gt;
+multiline_comment|/* Protects amanda part of conntracks */
+DECL|variable|ip_amanda_lock
+id|DECLARE_LOCK_EXTERN
+c_func
+(paren
+id|ip_amanda_lock
+)paren
+suffix:semicolon
+macro_line|#endif
+DECL|struct|conn
+r_struct
+id|conn
+(brace
+DECL|member|match
+r_char
+op_star
+id|match
+suffix:semicolon
+DECL|member|matchlen
+r_int
+id|matchlen
+suffix:semicolon
+)brace
+suffix:semicolon
+DECL|macro|NUM_MSGS
+mdefine_line|#define NUM_MSGS &t;3
+DECL|struct|ip_ct_amanda_expect
+r_struct
+id|ip_ct_amanda_expect
+(brace
+DECL|member|port
+id|u_int16_t
+id|port
+suffix:semicolon
+multiline_comment|/* port number of this expectation */
+DECL|member|offset
+id|u_int16_t
+id|offset
+suffix:semicolon
+multiline_comment|/* offset of the port specification in ctrl packet */
+DECL|member|len
+id|u_int16_t
+id|len
+suffix:semicolon
+multiline_comment|/* the length of the port number specification */
+)brace
+suffix:semicolon
+macro_line|#endif /* _IP_CONNTRACK_AMANDA_H */
+eof
