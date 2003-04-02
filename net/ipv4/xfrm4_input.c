e208481fcfd79fbea84caae1e2d71b2d3088fc07
Jean-Francois Dive <jef@linuxbe.org>
2003-04-02 13:04:39 -0800

    [IPSEC]: Missing xfrm_state use_time updates.

--- net/ipv4/xfrm4_input.c
+++ net/ipv4/xfrm4_input.c
@@ -196,6 +196,14 @@ suffix:semicolon
 id|x-&gt;curlft.packets
 op_increment
 suffix:semicolon
+id|x-&gt;curlft.use_time
+op_assign
+(paren
+r_int
+r_int
+)paren
+id|xtime.tv_sec
+suffix:semicolon
 id|spin_unlock
 c_func
 (paren
