d4d9843ada18648aa9e20db7f5314373b5329e27
Andrew Morton <akpm@digeo.com>
2003-04-02 16:26:20 -0800

    [PATCH] handle bad inodes in put_inode
    From: "J. Bruce Fields" <bfields@fieldses.org>
    
    If the NFS daemon is presented with a filehandle for a file that has
    been deleted, it does an iget() in fs/exportfs/expfs.c:export_iget() and
    gets a bad inode back.  When it subsequently iput()s the inode, the
    result is:
    
    Mar 27 12:53:40 snoopy kernel: EXT2-fs error (device ide0(3,3)): ext2_free_blocks: Freeing blocks not in datazone - block = 1802201963, count = 27499
    Mar 27 12:53:40 snoopy kernel: Remounting filesystem read-only
    
    The same can happen if ext2_get_inode() returns an error - ext2_read_inode()
    will return an uninitialised inode and ext2_put_inode() is not allowed to go
    looking inside the bad inode.

--- fs/ext3/inode.c
+++ fs/ext3/inode.c
@@ -440,10 +440,11 @@ id|inode
 )paren
 suffix:semicolon
 )brace
-multiline_comment|/*&n; * Called at each iput()&n; */
+multiline_comment|/*&n; * Called at each iput()&n; *&n; * The inode may be &quot;bad&quot; if ext3_read_inode() saw an error from&n; * ext3_get_inode(), so we need to check that to avoid freeing random disk&n; * blocks.&n; */
 DECL|function|ext3_put_inode
 r_void
 id|ext3_put_inode
+c_func
 (paren
 r_struct
 id|inode
@@ -451,7 +452,18 @@ op_star
 id|inode
 )paren
 (brace
+r_if
+c_cond
+(paren
+op_logical_neg
+id|is_bad_inode
+c_func
+(paren
+id|inode
+)paren
+)paren
 id|ext3_discard_prealloc
+c_func
 (paren
 id|inode
 )paren
