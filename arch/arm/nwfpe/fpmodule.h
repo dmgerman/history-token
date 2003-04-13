3d562e66478beef9449c08de048c854849b6e252
Ian Molton <spyro@com.rmk.(none)>
2003-04-13 12:32:04 +0100

    [ARM PATCH] 1458/1: finish nwfpe CONFIG_CPUnn removal
    Patch from Ian Molton
    
    missed a bit. apply ontop of earlier patch.

--- arch/arm/nwfpe/fpmodule.h
+++ arch/arm/nwfpe/fpmodule.h
@@ -1,19 +1,11 @@
-multiline_comment|/*&n;    NetWinder Floating Point Emulator&n;    (c) Rebel.com, 1998-1999&n;&n;    Direct questions, comments to Scott Bambrough &lt;scottb@netwinder.org&gt;&n;&n;    This program is free software; you can redistribute it and/or modify&n;    it under the terms of the GNU General Public License as published by&n;    the Free Software Foundation; either version 2 of the License, or&n;    (at your option) any later version.&n;&n;    This program is distributed in the hope that it will be useful,&n;    but WITHOUT ANY WARRANTY; without even the implied warranty of&n;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n;    GNU General Public License for more details.&n;&n;    You should have received a copy of the GNU General Public License&n;    along with this program; if not, write to the Free Software&n;    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n;*/
+multiline_comment|/*&n;    NetWinder Floating Point Emulator&n;    (c) Rebel.com, 1998-1999&n;&n;    Direct questions, comments to Scott Bambrough &lt;scottb@netwinder.org&gt;&n;&n;    This program is free software; you can redistribute it and/or modify&n;    it under the terms of the GNU General Public License as published by&n;    the Free Software Foundation; either version 2 of the License, or&n;    (at your option) any later version.&n;&n;    This program is distributed in the hope that it will be useful,&n;    but WITHOUT ANY WARRANTY; without even the implied warranty of&n;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n;    GNU General Public License for more details.&n;&n;    You should have received a copy of the GNU General Public License&n;    along with this program; if not, write to the Free Software&n;    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n;&n;    27/03/03 Ian Molton Clean up CONFIG_CPU&n;*/
 macro_line|#ifndef __FPMODULE_H__
 DECL|macro|__FPMODULE_H__
 mdefine_line|#define __FPMODULE_H__
-macro_line|#include &lt;linux/config.h&gt;
-macro_line|#ifdef CONFIG_CPU_32
 DECL|macro|REG_ORIG_R0
 mdefine_line|#define REG_ORIG_R0&t;17
 DECL|macro|REG_CPSR
 mdefine_line|#define REG_CPSR&t;16
-macro_line|#else
-DECL|macro|REG_ORIG_R0
-mdefine_line|#define REG_ORIG_R0&t;16
-DECL|macro|REG_CPSR
-mdefine_line|#define REG_CPSR&t;15
-macro_line|#endif
 DECL|macro|REG_PC
 mdefine_line|#define REG_PC&t;&t;15
 DECL|macro|REG_LR
