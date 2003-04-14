1097d730202c1ede90b50af5824abc15f97120c8
David S. Miller <davem@nuts.ninka.net>
2003-04-14 01:57:37 -0700

    [NETFILTER IPV6]: Fix route leak in ip6_route_me_harder.

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -506,6 +506,12 @@ id|KERN_DEBUG
 l_string|&quot;ip6_route_me_harder: No more route.&bslash;n&quot;
 )paren
 suffix:semicolon
+id|dst_release
+c_func
+(paren
+id|dst
+)paren
+suffix:semicolon
 r_return
 op_minus
 id|EINVAL
