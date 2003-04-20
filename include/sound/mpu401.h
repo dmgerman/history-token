d0ae58a7550c0dbe673a95c4fb76fb202b0333c4
David S. Miller <davem@nuts.ninka.net>
2003-04-20 09:46:05 -0700

    [SOUND]: mpu401.h needs linux/interrupt.h

--- include/sound/mpu401.h
+++ include/sound/mpu401.h
@@ -2,6 +2,7 @@ macro_line|#ifndef __SOUND_MPU401_H
 DECL|macro|__SOUND_MPU401_H
 mdefine_line|#define __SOUND_MPU401_H
 multiline_comment|/*&n; *  Header file for MPU-401 and compatible cards&n; *  Copyright (c) by Jaroslav Kysela &lt;perex@suse.cz&gt;&n; *&n; *&n; *   This program is free software; you can redistribute it and/or modify&n; *   it under the terms of the GNU General Public License as published by&n; *   the Free Software Foundation; either version 2 of the License, or&n; *   (at your option) any later version.&n; *&n; *   This program is distributed in the hope that it will be useful,&n; *   but WITHOUT ANY WARRANTY; without even the implied warranty of&n; *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the&n; *   GNU General Public License for more details.&n; *&n; *   You should have received a copy of the GNU General Public License&n; *   along with this program; if not, write to the Free Software&n; *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA&n; *&n; */
+macro_line|#include &lt;linux/interrupt.h&gt;
 macro_line|#include &quot;rawmidi.h&quot;
 DECL|macro|MPU401_HW_MPU401
 mdefine_line|#define MPU401_HW_MPU401&t;&t;1&t;/* native MPU401 */
