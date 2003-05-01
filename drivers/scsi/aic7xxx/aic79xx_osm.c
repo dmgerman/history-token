d4ab718bd14211521301492fe5c02c7e2611c648
Justin T. Gibbs <gibbs@overdrive.btc.adaptec.com>
2003-04-30 23:00:31 -0600

    Aic79xx Driver Update (version 1.3.8)  o Correct a few BE processor bugs  o Print an additional diagnostic during recovery processing

--- drivers/scsi/aic7xxx/aic79xx_osm.c
+++ drivers/scsi/aic7xxx/aic79xx_osm.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * Adaptec AIC79xx device driver for Linux.&n; *&n; * $Id: //depot/aic7xxx/linux/drivers/scsi/aic7xxx/aic79xx_osm.c#157 $&n; *&n; * --------------------------------------------------------------------------&n; * Copyright (c) 1994-2000 Justin T. Gibbs.&n; * Copyright (c) 1997-1999 Doug Ledford&n; * Copyright (c) 2000-2003 Adaptec Inc.&n; * All rights reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that the following conditions&n; * are met:&n; * 1. Redistributions of source code must retain the above copyright&n; *    notice, this list of conditions, and the following disclaimer,&n; *    without modification.&n; * 2. Redistributions in binary form must reproduce at minimum a disclaimer&n; *    substantially similar to the &quot;NO WARRANTY&quot; disclaimer below&n; *    (&quot;Disclaimer&quot;) and any redistribution must be conditioned upon&n; *    including a substantially similar Disclaimer requirement for further&n; *    binary redistribution.&n; * 3. Neither the names of the above-listed copyright holders nor the names&n; *    of any contributors may be used to endorse or promote products derived&n; *    from this software without specific prior written permission.&n; *&n; * Alternatively, this software may be distributed under the terms of the&n; * GNU General Public License (&quot;GPL&quot;) version 2 as published by the Free&n; * Software Foundation.&n; *&n; * NO WARRANTY&n; * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS&n; * &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT&n; * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR&n; * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT&n; * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL&n; * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS&n; * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)&n; * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,&n; * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING&n; * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE&n; * POSSIBILITY OF SUCH DAMAGES.&n; */
+multiline_comment|/*&n; * Adaptec AIC79xx device driver for Linux.&n; *&n; * $Id: //depot/aic7xxx/linux/drivers/scsi/aic7xxx/aic79xx_osm.c#159 $&n; *&n; * --------------------------------------------------------------------------&n; * Copyright (c) 1994-2000 Justin T. Gibbs.&n; * Copyright (c) 1997-1999 Doug Ledford&n; * Copyright (c) 2000-2003 Adaptec Inc.&n; * All rights reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that the following conditions&n; * are met:&n; * 1. Redistributions of source code must retain the above copyright&n; *    notice, this list of conditions, and the following disclaimer,&n; *    without modification.&n; * 2. Redistributions in binary form must reproduce at minimum a disclaimer&n; *    substantially similar to the &quot;NO WARRANTY&quot; disclaimer below&n; *    (&quot;Disclaimer&quot;) and any redistribution must be conditioned upon&n; *    including a substantially similar Disclaimer requirement for further&n; *    binary redistribution.&n; * 3. Neither the names of the above-listed copyright holders nor the names&n; *    of any contributors may be used to endorse or promote products derived&n; *    from this software without specific prior written permission.&n; *&n; * Alternatively, this software may be distributed under the terms of the&n; * GNU General Public License (&quot;GPL&quot;) version 2 as published by the Free&n; * Software Foundation.&n; *&n; * NO WARRANTY&n; * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS&n; * &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT&n; * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR&n; * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT&n; * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL&n; * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS&n; * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)&n; * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,&n; * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING&n; * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE&n; * POSSIBILITY OF SUCH DAMAGES.&n; */
 macro_line|#include &quot;aic79xx_osm.h&quot;
 macro_line|#include &quot;aic79xx_inline.h&quot;
 macro_line|#include &lt;scsi/scsicam.h&gt;
@@ -4260,6 +4260,9 @@ r_int
 id|retval
 suffix:semicolon
 r_int
+id|was_paused
+suffix:semicolon
+r_int
 id|paused
 suffix:semicolon
 r_int
@@ -4571,6 +4574,14 @@ id|done
 suffix:semicolon
 )brace
 multiline_comment|/*&n;&t; * Ensure that the card doesn&squot;t do anything&n;&t; * behind our back.  Also make sure that we&n;&t; * didn&squot;t &quot;just&quot; miss an interrupt that would&n;&t; * affect this cmd.&n;&t; */
+id|was_paused
+op_assign
+id|ahd_is_paused
+c_func
+(paren
+id|ahd
+)paren
+suffix:semicolon
 id|ahd_pause_and_flushwork
 c_func
 (paren
@@ -4615,6 +4626,19 @@ r_goto
 id|no_cmd
 suffix:semicolon
 )brace
+id|printf
+c_func
+(paren
+l_string|&quot;%s: At time of recovery, card was %spaused&bslash;n&quot;
+comma
+id|was_paused
+ques
+c_cond
+l_string|&quot;&quot;
+suffix:colon
+l_string|&quot;not &quot;
+)paren
+suffix:semicolon
 id|ahd_dump_card_state
 c_func
 (paren
@@ -4641,7 +4665,11 @@ l_char|&squot;A&squot;
 comma
 id|cmd-&gt;device-&gt;lun
 comma
-id|pending_scb-&gt;hscb-&gt;tag
+id|SCB_GET_TAG
+c_func
+(paren
+id|pending_scb
+)paren
 comma
 id|ROLE_INITIATOR
 comma
@@ -4774,7 +4802,11 @@ id|last_phase
 op_ne
 id|P_BUSFREE
 op_logical_and
-id|pending_scb-&gt;hscb-&gt;tag
+id|SCB_GET_TAG
+c_func
+(paren
+id|pending_scb
+)paren
 op_eq
 id|active_scbptr
 )paren
@@ -4860,7 +4892,11 @@ c_func
 (paren
 id|ahd
 comma
-id|pending_scb-&gt;hscb-&gt;tag
+id|SCB_GET_TAG
+c_func
+(paren
+id|pending_scb
+)paren
 )paren
 suffix:semicolon
 id|pending_scb-&gt;hscb-&gt;cdb_len
@@ -17480,14 +17516,6 @@ id|ahd_linux_device
 op_star
 id|dev
 suffix:semicolon
-id|LIST_REMOVE
-c_func
-(paren
-id|scb
-comma
-id|pending_links
-)paren
-suffix:semicolon
 r_if
 c_cond
 (paren
@@ -17505,7 +17533,11 @@ c_func
 (paren
 l_string|&quot;SCB %d done&squot;d twice&bslash;n&quot;
 comma
-id|scb-&gt;hscb-&gt;tag
+id|SCB_GET_TAG
+c_func
+(paren
+id|scb
+)paren
 )paren
 suffix:semicolon
 id|ahd_dump_card_state
@@ -17521,6 +17553,14 @@ l_string|&quot;Stopping for safety&quot;
 )paren
 suffix:semicolon
 )brace
+id|LIST_REMOVE
+c_func
+(paren
+id|scb
+comma
+id|pending_links
+)paren
+suffix:semicolon
 id|cmd
 op_assign
 id|scb-&gt;io_ctx
