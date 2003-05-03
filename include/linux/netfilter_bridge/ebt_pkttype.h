790967c2f405604fe7411f0009662e8a78cd4c5e
Bart De Schuymer <bdschuym@pandora.be>
2003-05-03 16:13:24 -0700

    [EBTABLES]: Add ebtables match for the pkt_type member of an skbuff.

---
+++ include/linux/netfilter_bridge/ebt_pkttype.h
@@ -0,0 +1,21 @@
+macro_line|#ifndef __LINUX_BRIDGE_EBT_PKTTYPE_H
+DECL|macro|__LINUX_BRIDGE_EBT_PKTTYPE_H
+mdefine_line|#define __LINUX_BRIDGE_EBT_PKTTYPE_H
+DECL|struct|ebt_pkttype_info
+r_struct
+id|ebt_pkttype_info
+(brace
+DECL|member|pkt_type
+r_uint8
+id|pkt_type
+suffix:semicolon
+DECL|member|invert
+r_uint8
+id|invert
+suffix:semicolon
+)brace
+suffix:semicolon
+DECL|macro|EBT_PKTTYPE_MATCH
+mdefine_line|#define EBT_PKTTYPE_MATCH &quot;pkttype&quot;
+macro_line|#endif
+eof
