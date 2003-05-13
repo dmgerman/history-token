d3e4ff9da4f4e2f781bf262b189a8b2f50f68643
David Mosberger <davidm@tiger.hpl.hp.com>
2003-05-13 09:51:42 -0700

    ia64: Move ia64 ELF relocations to ia64-specific elf.h.

--- include/asm-ia64/elf.h
+++ include/asm-ia64/elf.h
@@ -25,6 +25,172 @@ mdefine_line|#define ELF_EXEC_PAGESIZE&t;PAGE_SIZE
 multiline_comment|/*&n; * This is the location that an ET_DYN program is loaded if exec&squot;ed.&n; * Typical use of this is to invoke &quot;./ld.so someprog&quot; to test out a&n; * new version of the loader.  We need to make sure that it is out of&n; * the way of the program that it will &quot;exec&quot;, and that there is&n; * sufficient room for the brk.&n; */
 DECL|macro|ELF_ET_DYN_BASE
 mdefine_line|#define ELF_ET_DYN_BASE&t;&t;(TASK_UNMAPPED_BASE + 0x800000000)
+multiline_comment|/* IA-64 relocations: */
+DECL|macro|R_IA64_NONE
+mdefine_line|#define R_IA64_NONE&t;&t;0x00&t;/* none */
+DECL|macro|R_IA64_IMM14
+mdefine_line|#define R_IA64_IMM14&t;&t;0x21&t;/* symbol + addend, add imm14 */
+DECL|macro|R_IA64_IMM22
+mdefine_line|#define R_IA64_IMM22&t;&t;0x22&t;/* symbol + addend, add imm22 */
+DECL|macro|R_IA64_IMM64
+mdefine_line|#define R_IA64_IMM64&t;&t;0x23&t;/* symbol + addend, mov imm64 */
+DECL|macro|R_IA64_DIR32MSB
+mdefine_line|#define R_IA64_DIR32MSB&t;&t;0x24&t;/* symbol + addend, data4 MSB */
+DECL|macro|R_IA64_DIR32LSB
+mdefine_line|#define R_IA64_DIR32LSB&t;&t;0x25&t;/* symbol + addend, data4 LSB */
+DECL|macro|R_IA64_DIR64MSB
+mdefine_line|#define R_IA64_DIR64MSB&t;&t;0x26&t;/* symbol + addend, data8 MSB */
+DECL|macro|R_IA64_DIR64LSB
+mdefine_line|#define R_IA64_DIR64LSB&t;&t;0x27&t;/* symbol + addend, data8 LSB */
+DECL|macro|R_IA64_GPREL22
+mdefine_line|#define R_IA64_GPREL22&t;&t;0x2a&t;/* @gprel(sym+add), add imm22 */
+DECL|macro|R_IA64_GPREL64I
+mdefine_line|#define R_IA64_GPREL64I&t;&t;0x2b&t;/* @gprel(sym+add), mov imm64 */
+DECL|macro|R_IA64_GPREL32MSB
+mdefine_line|#define R_IA64_GPREL32MSB&t;0x2c&t;/* @gprel(sym+add), data4 MSB */
+DECL|macro|R_IA64_GPREL32LSB
+mdefine_line|#define R_IA64_GPREL32LSB&t;0x2d&t;/* @gprel(sym+add), data4 LSB */
+DECL|macro|R_IA64_GPREL64MSB
+mdefine_line|#define R_IA64_GPREL64MSB&t;0x2e&t;/* @gprel(sym+add), data8 MSB */
+DECL|macro|R_IA64_GPREL64LSB
+mdefine_line|#define R_IA64_GPREL64LSB&t;0x2f&t;/* @gprel(sym+add), data8 LSB */
+DECL|macro|R_IA64_LTOFF22
+mdefine_line|#define R_IA64_LTOFF22&t;&t;0x32&t;/* @ltoff(sym+add), add imm22 */
+DECL|macro|R_IA64_LTOFF64I
+mdefine_line|#define R_IA64_LTOFF64I&t;&t;0x33&t;/* @ltoff(sym+add), mov imm64 */
+DECL|macro|R_IA64_PLTOFF22
+mdefine_line|#define R_IA64_PLTOFF22&t;&t;0x3a&t;/* @pltoff(sym+add), add imm22 */
+DECL|macro|R_IA64_PLTOFF64I
+mdefine_line|#define R_IA64_PLTOFF64I&t;0x3b&t;/* @pltoff(sym+add), mov imm64 */
+DECL|macro|R_IA64_PLTOFF64MSB
+mdefine_line|#define R_IA64_PLTOFF64MSB&t;0x3e&t;/* @pltoff(sym+add), data8 MSB */
+DECL|macro|R_IA64_PLTOFF64LSB
+mdefine_line|#define R_IA64_PLTOFF64LSB&t;0x3f&t;/* @pltoff(sym+add), data8 LSB */
+DECL|macro|R_IA64_FPTR64I
+mdefine_line|#define R_IA64_FPTR64I&t;&t;0x43&t;/* @fptr(sym+add), mov imm64 */
+DECL|macro|R_IA64_FPTR32MSB
+mdefine_line|#define R_IA64_FPTR32MSB&t;0x44&t;/* @fptr(sym+add), data4 MSB */
+DECL|macro|R_IA64_FPTR32LSB
+mdefine_line|#define R_IA64_FPTR32LSB&t;0x45&t;/* @fptr(sym+add), data4 LSB */
+DECL|macro|R_IA64_FPTR64MSB
+mdefine_line|#define R_IA64_FPTR64MSB&t;0x46&t;/* @fptr(sym+add), data8 MSB */
+DECL|macro|R_IA64_FPTR64LSB
+mdefine_line|#define R_IA64_FPTR64LSB&t;0x47&t;/* @fptr(sym+add), data8 LSB */
+DECL|macro|R_IA64_PCREL60B
+mdefine_line|#define R_IA64_PCREL60B&t;&t;0x48&t;/* @pcrel(sym+add), brl */
+DECL|macro|R_IA64_PCREL21B
+mdefine_line|#define R_IA64_PCREL21B&t;&t;0x49&t;/* @pcrel(sym+add), ptb, call */
+DECL|macro|R_IA64_PCREL21M
+mdefine_line|#define R_IA64_PCREL21M&t;&t;0x4a&t;/* @pcrel(sym+add), chk.s */
+DECL|macro|R_IA64_PCREL21F
+mdefine_line|#define R_IA64_PCREL21F&t;&t;0x4b&t;/* @pcrel(sym+add), fchkf */
+DECL|macro|R_IA64_PCREL32MSB
+mdefine_line|#define R_IA64_PCREL32MSB&t;0x4c&t;/* @pcrel(sym+add), data4 MSB */
+DECL|macro|R_IA64_PCREL32LSB
+mdefine_line|#define R_IA64_PCREL32LSB&t;0x4d&t;/* @pcrel(sym+add), data4 LSB */
+DECL|macro|R_IA64_PCREL64MSB
+mdefine_line|#define R_IA64_PCREL64MSB&t;0x4e&t;/* @pcrel(sym+add), data8 MSB */
+DECL|macro|R_IA64_PCREL64LSB
+mdefine_line|#define R_IA64_PCREL64LSB&t;0x4f&t;/* @pcrel(sym+add), data8 LSB */
+DECL|macro|R_IA64_LTOFF_FPTR22
+mdefine_line|#define R_IA64_LTOFF_FPTR22&t;0x52&t;/* @ltoff(@fptr(s+a)), imm22 */
+DECL|macro|R_IA64_LTOFF_FPTR64I
+mdefine_line|#define R_IA64_LTOFF_FPTR64I&t;0x53&t;/* @ltoff(@fptr(s+a)), imm64 */
+DECL|macro|R_IA64_LTOFF_FPTR32MSB
+mdefine_line|#define R_IA64_LTOFF_FPTR32MSB&t;0x54&t;/* @ltoff(@fptr(s+a)), 4 MSB */
+DECL|macro|R_IA64_LTOFF_FPTR32LSB
+mdefine_line|#define R_IA64_LTOFF_FPTR32LSB&t;0x55&t;/* @ltoff(@fptr(s+a)), 4 LSB */
+DECL|macro|R_IA64_LTOFF_FPTR64MSB
+mdefine_line|#define R_IA64_LTOFF_FPTR64MSB&t;0x56&t;/* @ltoff(@fptr(s+a)), 8 MSB */
+DECL|macro|R_IA64_LTOFF_FPTR64LSB
+mdefine_line|#define R_IA64_LTOFF_FPTR64LSB&t;0x57&t;/* @ltoff(@fptr(s+a)), 8 LSB */
+DECL|macro|R_IA64_SEGREL32MSB
+mdefine_line|#define R_IA64_SEGREL32MSB&t;0x5c&t;/* @segrel(sym+add), data4 MSB */
+DECL|macro|R_IA64_SEGREL32LSB
+mdefine_line|#define R_IA64_SEGREL32LSB&t;0x5d&t;/* @segrel(sym+add), data4 LSB */
+DECL|macro|R_IA64_SEGREL64MSB
+mdefine_line|#define R_IA64_SEGREL64MSB&t;0x5e&t;/* @segrel(sym+add), data8 MSB */
+DECL|macro|R_IA64_SEGREL64LSB
+mdefine_line|#define R_IA64_SEGREL64LSB&t;0x5f&t;/* @segrel(sym+add), data8 LSB */
+DECL|macro|R_IA64_SECREL32MSB
+mdefine_line|#define R_IA64_SECREL32MSB&t;0x64&t;/* @secrel(sym+add), data4 MSB */
+DECL|macro|R_IA64_SECREL32LSB
+mdefine_line|#define R_IA64_SECREL32LSB&t;0x65&t;/* @secrel(sym+add), data4 LSB */
+DECL|macro|R_IA64_SECREL64MSB
+mdefine_line|#define R_IA64_SECREL64MSB&t;0x66&t;/* @secrel(sym+add), data8 MSB */
+DECL|macro|R_IA64_SECREL64LSB
+mdefine_line|#define R_IA64_SECREL64LSB&t;0x67&t;/* @secrel(sym+add), data8 LSB */
+DECL|macro|R_IA64_REL32MSB
+mdefine_line|#define R_IA64_REL32MSB&t;&t;0x6c&t;/* data 4 + REL */
+DECL|macro|R_IA64_REL32LSB
+mdefine_line|#define R_IA64_REL32LSB&t;&t;0x6d&t;/* data 4 + REL */
+DECL|macro|R_IA64_REL64MSB
+mdefine_line|#define R_IA64_REL64MSB&t;&t;0x6e&t;/* data 8 + REL */
+DECL|macro|R_IA64_REL64LSB
+mdefine_line|#define R_IA64_REL64LSB&t;&t;0x6f&t;/* data 8 + REL */
+DECL|macro|R_IA64_LTV32MSB
+mdefine_line|#define R_IA64_LTV32MSB&t;&t;0x74&t;/* symbol + addend, data4 MSB */
+DECL|macro|R_IA64_LTV32LSB
+mdefine_line|#define R_IA64_LTV32LSB&t;&t;0x75&t;/* symbol + addend, data4 LSB */
+DECL|macro|R_IA64_LTV64MSB
+mdefine_line|#define R_IA64_LTV64MSB&t;&t;0x76&t;/* symbol + addend, data8 MSB */
+DECL|macro|R_IA64_LTV64LSB
+mdefine_line|#define R_IA64_LTV64LSB&t;&t;0x77&t;/* symbol + addend, data8 LSB */
+DECL|macro|R_IA64_PCREL21BI
+mdefine_line|#define R_IA64_PCREL21BI&t;0x79&t;/* @pcrel(sym+add), ptb, call */
+DECL|macro|R_IA64_PCREL22
+mdefine_line|#define R_IA64_PCREL22&t;&t;0x7a&t;/* @pcrel(sym+add), imm22 */
+DECL|macro|R_IA64_PCREL64I
+mdefine_line|#define R_IA64_PCREL64I&t;&t;0x7b&t;/* @pcrel(sym+add), imm64 */
+DECL|macro|R_IA64_IPLTMSB
+mdefine_line|#define R_IA64_IPLTMSB&t;&t;0x80&t;/* dynamic reloc, imported PLT, MSB */
+DECL|macro|R_IA64_IPLTLSB
+mdefine_line|#define R_IA64_IPLTLSB&t;&t;0x81&t;/* dynamic reloc, imported PLT, LSB */
+DECL|macro|R_IA64_COPY
+mdefine_line|#define R_IA64_COPY&t;&t;0x84&t;/* dynamic reloc, data copy */
+DECL|macro|R_IA64_SUB
+mdefine_line|#define R_IA64_SUB&t;&t;0x85&t;/* -symbol + addend, add imm22 */
+DECL|macro|R_IA64_LTOFF22X
+mdefine_line|#define R_IA64_LTOFF22X&t;&t;0x86&t;/* LTOFF22, relaxable.  */
+DECL|macro|R_IA64_LDXMOV
+mdefine_line|#define R_IA64_LDXMOV&t;&t;0x87&t;/* Use of LTOFF22X.  */
+DECL|macro|R_IA64_TPREL14
+mdefine_line|#define R_IA64_TPREL14&t;&t;0x91&t;/* @tprel(sym+add), add imm14 */
+DECL|macro|R_IA64_TPREL22
+mdefine_line|#define R_IA64_TPREL22&t;&t;0x92&t;/* @tprel(sym+add), add imm22 */
+DECL|macro|R_IA64_TPREL64I
+mdefine_line|#define R_IA64_TPREL64I&t;&t;0x93&t;/* @tprel(sym+add), add imm64 */
+DECL|macro|R_IA64_TPREL64MSB
+mdefine_line|#define R_IA64_TPREL64MSB&t;0x96&t;/* @tprel(sym+add), data8 MSB */
+DECL|macro|R_IA64_TPREL64LSB
+mdefine_line|#define R_IA64_TPREL64LSB&t;0x97&t;/* @tprel(sym+add), data8 LSB */
+DECL|macro|R_IA64_LTOFF_TPREL22
+mdefine_line|#define R_IA64_LTOFF_TPREL22&t;0x9a&t;/* @ltoff(@tprel(s+a)), add imm22 */
+DECL|macro|R_IA64_DTPMOD64MSB
+mdefine_line|#define R_IA64_DTPMOD64MSB&t;0xa6&t;/* @dtpmod(sym+add), data8 MSB */
+DECL|macro|R_IA64_DTPMOD64LSB
+mdefine_line|#define R_IA64_DTPMOD64LSB&t;0xa7&t;/* @dtpmod(sym+add), data8 LSB */
+DECL|macro|R_IA64_LTOFF_DTPMOD22
+mdefine_line|#define R_IA64_LTOFF_DTPMOD22&t;0xaa&t;/* @ltoff(@dtpmod(s+a)), imm22 */
+DECL|macro|R_IA64_DTPREL14
+mdefine_line|#define R_IA64_DTPREL14&t;&t;0xb1&t;/* @dtprel(sym+add), imm14 */
+DECL|macro|R_IA64_DTPREL22
+mdefine_line|#define R_IA64_DTPREL22&t;&t;0xb2&t;/* @dtprel(sym+add), imm22 */
+DECL|macro|R_IA64_DTPREL64I
+mdefine_line|#define R_IA64_DTPREL64I&t;0xb3&t;/* @dtprel(sym+add), imm64 */
+DECL|macro|R_IA64_DTPREL32MSB
+mdefine_line|#define R_IA64_DTPREL32MSB&t;0xb4&t;/* @dtprel(sym+add), data4 MSB */
+DECL|macro|R_IA64_DTPREL32LSB
+mdefine_line|#define R_IA64_DTPREL32LSB&t;0xb5&t;/* @dtprel(sym+add), data4 LSB */
+DECL|macro|R_IA64_DTPREL64MSB
+mdefine_line|#define R_IA64_DTPREL64MSB&t;0xb6&t;/* @dtprel(sym+add), data8 MSB */
+DECL|macro|R_IA64_DTPREL64LSB
+mdefine_line|#define R_IA64_DTPREL64LSB&t;0xb7&t;/* @dtprel(sym+add), data8 LSB */
+DECL|macro|R_IA64_LTOFF_DTPREL22
+mdefine_line|#define R_IA64_LTOFF_DTPREL22&t;0xba&t;/* @ltoff(@dtprel(s+a)), imm22 */
+multiline_comment|/* IA-64 specific section flags: */
+DECL|macro|SHF_IA_64_SHORT
+mdefine_line|#define SHF_IA_64_SHORT&t;&t;0x10000000&t;/* section near gp */
 multiline_comment|/*&n; * We use (abuse?) this macro to insert the (empty) vm_area that is&n; * used to map the register backing store.  I don&squot;t see any better&n; * place to do this, but we should discuss this with Linus once we can&n; * talk to him...&n; */
 r_extern
 r_void
