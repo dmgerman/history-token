c883d6375378a1dd9eacbfae5c549baa8bd77f4c
Justin T. Gibbs <gibbs@overdrive.btc.adaptec.com>
2003-05-22 23:58:40 -0600

    Aic7xxx and Aic79xx Driver Updates  o Fix style nits.

--- drivers/scsi/aic7xxx/aic7xxx_core.c
+++ drivers/scsi/aic7xxx/aic7xxx_core.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * Core routines and tables shareable across OS platforms.&n; *&n; * Copyright (c) 1994-2002 Justin T. Gibbs.&n; * Copyright (c) 2000-2002 Adaptec Inc.&n; * All rights reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that the following conditions&n; * are met:&n; * 1. Redistributions of source code must retain the above copyright&n; *    notice, this list of conditions, and the following disclaimer,&n; *    without modification.&n; * 2. Redistributions in binary form must reproduce at minimum a disclaimer&n; *    substantially similar to the &quot;NO WARRANTY&quot; disclaimer below&n; *    (&quot;Disclaimer&quot;) and any redistribution must be conditioned upon&n; *    including a substantially similar Disclaimer requirement for further&n; *    binary redistribution.&n; * 3. Neither the names of the above-listed copyright holders nor the names&n; *    of any contributors may be used to endorse or promote products derived&n; *    from this software without specific prior written permission.&n; *&n; * Alternatively, this software may be distributed under the terms of the&n; * GNU General Public License (&quot;GPL&quot;) version 2 as published by the Free&n; * Software Foundation.&n; *&n; * NO WARRANTY&n; * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS&n; * &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT&n; * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR&n; * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT&n; * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL&n; * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS&n; * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)&n; * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,&n; * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING&n; * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE&n; * POSSIBILITY OF SUCH DAMAGES.&n; *&n; * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.c#130 $&n; *&n; * $FreeBSD$&n; */
+multiline_comment|/*&n; * Core routines and tables shareable across OS platforms.&n; *&n; * Copyright (c) 1994-2002 Justin T. Gibbs.&n; * Copyright (c) 2000-2002 Adaptec Inc.&n; * All rights reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that the following conditions&n; * are met:&n; * 1. Redistributions of source code must retain the above copyright&n; *    notice, this list of conditions, and the following disclaimer,&n; *    without modification.&n; * 2. Redistributions in binary form must reproduce at minimum a disclaimer&n; *    substantially similar to the &quot;NO WARRANTY&quot; disclaimer below&n; *    (&quot;Disclaimer&quot;) and any redistribution must be conditioned upon&n; *    including a substantially similar Disclaimer requirement for further&n; *    binary redistribution.&n; * 3. Neither the names of the above-listed copyright holders nor the names&n; *    of any contributors may be used to endorse or promote products derived&n; *    from this software without specific prior written permission.&n; *&n; * Alternatively, this software may be distributed under the terms of the&n; * GNU General Public License (&quot;GPL&quot;) version 2 as published by the Free&n; * Software Foundation.&n; *&n; * NO WARRANTY&n; * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS&n; * &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT&n; * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR&n; * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT&n; * HOLDERS OR CONTRIBUTORS BE LIABLE FOR SPECIAL, EXEMPLARY, OR CONSEQUENTIAL&n; * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS&n; * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)&n; * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,&n; * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING&n; * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE&n; * POSSIBILITY OF SUCH DAMAGES.&n; *&n; * $Id: //depot/aic7xxx/aic7xxx/aic7xxx.c#131 $&n; *&n; * $FreeBSD$&n; */
 macro_line|#ifdef __linux__
 macro_line|#include &quot;aic7xxx_osm.h&quot;
 macro_line|#include &quot;aic7xxx_inline.h&quot;
@@ -830,7 +830,7 @@ r_int
 id|verbose_level
 )paren
 suffix:semicolon
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 r_static
 r_void
 id|ahc_setup_target_msgin
@@ -2984,7 +2984,7 @@ l_int|0
 suffix:semicolon
 )brace
 )brace
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 r_else
 (brace
 r_if
@@ -16401,7 +16401,7 @@ op_ne
 l_int|NULL
 )paren
 (brace
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 r_int
 id|j
 suffix:semicolon
@@ -16467,7 +16467,7 @@ id|M_DEVBUF
 suffix:semicolon
 )brace
 )brace
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 r_if
 c_cond
 (paren
@@ -21628,7 +21628,7 @@ id|EBUSY
 )paren
 suffix:semicolon
 )brace
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 multiline_comment|/*&n;&t; * XXX What about ATIOs that have not yet been serviced?&n;&t; * Perhaps we should just refuse to be suspended if we&n;&t; * are acting in a target role.&n;&t; */
 r_if
 c_cond
@@ -22156,7 +22156,7 @@ op_ne
 l_int|0
 )paren
 (brace
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 r_int
 id|group
 suffix:semicolon
@@ -25262,7 +25262,7 @@ c_func
 id|ahc
 )paren
 suffix:semicolon
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 multiline_comment|/*&n;&t; * XXX - In Twin mode, the tqinfifo may have commands&n;&t; *&t; for an unaffected channel in it.  However, if&n;&t; *&t; we have run out of ATIO resources to drain that&n;&t; *&t; queue, we may not get them all out here.  Further,&n;&t; *&t; the blocked transactions for the reset channel&n;&t; *&t; should just be killed off, irrespecitve of whether&n;&t; *&t; we are blocked on ATIO resources.  Write a routine&n;&t; *&t; to compact the tqinfifo appropriately.&n;&t; */
 r_if
 c_cond
@@ -25375,7 +25375,7 @@ op_or
 id|ENSCSIRST
 )paren
 suffix:semicolon
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 multiline_comment|/*&n;&t;&t; * Bus resets clear ENSELI, so we cannot&n;&t;&t; * defer re-enabling bus reset interrupts&n;&t;&t; * if we are in target mode.&n;&t;&t; */
 r_if
 c_cond
@@ -25473,7 +25473,7 @@ op_or
 id|ENSCSIRST
 )paren
 suffix:semicolon
-macro_line|#if AHC_TARGET_MODE
+macro_line|#ifdef AHC_TARGET_MODE
 multiline_comment|/*&n;&t;&t; * Bus resets clear ENSELI, so we cannot&n;&t;&t; * defer re-enabling bus reset interrupts&n;&t;&t; * if we are in target mode.&n;&t;&t; */
 r_if
 c_cond
