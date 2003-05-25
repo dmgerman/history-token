c0460df553c26e750b7abf788380b0080f1969a5
Andrew Morton <akpm@digeo.com>
2003-05-25 01:08:22 -0700

    [PATCH] ppc64: PPC64 irq return fix
    PPC64 irq return fix

--- arch/ppc64/kernel/xics.c
+++ arch/ppc64/kernel/xics.c
@@ -1214,11 +1214,6 @@ c_func
 (paren
 )paren
 suffix:semicolon
-r_int
-id|handled
-op_assign
-l_int|0
-suffix:semicolon
 id|ops
 op_member_access_from_pointer
 id|qirr_info
@@ -1240,10 +1235,6 @@ dot
 id|value
 )paren
 (brace
-id|handled
-op_assign
-l_int|1
-suffix:semicolon
 r_if
 c_cond
 (paren
@@ -1378,11 +1369,7 @@ suffix:semicolon
 macro_line|#endif
 )brace
 r_return
-id|IRQ_RETVAL
-c_func
-(paren
-id|handled
-)paren
+id|IRQ_HANDLED
 suffix:semicolon
 )brace
 DECL|function|xics_cause_IPI
