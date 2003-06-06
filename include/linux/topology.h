9dcf27d649ec279499e8f0427408ccd3dcd4d6bc
Andrew Morton <akpm@digeo.com>
2003-06-05 18:55:28 -0700

    [PATCH] Don't let processes be scheduled on CPU-less nodes (3/3)
    From: Matthew Dobson <colpatch@us.ibm.com>
    
    This patch implements a generic version of the nr_cpus_node(node) macro
    implemented for ppc64 by the previous patch.
    
    The generic version simply computes an hweight of the bitmask returned by
    node_to_cpumask(node) topology macro.
    
    This patch also adds a generic_hweight64() function and an hweight_long()
    function which are used as helpers for the generic nr_cpus_node() macro.
    
    This patch also adds a for_each_node_with_cpus() macro, which is used in
    sched_best_cpu() in kernel/sched.c to fix the original problem of
    scheduling processes on CPU-less nodes.  This macro should also be used in
    the future to avoid similar problems.
    
    Test compiled and booted by Andrew Theurer (habanero@us.ibm.com) on both
    x440 and ppc64.

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -2,6 +2,49 @@ multiline_comment|/*&n; * include/linux/topology.h&n; *&n; * Written by: Matthew
 macro_line|#ifndef _LINUX_TOPOLOGY_H
 DECL|macro|_LINUX_TOPOLOGY_H
 mdefine_line|#define _LINUX_TOPOLOGY_H
+macro_line|#include &lt;linux/bitops.h&gt;
+macro_line|#include &lt;linux/mmzone.h&gt;
+macro_line|#include &lt;linux/smp.h&gt;
 macro_line|#include &lt;asm/topology.h&gt;
+macro_line|#ifndef nr_cpus_node
+DECL|macro|nr_cpus_node
+mdefine_line|#define nr_cpus_node(node)&t;(hweight_long(node_to_cpumask(node)))
+macro_line|#endif
+DECL|function|__next_node_with_cpus
+r_static
+r_inline
+r_int
+id|__next_node_with_cpus
+c_func
+(paren
+r_int
+id|node
+)paren
+(brace
+r_do
+op_increment
+id|node
+suffix:semicolon
+r_while
+c_loop
+(paren
+id|node
+OL
+id|numnodes
+op_logical_and
+op_logical_neg
+id|nr_cpus_node
+c_func
+(paren
+id|node
+)paren
+)paren
+suffix:semicolon
+r_return
+id|node
+suffix:semicolon
+)brace
+DECL|macro|for_each_node_with_cpus
+mdefine_line|#define for_each_node_with_cpus(node) &bslash;&n;&t;for (node = 0; node &lt; numnodes; node = __next_node_with_cpus(node))
 macro_line|#endif /* _LINUX_TOPOLOGY_H */
 eof
