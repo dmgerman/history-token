efb161c28d1c25188e332e41162a5524d75e315f
Patrick Mochel <mochel@osdl.org>
2003-06-09 22:03:25 -0700

    [i8259] Use sysdev.h instead of device.h

--- arch/i386/kernel/i8259.c
+++ arch/i386/kernel/i8259.c
@@ -10,7 +10,7 @@ macro_line|#include &lt;linux/random.h&gt;
 macro_line|#include &lt;linux/smp_lock.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/kernel_stat.h&gt;
-macro_line|#include &lt;linux/device.h&gt;
+macro_line|#include &lt;linux/sysdev.h&gt;
 macro_line|#include &lt;asm/atomic.h&gt;
 macro_line|#include &lt;asm/system.h&gt;
 macro_line|#include &lt;asm/io.h&gt;
