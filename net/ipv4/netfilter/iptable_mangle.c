67ef1e9f111ded4dfd2940e4154d8f2798bd32f7
Rusty Russell <rusty@rustcorp.com.au>
2003-06-11 09:35:24 -0700

    [NETFILTER]: Delete unnecessary skb_linearize() calls in iptables_{filter,mangle}.c

--- net/ipv4/netfilter/iptable_mangle.c
+++ net/ipv4/netfilter/iptable_mangle.c
@@ -943,31 +943,6 @@ r_int
 r_int
 id|nfmark
 suffix:semicolon
-multiline_comment|/* FIXME: Push down to extensions --RR */
-r_if
-c_cond
-(paren
-id|skb_is_nonlinear
-c_func
-(paren
-op_star
-id|pskb
-)paren
-op_logical_and
-id|skb_linearize
-c_func
-(paren
-op_star
-id|pskb
-comma
-id|GFP_ATOMIC
-)paren
-op_ne
-l_int|0
-)paren
-r_return
-id|NF_DROP
-suffix:semicolon
 multiline_comment|/* root is playing with raw sockets. */
 r_if
 c_cond
