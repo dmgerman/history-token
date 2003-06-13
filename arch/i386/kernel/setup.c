46ce20fe118b84a76f3a10af858f6da7c5eb1809
Andy Grover <agrover@groveronline.com>
2003-06-12 22:58:30 -0700

    ACPI: Re-add acpitable.c and acpismp=force. This improves backwards compatibility and also cleans up the code to a significant degree.

--- arch/i386/kernel/setup.c
+++ arch/i386/kernel/setup.c
@@ -109,6 +109,15 @@ c_func
 id|mmu_cr4_features
 )paren
 suffix:semicolon
+macro_line|#ifdef CONFIG_ACPI_HT_ONLY
+DECL|variable|__initdata
+r_int
+id|acpi_disabled
+id|__initdata
+op_assign
+l_int|1
+suffix:semicolon
+macro_line|#else
 DECL|variable|__initdata
 r_int
 id|acpi_disabled
@@ -116,6 +125,7 @@ id|__initdata
 op_assign
 l_int|0
 suffix:semicolon
+macro_line|#endif
 DECL|variable|MCA_bus
 r_int
 id|MCA_bus
@@ -2035,6 +2045,26 @@ l_int|8
 id|acpi_disabled
 op_assign
 l_int|1
+suffix:semicolon
+multiline_comment|/* &quot;acpismp=force&quot; turns on ACPI again */
+r_else
+r_if
+c_cond
+(paren
+op_logical_neg
+id|memcmp
+c_func
+(paren
+id|from
+comma
+l_string|&quot;acpismp=force&quot;
+comma
+l_int|14
+)paren
+)paren
+id|acpi_disabled
+op_assign
+l_int|0
 suffix:semicolon
 multiline_comment|/*&n;&t;&t; * highmem=size forces highmem to be exactly &squot;size&squot; bytes.&n;&t;&t; * This works even on boxes that have no highmem otherwise.&n;&t;&t; * This also works to reduce highmem size on bigger boxes.&n;&t;&t; */
 r_if
