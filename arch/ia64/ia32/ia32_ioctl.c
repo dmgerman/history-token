05f3678244f56d44222d4c7bf87c0139a75afdc4
David Mosberger <davidm@tiger.hpl.hp.com>
2003-06-17 10:37:36 -0700

    ia64: Initial sync with 2.5.72.

--- arch/ia64/ia32/ia32_ioctl.c
+++ arch/ia64/ia32/ia32_ioctl.c
@@ -53,6 +53,7 @@ macro_line|#include &lt;linux/reiserfs_fs.h&gt;
 macro_line|#include &lt;linux/if_tun.h&gt;
 macro_line|#include &lt;linux/dirent.h&gt;
 macro_line|#include &lt;linux/ctype.h&gt;
+macro_line|#include &lt;linux/ncp_fs.h&gt;
 macro_line|#include &lt;net/bluetooth/bluetooth.h&gt;
 macro_line|#include &lt;net/bluetooth/rfcomm.h&gt;
 macro_line|#include &lt;scsi/scsi.h&gt;
