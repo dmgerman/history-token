95ccf8a092b9571731ed0b3a37458e4c6dc3b71e
David Mosberger <davidm@tiger.hpl.hp.com>
2003-06-23 03:00:04 -0700

    Drop pcibios_update_resource() #warning.

--- arch/ia64/pci/pci.c
+++ arch/ia64/pci/pci.c
@@ -1576,7 +1576,6 @@ suffix:semicolon
 r_return
 suffix:semicolon
 )brace
-macro_line|#warning pcibios_update_resource() is now a generic implementation - please check
 r_void
 id|__devinit
 DECL|function|pcibios_update_irq
