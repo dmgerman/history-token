79d9c4469d92b478520555cb1b03140fdd686dc3
Rob Radez <rob@osinvestor.com>
2003-06-25 09:42:14 -0700

    [SPARC]: Do not use __builtin_trap() on sparc until gcc is fixed.

--- include/asm-sparc/bug.h
+++ include/asm-sparc/bug.h
@@ -2,6 +2,9 @@ multiline_comment|/* $Id$ */
 macro_line|#ifndef _SPARC_BUG_H
 DECL|macro|_SPARC_BUG_H
 mdefine_line|#define _SPARC_BUG_H
+multiline_comment|/* Only use the inline asm until a gcc release that can handle __builtin_trap&n; * -rob 2003-06-25 */
+DECL|macro|__bug_trap
+mdefine_line|#define __bug_trap()&t;&t;&t;&t;&t;&bslash;&n;&t;__asm__ __volatile__ (&quot;t 0x5&bslash;n&bslash;t&quot; : : )
 macro_line|#ifdef CONFIG_DEBUG_BUGVERBOSE
 r_extern
 r_void
@@ -18,10 +21,10 @@ id|line
 )paren
 suffix:semicolon
 DECL|macro|BUG
-mdefine_line|#define BUG() do {&t;&t;&t;&t;&t;&bslash;&n;&t;do_BUG(__FILE__, __LINE__);&t;&t;&t;&bslash;&n;&t;__builtin_trap();&t;&t;&t;&t;&bslash;&n;} while (0)
+mdefine_line|#define BUG() do {&t;&t;&t;&t;&t;&bslash;&n;&t;do_BUG(__FILE__, __LINE__);&t;&t;&t;&bslash;&n;&t;__bug_trap();&t;&t;&t;&t;&bslash;&n;} while (0)
 macro_line|#else
 DECL|macro|BUG
-mdefine_line|#define BUG()&t;&t;__builtin_trap()
+mdefine_line|#define BUG()&t;&t;__bug_trap()
 macro_line|#endif
 DECL|macro|BUG_ON
 mdefine_line|#define BUG_ON(condition) do { &bslash;&n;&t;if (unlikely((condition)!=0)) &bslash;&n;&t;&t;BUG(); &bslash;&n;} while(0)
