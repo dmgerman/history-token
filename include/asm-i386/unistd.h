e4e56b51b40da740ab7307b498076f8ec7b7c127
Ulrich Drepper <drepper@redhat.com>
2003-07-07 01:31:32 -0700

    [PATCH] tgkill patch for safe inter-thread signals
    This is the updated versions of the patch Ingo sent some time ago to
    implement a new tgkill() syscall which specifies the target thread
    without any possibility of ambiguity or thread ID wrap races, by passing
    in both the thread group _and_ the thread ID as the arguments.
    
    This is really needed since many/most people still run with limited PID
    ranges (maybe due to legacy apps breaking) and the PID reuse can cause
    problems.

--- include/asm-i386/unistd.h
+++ include/asm-i386/unistd.h
@@ -539,8 +539,10 @@ DECL|macro|__NR_statfs64
 mdefine_line|#define __NR_statfs64&t;&t;268
 DECL|macro|__NR_fstatfs64
 mdefine_line|#define __NR_fstatfs64&t;&t;269
+DECL|macro|__NR_tgkill
+mdefine_line|#define __NR_tgkill&t;&t;270
 DECL|macro|NR_syscalls
-mdefine_line|#define NR_syscalls 270
+mdefine_line|#define NR_syscalls 271
 multiline_comment|/* user-visible error numbers are in the range -1 - -124: see &lt;asm-i386/errno.h&gt; */
 DECL|macro|__syscall_return
 mdefine_line|#define __syscall_return(type, res) &bslash;&n;do { &bslash;&n;&t;if ((unsigned long)(res) &gt;= (unsigned long)(-125)) { &bslash;&n;&t;&t;errno = -(res); &bslash;&n;&t;&t;res = -1; &bslash;&n;&t;} &bslash;&n;&t;return (type) (res); &bslash;&n;} while (0)
