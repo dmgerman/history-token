4002748c6d200af6c014838379ab40d034c8c819
Alan Stern <stern@rowland.harvard.edu>
2003-07-31 08:49:25 -0700

    [PATCH] USB: Rename probe and unbind functions
    The is the first part of what used to be as66c.  It simply renames
    usb_device_probe() and usb_device_remove() to usb_probe_interface() and
    usb_unbind_interface().  And since they're not needed outside of usbcore,
    it stops exporting them.

--- drivers/usb/core/devio.c
+++ drivers/usb/core/devio.c
@@ -13,6 +13,7 @@ macro_line|#include &lt;linux/usbdevice_fs.h&gt;
 macro_line|#include &lt;asm/uaccess.h&gt;
 macro_line|#include &lt;asm/byteorder.h&gt;
 macro_line|#include &quot;hcd.h&quot;&t;/* for usbcore internals */
+macro_line|#include &quot;usb.h&quot;
 DECL|struct|async
 r_struct
 id|async
@@ -3708,7 +3709,7 @@ op_logical_and
 id|ps-&gt;dev
 )paren
 (brace
-id|usb_device_probe
+id|usb_probe_interface
 (paren
 op_amp
 id|intf-&gt;dev
@@ -6025,7 +6026,7 @@ comma
 id|ctrl.ifno
 )paren
 suffix:semicolon
-id|usb_device_remove
+id|usb_unbind_interface
 c_func
 (paren
 op_amp
@@ -6057,7 +6058,7 @@ c_func
 suffix:semicolon
 id|retval
 op_assign
-id|usb_device_probe
+id|usb_probe_interface
 (paren
 op_amp
 id|ifp-&gt;dev
