3b401628ebf7c1e4ae5923cb523f5f2ed054354a
Dave Jones <davej@redhat.com>
2003-08-15 17:14:02 +0100

    [AGPGART] Fix compiler warning. Need cacheflush.h for change_page_attr()

--- include/asm-i386/agp.h
+++ include/asm-i386/agp.h
@@ -2,6 +2,7 @@ macro_line|#ifndef AGP_H
 DECL|macro|AGP_H
 mdefine_line|#define AGP_H 1
 macro_line|#include &lt;asm/pgtable.h&gt;
+macro_line|#include &lt;asm/cacheflush.h&gt;
 multiline_comment|/* &n; * Functions to keep the agpgart mappings coherent with the MMU.&n; * The GART gives the CPU a physical alias of pages in memory. The alias region is&n; * mapped uncacheable. Make sure there are no conflicting mappings&n; * with different cachability attributes for the same page. This avoids&n; * data corruption on some CPUs.&n; */
 DECL|macro|map_page_into_agp
 mdefine_line|#define map_page_into_agp(page) change_page_attr(page, 1, PAGE_KERNEL_NOCACHE)
