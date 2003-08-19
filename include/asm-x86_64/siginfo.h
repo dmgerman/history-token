3404a00d1a49f011669410f88b5aaaab05a36b53
Linus Torvalds <torvalds@home.osdl.org>
2003-08-18 20:02:21 -0700

    The default ARCH_SI_BAND_T should be "int", since that is what Linux historically has had. Only x86-64 uses anything else, so make the special case be _there_.

--- include/asm-x86_64/siginfo.h
+++ include/asm-x86_64/siginfo.h
@@ -3,6 +3,8 @@ DECL|macro|_X8664_SIGINFO_H
 mdefine_line|#define _X8664_SIGINFO_H
 DECL|macro|__ARCH_SI_PREAMBLE_SIZE
 mdefine_line|#define __ARCH_SI_PREAMBLE_SIZE&t;(4 * sizeof(int))
+DECL|macro|__ARCH_SI_BAND_T
+mdefine_line|#define __ARCH_SI_BAND_T long
 macro_line|#include &lt;asm-generic/siginfo.h&gt;
 macro_line|#endif
 eof
