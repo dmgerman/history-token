035ad6f1b82c86c8f65247820415c17fbcdddc88
Greg Ungerer <gerg@snapgear.com>
2003-09-01 03:38:26 -0700

    [PATCH] create an m68knommu sections.h
    This patch creates a sections.h for the m68knommu architecture.
    Nothing special needed, just an include of the generic code.

---
+++ include/asm-m68knommu/sections.h
@@ -0,0 +1,7 @@
+macro_line|#ifndef _M68KNOMMU_SECTIONS_H
+DECL|macro|_M68KNOMMU_SECTIONS_H
+mdefine_line|#define _M68KNOMMU_SECTIONS_H
+multiline_comment|/* nothing to see, move along */
+macro_line|#include &lt;asm-generic/sections.h&gt;
+macro_line|#endif
+eof
