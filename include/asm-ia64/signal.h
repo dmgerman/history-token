22b86846c6d3bd66018e246b039ada16fc0c395e
David Mosberger <davidm@tiger.hpl.hp.com>
2003-09-05 03:19:35 -0700

    ia64: Document the typo that made it into the definition of MINSTKSZ 	(last two numbers got transposed).  Thanks to Arun Sharma for 	finding this.  New glibc's will have the value corrected, but 	we leave the kernel at the old (bogus) value to retain backwards- 	compatibility (and while a strange value, the old value works just 	fine).

--- include/asm-ia64/signal.h
+++ include/asm-ia64/signal.h
@@ -1,7 +1,7 @@
 macro_line|#ifndef _ASM_IA64_SIGNAL_H
 DECL|macro|_ASM_IA64_SIGNAL_H
 mdefine_line|#define _ASM_IA64_SIGNAL_H
-multiline_comment|/*&n; * Copyright (C) 1998-2001 Hewlett-Packard Co&n; *&t;David Mosberger-Tang &lt;davidm@hpl.hp.com&gt;&n; *&n; * Unfortunately, this file is being included by bits/signal.h in&n; * glibc-2.x.  Hence the #ifdef __KERNEL__ ugliness.&n; */
+multiline_comment|/*&n; * Copyright (C) 1998-2001, 2003 Hewlett-Packard Co&n; *&t;David Mosberger-Tang &lt;davidm@hpl.hp.com&gt;&n; *&n; * Unfortunately, this file is being included by bits/signal.h in&n; * glibc-2.x.  Hence the #ifdef __KERNEL__ ugliness.&n; */
 DECL|macro|SIGHUP
 mdefine_line|#define SIGHUP&t;&t; 1
 DECL|macro|SIGINT
@@ -106,8 +106,14 @@ mdefine_line|#define SS_ONSTACK&t;1
 DECL|macro|SS_DISABLE
 mdefine_line|#define SS_DISABLE&t;2
 multiline_comment|/*&n; * The minimum stack size needs to be fairly large because we want to&n; * be sure that an app compiled for today&squot;s CPUs will continue to run&n; * on all future CPU models.  The CPU model matters because the signal&n; * frame needs to have space for the complete machine state, including&n; * all physical stacked registers.  The number of physical stacked&n; * registers is CPU model dependent, but given that the width of&n; * ar.rsc.loadrs is 14 bits, we can assume that they&squot;ll never take up&n; * more than 16KB of space.&n; */
+macro_line|#if 1
+multiline_comment|/*&n;   * This is a stupid typo: the value was _meant_ to be 131072 (0x20000), but I typed it&n;   * in wrong. ;-(  To preserve backwards compatibility, we leave the kernel at the&n;   * incorrect value and fix libc only.&n;   */
 DECL|macro|MINSIGSTKSZ
-mdefine_line|#define MINSIGSTKSZ&t;131027&t;/* min. stack size for sigaltstack() */
+macro_line|# define MINSIGSTKSZ&t;131027&t;/* min. stack size for sigaltstack() */
+macro_line|#else
+DECL|macro|MINSIGSTKSZ
+macro_line|# define MINSIGSTKSZ&t;131072&t;/* min. stack size for sigaltstack() */
+macro_line|#endif
 DECL|macro|SIGSTKSZ
 mdefine_line|#define SIGSTKSZ&t;262144&t;/* default stack size for sigaltstack() */
 macro_line|#ifdef __KERNEL__
