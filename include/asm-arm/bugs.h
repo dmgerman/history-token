824d3c4bd54af3ce83bdbfa6169b6bb74020a32d
Russell King <rmk@flint.arm.linux.org.uk>
2003-09-17 00:31:42 +0100

    [ARM] Detect and fix up CPUs with non-coherent write buffers.
    Jamie Lokier's cache testing program discovered a problem which seems
    to be present in some CPU write buffers.  This cset allows the kernel
    to detect the flaw and activate a workaround to restore the user
    space expectations.

--- include/asm-arm/bugs.h
+++ include/asm-arm/bugs.h
@@ -2,7 +2,15 @@ multiline_comment|/*&n; *  linux/include/asm-arm/bugs.h&n; *&n; *  Copyright (C)
 macro_line|#ifndef __ASM_BUGS_H
 DECL|macro|__ASM_BUGS_H
 mdefine_line|#define __ASM_BUGS_H
+r_extern
+r_void
+id|check_writebuffer_bugs
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
 DECL|macro|check_bugs
-mdefine_line|#define check_bugs() do { } while (0)
+mdefine_line|#define check_bugs() check_writebuffer_bugs()
 macro_line|#endif
 eof
