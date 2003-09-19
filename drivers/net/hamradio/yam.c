10dec5ad120f89cfcd1031ad03e3454304616947
Randy Dunlap <rddunlap@osdl.org>
2003-09-19 11:02:52 -0700

    [NET]: Remove unneeded includes (hamradio, from Randy Hron).

--- drivers/net/hamradio/yam.c
+++ drivers/net/hamradio/yam.c
@@ -29,7 +29,6 @@ macro_line|#include &lt;linux/tcp.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/proc_fs.h&gt;
 macro_line|#include &lt;linux/seq_file.h&gt;
-macro_line|#include &lt;linux/version.h&gt;
 macro_line|#include &lt;asm/uaccess.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/yam.h&gt;
