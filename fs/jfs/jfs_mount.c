077342a742a43390280a8984fc65a24713e7f1d1
Alexander Viro <viro@www.linux.org.uk>
2003-09-22 22:51:00 -0700

    [PATCH] prepare for 32-bit dev_t: JFS
    	Added missing old_encode_dev() in JFS.

--- fs/jfs/jfs_mount.c
+++ fs/jfs/jfs_mount.c
@@ -1440,7 +1440,11 @@ op_assign
 id|cpu_to_le32
 c_func
 (paren
+id|old_encode_dev
+c_func
+(paren
 id|sbi-&gt;log-&gt;bdev-&gt;bd_dev
+)paren
 )paren
 suffix:semicolon
 id|j_sb-&gt;s_logserial
@@ -1628,7 +1632,11 @@ op_assign
 id|cpu_to_le32
 c_func
 (paren
+id|old_encode_dev
+c_func
+(paren
 id|sb-&gt;s_bdev-&gt;bd_dev
+)paren
 )paren
 suffix:semicolon
 id|lmLog
