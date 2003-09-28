53eb8c0ead39cb95c27bee9a7264bb4d3846b3b3
Arnaldo Carvalho de Melo <acme@conectiva.com.br>
2003-09-28 17:49:26 -0300

    o NET: move drivers/net/loopback.c EXPORT_SYMBOLs from net/netsyms.c

--- net/netsyms.c
+++ net/netsyms.c
@@ -2,7 +2,6 @@ multiline_comment|/*&n; *  linux/net/netsyms.c&n; *&n; *  Symbol table for the l
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/fs.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
-macro_line|#include &lt;linux/netdevice.h&gt;
 multiline_comment|/* Needed by unix.o */
 DECL|variable|files_stat
 id|EXPORT_SYMBOL
@@ -11,15 +10,7 @@ c_func
 id|files_stat
 )paren
 suffix:semicolon
-multiline_comment|/* support for loadable net drivers */
 macro_line|#ifdef CONFIG_NET
-DECL|variable|loopback_dev
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|loopback_dev
-)paren
-suffix:semicolon
 DECL|variable|__kill_fasync
 id|EXPORT_SYMBOL
 c_func
