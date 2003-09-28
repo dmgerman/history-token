1d7ff875df35686140fcb3c2159ec40180a2c8ff
Arnaldo Carvalho de Melo <acme@conectiva.com.br>
2003-09-28 17:24:16 -0300

    o NET: move net/802/*.c EXPORT_SYMBOLs from net/netsyms.c
    Also add #include <linux/netdevice.h> to include/net/pkt_sched.h, as it uses
    functions defined on this include (netif_queue_stopped, etc). Ah, the other
    datalink stuff was moved as well (eth.c, pe2.c)

--- net/netsyms.c
+++ net/netsyms.c
@@ -1,14 +1,6 @@
 multiline_comment|/*&n; *  linux/net/netsyms.c&n; *&n; *  Symbol table for the linux networking subsystem. Moved here to&n; *  make life simpler in ksyms.c.&n; */
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
-macro_line|#include &lt;linux/netdevice.h&gt;
-macro_line|#include &lt;linux/fddidevice.h&gt;
-macro_line|#include &lt;linux/trdevice.h&gt;
-macro_line|#include &lt;linux/fcdevice.h&gt;
-macro_line|#include &lt;linux/etherdevice.h&gt;
-macro_line|#ifdef CONFIG_HIPPI
-macro_line|#include &lt;linux/hippidevice.h&gt;
-macro_line|#endif
 macro_line|#include &lt;net/pkt_sched.h&gt;
 macro_line|#ifdef CONFIG_INET
 macro_line|#if defined(CONFIG_INET_AH) || defined(CONFIG_INET_AH_MODULE) || defined(CONFIG_INET6_AH) || defined(CONFIG_INET6_AH_MODULE)
@@ -18,48 +10,6 @@ macro_line|#if defined(CONFIG_INET_ESP) || defined(CONFIG_INET_ESP_MODULE) || de
 macro_line|#include &lt;net/esp.h&gt;
 macro_line|#endif
 macro_line|#endif
-macro_line|#ifdef CONFIG_IPX_MODULE
-r_extern
-r_struct
-id|datalink_proto
-op_star
-id|make_EII_client
-c_func
-(paren
-r_void
-)paren
-suffix:semicolon
-r_extern
-r_struct
-id|datalink_proto
-op_star
-id|make_8023_client
-c_func
-(paren
-r_void
-)paren
-suffix:semicolon
-r_extern
-r_void
-id|destroy_EII_client
-c_func
-(paren
-r_struct
-id|datalink_proto
-op_star
-)paren
-suffix:semicolon
-r_extern
-r_void
-id|destroy_8023_client
-c_func
-(paren
-r_struct
-id|datalink_proto
-op_star
-)paren
-suffix:semicolon
-macro_line|#endif
 multiline_comment|/* Needed by unix.o */
 DECL|variable|files_stat
 id|EXPORT_SYMBOL
@@ -68,36 +18,6 @@ c_func
 id|files_stat
 )paren
 suffix:semicolon
-macro_line|#ifdef CONFIG_IPX_MODULE
-DECL|variable|make_8023_client
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|make_8023_client
-)paren
-suffix:semicolon
-DECL|variable|destroy_8023_client
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|destroy_8023_client
-)paren
-suffix:semicolon
-DECL|variable|make_EII_client
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|make_EII_client
-)paren
-suffix:semicolon
-DECL|variable|destroy_EII_client
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|destroy_EII_client
-)paren
-suffix:semicolon
-macro_line|#endif
 macro_line|#ifdef CONFIG_INET
 macro_line|#if defined(CONFIG_INET_ESP) || defined(CONFIG_INET_ESP_MODULE) || defined(CONFIG_INET6_ESP) || defined(CONFIG_INET6_ESP_MODULE)
 DECL|variable|skb_cow_data
@@ -123,22 +43,6 @@ id|skb_to_sgvec
 suffix:semicolon
 macro_line|#endif
 macro_line|#endif  /* CONFIG_INET */
-macro_line|#ifdef CONFIG_TR
-DECL|variable|tr_source_route
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|tr_source_route
-)paren
-suffix:semicolon
-DECL|variable|tr_type_trans
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|tr_type_trans
-)paren
-suffix:semicolon
-macro_line|#endif
 multiline_comment|/* support for loadable net drivers */
 macro_line|#ifdef CONFIG_NET
 DECL|variable|loopback_dev
@@ -148,22 +52,6 @@ c_func
 id|loopback_dev
 )paren
 suffix:semicolon
-DECL|variable|eth_type_trans
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|eth_type_trans
-)paren
-suffix:semicolon
-macro_line|#ifdef CONFIG_FDDI
-DECL|variable|fddi_type_trans
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|fddi_type_trans
-)paren
-suffix:semicolon
-macro_line|#endif /* CONFIG_FDDI */
 DECL|variable|dev_base
 id|EXPORT_SYMBOL
 c_func
@@ -185,15 +73,6 @@ c_func
 id|__kill_fasync
 )paren
 suffix:semicolon
-macro_line|#ifdef CONFIG_HIPPI
-DECL|variable|hippi_type_trans
-id|EXPORT_SYMBOL
-c_func
-(paren
-id|hippi_type_trans
-)paren
-suffix:semicolon
-macro_line|#endif
 macro_line|#ifdef CONFIG_NET_SCHED
 DECL|variable|pfifo_qdisc_ops
 id|EXPORT_SYMBOL
