6d85771c16116f6c09bf969e00c210f98bea84ad
Andrew Morton <akpm@osdl.org>
2003-10-04 21:03:36 -0700

    [PATCH] EISA_bus cleanup
    From: Marc Zyngier <mzyngier@freesurf.fr>, Christoph Hellwig
    
    I'd like to kill willy's CONFIG_EISA_ALWAYS kludge.  So make EISA_bus a
    variable always when CONFIG_EISA is set and initialize it to 1 for alpha.
    We probably want to do that only if the system actually supports eisa, but
    I keep the old behaviour for now.

--- drivers/eisa/eisa-bus.c
+++ drivers/eisa/eisa-bus.c
@@ -1835,19 +1835,17 @@ id|postcore_initcall
 id|eisa_init
 )paren
 suffix:semicolon
-macro_line|#ifndef CONFIG_EISA_ALWAYS
 DECL|variable|EISA_bus
 r_int
 id|EISA_bus
 suffix:semicolon
+multiline_comment|/* for legacy drivers */
 DECL|variable|EISA_bus
 id|EXPORT_SYMBOL
-c_func
 (paren
 id|EISA_bus
 )paren
 suffix:semicolon
-macro_line|#endif
 DECL|variable|eisa_bus_type
 id|EXPORT_SYMBOL
 (paren
