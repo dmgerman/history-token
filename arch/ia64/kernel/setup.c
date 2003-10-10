67c108f0aa9ed243d0b85a523aad7c5acbf16563
Jesse Barnes <jbarnes@sgi.com>
2003-10-10 02:19:38 -0700

    [PATCH] ia64: fix NUMA boot
    Now that the ACPI table parsing stuff has been fixed we can move
    find_memory() back where it belongs--after the SRAT table has been
    parsed.

--- arch/ia64/kernel/setup.c
+++ arch/ia64/kernel/setup.c
@@ -790,11 +790,6 @@ c_func
 (paren
 )paren
 suffix:semicolon
-id|find_memory
-c_func
-(paren
-)paren
-suffix:semicolon
 macro_line|#ifdef CONFIG_ACPI_BOOT
 multiline_comment|/* Initialize the ACPI boot-time table parser */
 id|acpi_table_init
@@ -819,6 +814,11 @@ suffix:semicolon
 multiline_comment|/* happens, e.g., with the Ski simulator */
 macro_line|# endif
 macro_line|#endif /* CONFIG_APCI_BOOT */
+id|find_memory
+c_func
+(paren
+)paren
+suffix:semicolon
 multiline_comment|/* process SAL system table: */
 id|ia64_sal_init
 c_func
