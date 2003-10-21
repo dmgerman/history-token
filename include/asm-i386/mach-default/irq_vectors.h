6f581cfe27d35684395e2edc9b839b31fd9ad2f2
James Cleverdon <jamesclv@us.ibm.com>
2003-10-21 07:27:58 -0700

    [PATCH] Allow more APIC irq sources
    The "irq_vector[]" array is indexed by the sum of all RTEs in all I/O
    APICs, and is not necessarily limited by the x86 CPU irq vector inputs.
    
    In fact, the irq vector index would overflow on big machines with lots
    of IO APIC's, causing the boot to fail.
    
    So grow the array for the big SMP boxes, keeping the default the same as
    before (and shrink the vector entry size down to a 8-bit value, since
    that's the size of the actual CPU vector entry).

--- include/asm-i386/mach-default/irq_vectors.h
+++ include/asm-i386/mach-default/irq_vectors.h
@@ -35,9 +35,18 @@ multiline_comment|/*&n; * 16 8259A IRQ&squot;s, 208 potential APIC interrupt sou
 macro_line|#ifdef CONFIG_X86_IO_APIC
 DECL|macro|NR_IRQS
 mdefine_line|#define NR_IRQS 224
+macro_line|# if (224 &gt;= 32 * NR_CPUS)
+DECL|macro|NR_IRQ_VECTORS
+macro_line|# define NR_IRQ_VECTORS NR_IRQS
+macro_line|# else
+DECL|macro|NR_IRQ_VECTORS
+macro_line|# define NR_IRQ_VECTORS (32 * NR_CPUS)
+macro_line|# endif
 macro_line|#else
 DECL|macro|NR_IRQS
 mdefine_line|#define NR_IRQS 16
+DECL|macro|NR_IRQ_VECTORS
+mdefine_line|#define NR_IRQ_VECTORS NR_IRQS
 macro_line|#endif
 DECL|macro|FPU_IRQ
 mdefine_line|#define FPU_IRQ&t;&t;&t;13
