48b941a7dee8ca260df441f774dd6953f84d75f7
Alex Williamson <alex.williamson@hp.com>
2003-10-22 02:52:04 -0700

    [PATCH] ia64: trivial ia64 numa/discontig fixes
    I stumbled on a couple trivial bugs in ia64 numa/discontig support.
    The first just sets the default number of nodes to something reasonable
    for a generic kernel, otherwise it's really easy to start walking over
    your initdata (more error checking should probably be added).  The
    second fixes a memcpy to a physical address.

--- include/asm-ia64/numnodes.h
+++ include/asm-ia64/numnodes.h
@@ -5,7 +5,7 @@ macro_line|#ifdef CONFIG_IA64_DIG
 multiline_comment|/* Max 8 Nodes */
 DECL|macro|NODES_SHIFT
 mdefine_line|#define NODES_SHIFT&t;3
-macro_line|#elif defined(CONFIG_IA64_SGI_SN2)
+macro_line|#elif defined(CONFIG_IA64_SGI_SN2) || defined(CONFIG_IA64_GENERIC)
 multiline_comment|/* Max 128 Nodes */
 DECL|macro|NODES_SHIFT
 mdefine_line|#define NODES_SHIFT&t;7
