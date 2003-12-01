d1102c9205616b4a832d368aaeba27e152956686
Jaroslav Kysela <perex@suse.cz>
2003-12-01 12:12:21 +0100

    ALSA CVS update - version 1.0.0pre3

--- include/sound/version.h
+++ include/sound/version.h
@@ -1,6 +1,6 @@
 multiline_comment|/* include/version.h.  Generated automatically by configure.  */
 DECL|macro|CONFIG_SND_VERSION
-mdefine_line|#define CONFIG_SND_VERSION &quot;0.9.7&quot;
+mdefine_line|#define CONFIG_SND_VERSION &quot;1.0.0pre3&quot;
 DECL|macro|CONFIG_SND_DATE
-mdefine_line|#define CONFIG_SND_DATE &quot; (Thu Sep 25 19:16:36 2003 UTC)&quot;
+mdefine_line|#define CONFIG_SND_DATE &quot; (Mon Dec 01 08:36:23 2003 UTC)&quot;
 eof
