96a3036dcd55debca2a1162e6ef8080f62e0ac14
Arnaud Quette <arnaud.quette@mgeups.com>
2003-12-28 20:31:36 -0800

    [PATCH] USB: disable hiddev support for MGE UPS
    following my recent posts on libusb-devel and hidups, here's
    a patch to disable hiddev support for MGE UPSs. It only
    declares VID/PID as QUIRK_IGNORE in hid-core's blacklist.
    This simply prevent hiddev to be loaded when plugging
    an MGE UPS.

--- drivers/usb/input/hid-core.c
+++ drivers/usb/input/hid-core.c
@@ -6431,7 +6431,7 @@ id|USB_VENDOR_ID_MGE
 comma
 id|USB_DEVICE_ID_MGE_UPS
 comma
-id|HID_QUIRK_HIDDEV
+id|HID_QUIRK_IGNORE
 )brace
 comma
 (brace
@@ -6439,7 +6439,7 @@ id|USB_VENDOR_ID_MGE
 comma
 id|USB_DEVICE_ID_MGE_UPS1
 comma
-id|HID_QUIRK_HIDDEV
+id|HID_QUIRK_IGNORE
 )brace
 comma
 (brace
