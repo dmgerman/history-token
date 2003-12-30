e40daeded5b7abe71b230cd54b53823a04e7495f
Jaroslav Kysela <perex@suse.cz>
2003-12-30 12:45:54 +0100

    ALSA CVS update - Jaroslav Kysela <perex@suse.cz> VIA82xx driver Added Easy Note 3171, Packard Bell - VIA_DXS_ENABLE

--- sound/pci/via82xx.c
+++ sound/pci/via82xx.c
@@ -9484,6 +9484,24 @@ multiline_comment|/* MSI KT4V */
 dot
 id|vendor
 op_assign
+l_int|0x1631
+comma
+dot
+id|device
+op_assign
+l_int|0xe004
+comma
+dot
+id|action
+op_assign
+id|VIA_DXS_ENABLE
+)brace
+comma
+multiline_comment|/* Easy Note 3174, Packard Bell */
+(brace
+dot
+id|vendor
+op_assign
 l_int|0x1695
 comma
 dot
