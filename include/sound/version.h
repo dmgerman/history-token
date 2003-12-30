15b988c33625edd622dcd19d7366f59c4efd672e
Jaroslav Kysela <perex@suse.cz>
2003-12-30 13:12:54 +0100

    ALSA 1.0.1

--- include/sound/version.h
+++ include/sound/version.h
@@ -1,6 +1,6 @@
 multiline_comment|/* include/version.h.  Generated automatically by configure.  */
 DECL|macro|CONFIG_SND_VERSION
-mdefine_line|#define CONFIG_SND_VERSION &quot;1.0.0rc2&quot;
+mdefine_line|#define CONFIG_SND_VERSION &quot;1.0.1&quot;
 DECL|macro|CONFIG_SND_DATE
-mdefine_line|#define CONFIG_SND_DATE &quot; (Fri Dec 05 13:57:15 2003 UTC)&quot;
+mdefine_line|#define CONFIG_SND_DATE &quot; (Tue Dec 30 10:04:14 2003 UTC)&quot;
 eof
