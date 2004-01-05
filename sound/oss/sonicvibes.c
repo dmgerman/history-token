eeed7b06eac9aa6787657b27f437e5a5b1cff270
Bartlomiej Zolnierkiewicz <b.zolnierkiewicz@elka.pw.edu.pl>
2004-01-05 05:39:29 -0800

    [PATCH] fix oopses on rmmod in some OSS drivers
    Fix for oops on rmmod caused by *_remove() function marked as __devinit
    (and thus discarded after module initialization - if CONFIG_MODULES=y
    and CONFIG_HOTPLUG is not set).
    This patch changes __devinit to __devexit and adds __devexit_p() where
    pointer to such function is used.
    
    The only exception is au1000, where au1000_remove() is called from
    cleanup_au1000() function - __devinit is jest removed there.

--- sound/oss/sonicvibes.c
+++ sound/oss/sonicvibes.c
@@ -15317,7 +15317,7 @@ suffix:semicolon
 DECL|function|sv_remove
 r_static
 r_void
-id|__devinit
+id|__devexit
 id|sv_remove
 c_func
 (paren
@@ -15564,7 +15564,11 @@ comma
 dot
 id|remove
 op_assign
+id|__devexit_p
+c_func
+(paren
 id|sv_remove
+)paren
 comma
 )brace
 suffix:semicolon
