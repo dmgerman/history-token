7aa29e2d8c0f9f13465db1edbe141357ad545593
Nathan Scott <nathans@sgi.com>
2004-01-09 11:29:25 +1100

    [XFS] Rename pagebuf debug option (ie. pagebuf tracing) into a generic XFS tracing option for the other XFS trace code to use too (once fixed).
    SGI Modid: 2.5.x-xfs:slinx:159717a

--- fs/xfs/linux/xfs_super.h
+++ fs/xfs/linux/xfs_super.h
@@ -62,12 +62,12 @@ macro_line|#else
 DECL|macro|XFS_BIGFS_STRING
 macro_line|# define XFS_BIGFS_STRING
 macro_line|#endif
-macro_line|#ifdef CONFIG_XFS_VNODE_TRACING
-DECL|macro|XFS_VNTRACE_STRING
-macro_line|# define XFS_VNTRACE_STRING&t;&quot;VN-trace, &quot;
+macro_line|#ifdef CONFIG_XFS_TRACE
+DECL|macro|XFS_TRACE_STRING
+macro_line|# define XFS_TRACE_STRING&t;&quot;tracing, &quot;
 macro_line|#else
-DECL|macro|XFS_VNTRACE_STRING
-macro_line|# define XFS_VNTRACE_STRING
+DECL|macro|XFS_TRACE_STRING
+macro_line|# define XFS_TRACE_STRING
 macro_line|#endif
 macro_line|#ifdef XFSDEBUG
 DECL|macro|XFS_DBG_STRING
@@ -77,7 +77,7 @@ DECL|macro|XFS_DBG_STRING
 macro_line|# define XFS_DBG_STRING&t;&t;&quot;no debug&quot;
 macro_line|#endif
 DECL|macro|XFS_BUILD_OPTIONS
-mdefine_line|#define XFS_BUILD_OPTIONS&t;XFS_ACL_STRING &bslash;&n;&t;&t;&t;&t;XFS_REALTIME_STRING &bslash;&n;&t;&t;&t;&t;XFS_BIGFS_STRING &bslash;&n;&t;&t;&t;&t;XFS_VNTRACE_STRING &bslash;&n;&t;&t;&t;&t;XFS_DBG_STRING /* DBG must be last */
+mdefine_line|#define XFS_BUILD_OPTIONS&t;XFS_ACL_STRING &bslash;&n;&t;&t;&t;&t;XFS_REALTIME_STRING &bslash;&n;&t;&t;&t;&t;XFS_BIGFS_STRING &bslash;&n;&t;&t;&t;&t;XFS_TRACE_STRING &bslash;&n;&t;&t;&t;&t;XFS_DBG_STRING /* DBG must be last */
 DECL|macro|LINVFS_GET_VFS
 mdefine_line|#define LINVFS_GET_VFS(s) &bslash;&n;&t;(vfs_t *)((s)-&gt;s_fs_info)
 DECL|macro|LINVFS_SET_VFS
