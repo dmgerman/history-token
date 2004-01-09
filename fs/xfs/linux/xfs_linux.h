5819f50ed6ea88c84e0672524d937fcef3297a6d
Nathan Scott <nathans@sgi.com>
2004-01-09 14:58:35 +1100

    [XFS] Move the stack trace wrapper into a kernel-version-specific location.
    SGI Modid: 2.5.x-xfs:slinx:162317a

--- fs/xfs/linux/xfs_linux.h
+++ fs/xfs/linux/xfs_linux.h
@@ -201,6 +201,22 @@ DECL|macro|howmany
 mdefine_line|#define howmany(x, y)&t;(((x)+((y)-1))/(y))
 DECL|macro|roundup
 mdefine_line|#define roundup(x, y)&t;((((x)+((y)-1))/(y))*(y))
+DECL|function|xfs_stack_trace
+r_static
+r_inline
+r_void
+id|xfs_stack_trace
+c_func
+(paren
+r_void
+)paren
+(brace
+id|dump_stack
+c_func
+(paren
+)paren
+suffix:semicolon
+)brace
 multiline_comment|/* Move the kernel do_div definition off to one side */
 macro_line|#if defined __i386__
 multiline_comment|/* For ia32 we need to pull some tricks to get past various versions&n; * of the compiler which do not like us using do_div in the middle&n; * of large functions.&n; */
