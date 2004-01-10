a81fea89ece7a741da119ae967e67df518d10ad1
Stephen Hemminger <shemminger@osdl.org>
2004-01-10 04:53:12 -0500

    [PATCH] (7/42) cs89x0
    Based on viro NE31-cs89x0
    	* switched cs89x0 to dynamic allocation
    	* cs89x0: embedded ->priv
    	* cs89x0: fixed resource leaks on failure exits
    	* cs89x0: fixed clobbering on autoprobe
    	* NB: cs89x0 calls request_region() with very odd arguments.  Somebody
    	  ought to check WTF is going on there.

--- drivers/net/Space.c
+++ drivers/net/Space.c
@@ -444,14 +444,14 @@ op_star
 )paren
 suffix:semicolon
 r_extern
-r_int
-id|cs89x0_probe
-c_func
-(paren
 r_struct
 id|net_device
 op_star
-id|dev
+id|cs89x0_probe
+c_func
+(paren
+r_int
+id|unit
 )paren
 suffix:semicolon
 r_extern
@@ -1067,14 +1067,6 @@ l_int|0
 )brace
 comma
 macro_line|#endif
-macro_line|#ifdef CONFIG_CS89x0
-(brace
-id|cs89x0_probe
-comma
-l_int|0
-)brace
-comma
-macro_line|#endif
 (brace
 l_int|NULL
 comma
@@ -1093,6 +1085,14 @@ id|isa_probes2
 id|__initdata
 op_assign
 (brace
+macro_line|#ifdef CONFIG_CS89x0
+(brace
+id|cs89x0_probe
+comma
+l_int|0
+)brace
+comma
+macro_line|#endif
 macro_line|#ifdef CONFIG_AT1700
 (brace
 id|at1700_probe
