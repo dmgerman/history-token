5f1bea816064e1cbee833d782654bdf937b603ee
Stephen Hemminger <shemminger@osdl.org>
2004-01-10 04:55:11 -0500

    [PATCH] (17/42) wd
    Based on viro NE41-wd
    	* switched wd to dynamic allocation
    	* wd: fixed order of freeing bugs
    	* wd: fixed resource leaks on failure exits
    	* wd: fixed clobbering on autoprobe

--- drivers/net/Space.c
+++ drivers/net/Space.c
@@ -53,14 +53,14 @@ id|dev
 )paren
 suffix:semicolon
 r_extern
-r_int
-id|wd_probe
-c_func
-(paren
 r_struct
 id|net_device
 op_star
-id|dev
+id|wd_probe
+c_func
+(paren
+r_int
+id|unit
 )paren
 suffix:semicolon
 r_extern
@@ -980,14 +980,6 @@ l_int|0
 )brace
 comma
 macro_line|#endif
-macro_line|#ifdef CONFIG_WD80x3 
-(brace
-id|wd_probe
-comma
-l_int|0
-)brace
-comma
-macro_line|#endif
 (brace
 l_int|NULL
 comma
@@ -1006,6 +998,14 @@ id|isa_probes2
 id|__initdata
 op_assign
 (brace
+macro_line|#ifdef CONFIG_WD80x3 
+(brace
+id|wd_probe
+comma
+l_int|0
+)brace
+comma
+macro_line|#endif
 macro_line|#ifdef CONFIG_EL2 &t;&t;/* 3c503 */
 (brace
 id|el2_probe
