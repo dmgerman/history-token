3f1517296333d4ac2de1aa95f1d88c9f1aebb32f
Stephen Hemminger <shemminger@osdl.org>
2004-01-10 04:36:32 -0500

    [PATCH] (08/12) Probe2 -- 3c507
    Originally by Al Viro (NE19-3c507)
    	* switched 3c507 to dynamic allocation
    	* 3c507: embedded ->priv
    	* 3c507: fixed clobbering on autoprobe
    	* NB: 3c507.c buggers port 0x100 without claiming it.  Most likely it
    	  should be doing request_region() there.
    Updated to apply agains jgarzik/net-drivers-2.5-exp

--- drivers/net/Space.c
+++ drivers/net/Space.c
@@ -218,13 +218,14 @@ op_star
 )paren
 suffix:semicolon
 r_extern
-r_int
-id|el16_probe
-c_func
-(paren
 r_struct
 id|net_device
 op_star
+id|el16_probe
+c_func
+(paren
+r_int
+id|unit
 )paren
 suffix:semicolon
 r_extern
@@ -1145,14 +1146,6 @@ l_int|0
 )brace
 comma
 macro_line|#endif
-macro_line|#ifdef CONFIG_EL16&t;&t;/* 3c507 */
-(brace
-id|el16_probe
-comma
-l_int|0
-)brace
-comma
-macro_line|#endif
 (brace
 l_int|NULL
 comma
@@ -1171,6 +1164,14 @@ id|isa_probes2
 id|__initdata
 op_assign
 (brace
+macro_line|#ifdef CONFIG_EL16&t;&t;/* 3c507 */
+(brace
+id|el16_probe
+comma
+l_int|0
+)brace
+comma
+macro_line|#endif
 macro_line|#ifdef CONFIG_ELPLUS&t;&t;/* 3c505 */
 (brace
 id|elplus_probe
