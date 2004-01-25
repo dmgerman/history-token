652f5e10a72ce4bb171476f3d3d5d20f64514e3e
Jaroslav Kysela <perex@suse.cz>
2004-01-25 12:48:45 +0100

    ALSA - updated date identification

--- include/sound/version.h
+++ include/sound/version.h
@@ -2,5 +2,5 @@ multiline_comment|/* include/version.h.  Generated automatically by configure.  
 DECL|macro|CONFIG_SND_VERSION
 mdefine_line|#define CONFIG_SND_VERSION &quot;1.0.1&quot;
 DECL|macro|CONFIG_SND_DATE
-mdefine_line|#define CONFIG_SND_DATE &quot; (Tue Dec 30 10:04:14 2003 UTC)&quot;
+mdefine_line|#define CONFIG_SND_DATE &quot; (Fri Jan 23 13:43:51 2004 UTC)&quot;
 eof
