39184e735baaafea512720dc03ed2379e3ceb62b
Steve French <stevef@stevef95.austin.ibm.com>
2004-02-02 05:54:45 -0600

    fix remoting caching part 2

--- fs/cifs/misc.c
+++ fs/cifs/misc.c
@@ -516,6 +516,8 @@ r_struct
 id|smb_hdr
 op_star
 id|ret_buf
+op_assign
+l_int|0
 suffix:semicolon
 multiline_comment|/* We could use negotiated size instead of max_msgsize - &n;   but it may be more efficient to always alloc same size &n;   albeit slightly larger than necessary and maxbuffersize &n;   defaults to this and can not be bigger */
 id|ret_buf
