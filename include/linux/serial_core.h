5c2ab7dd2b9ec58f26e0812117f8938f9eda237d
Holger Schurig <h.schurig@de.rmk.(none)>
2004-02-20 16:52:31 +0000

    [ARM PATCH] 1670/1: PXA serial driver
    Patch from Holger Schurig
    
    PXA Serial port (FFUART, BTUART, STUART) and console support
    
    This patch works perfectly here with "console=ttyS0,115200" in the
    kernel command line. It contains Jean-frederic Clere fix to detect
    STUART and BTUART.
    
    Nico said that I should post it to your patch tracker. It replaces patch 1662/1

--- include/linux/serial_core.h
+++ include/linux/serial_core.h
@@ -33,6 +33,8 @@ mdefine_line|#define PORT_NS16550A&t;14
 DECL|macro|PORT_MAX_8250
 mdefine_line|#define PORT_MAX_8250&t;14&t;/* max port ID */
 multiline_comment|/*&n; * ARM specific type numbers.  These are not currently guaranteed&n; * to be implemented, and will change in the future.  These are&n; * separate so any additions to the old serial.c that occur before&n; * we are merged can be easily merged here.&n; */
+DECL|macro|PORT_PXA
+mdefine_line|#define PORT_PXA&t;31
 DECL|macro|PORT_AMBA
 mdefine_line|#define PORT_AMBA&t;32
 DECL|macro|PORT_CLPS711X
