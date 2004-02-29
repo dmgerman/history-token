a8f79a7e682dc290c497af2359fdaf6b7e56aceb
Ben Collins <bcollins@debian.org>
2004-02-28 22:46:48 -0500

    IEEE1394: Revision sync.

--- drivers/ieee1394/eth1394.c
+++ drivers/ieee1394/eth1394.c
@@ -49,7 +49,7 @@ id|version
 )braket
 id|__devinitdata
 op_assign
-l_string|&quot;$Rev: 1169 $ Ben Collins &lt;bcollins@debian.org&gt;&quot;
+l_string|&quot;$Rev: 1175 $ Ben Collins &lt;bcollins@debian.org&gt;&quot;
 suffix:semicolon
 DECL|struct|fragment_info
 r_struct
@@ -7738,7 +7738,7 @@ id|strcpy
 (paren
 id|info.version
 comma
-l_string|&quot;$Rev: 1169 $&quot;
+l_string|&quot;$Rev: 1175 $&quot;
 )paren
 suffix:semicolon
 multiline_comment|/* FIXME XXX provide sane businfo */
