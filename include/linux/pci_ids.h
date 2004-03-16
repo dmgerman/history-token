651f09f9b5692b2fa299152db293cceb30c986ff
Bartlomiej Zolnierkiewicz <b.zolnierkiewicz@elka.pw.edu.pl>
2004-03-16 00:46:50 -0800

    [PATCH] ATI IXP IDE support
    It was tested by a few people and has been in -mm since 2.6.4-rc2-mm1.

--- include/linux/pci_ids.h
+++ include/linux/pci_ids.h
@@ -597,6 +597,9 @@ DECL|macro|PCI_DEVICE_ID_ATI_RS300_166
 mdefine_line|#define PCI_DEVICE_ID_ATI_RS300_166&t;0x5832
 DECL|macro|PCI_DEVICE_ID_ATI_RS300_200
 mdefine_line|#define PCI_DEVICE_ID_ATI_RS300_200&t;0x5833
+multiline_comment|/* ATI IXP Chipset */
+DECL|macro|PCI_DEVICE_ID_ATI_IXP_IDE
+mdefine_line|#define PCI_DEVICE_ID_ATI_IXP_IDE&t;0x4349
 DECL|macro|PCI_VENDOR_ID_VLSI
 mdefine_line|#define PCI_VENDOR_ID_VLSI&t;&t;0x1004
 DECL|macro|PCI_DEVICE_ID_VLSI_82C592
