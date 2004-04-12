7674b6d9cd941f8acadecccf5e4b3def88b19d01
Andrew Morton <akpm@osdl.org>
2004-04-12 01:03:56 -0700

    [PATCH] policydb printk warnings
    security/selinux/ss/policydb.c:1160: warning: signed size_t format, different type arg (arg 3)
    security/selinux/ss/policydb.c:1160: warning: signed size_t format, different type arg (arg 3)

--- security/selinux/ss/policydb.c
+++ security/selinux/ss/policydb.c
@@ -5348,7 +5348,7 @@ c_func
 (paren
 id|KERN_ERR
 l_string|&quot;security:  policydb string length %d does not &quot;
-l_string|&quot;match expected length %Zd&bslash;n&quot;
+l_string|&quot;match expected length %Zu&bslash;n&quot;
 comma
 id|len
 comma
