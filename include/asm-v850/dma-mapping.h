7a589b77284c11e0322138146f31eda9ffc16286
Andrew Morton <akpm@osdl.org>
2004-04-11 23:33:47 -0700

    [PATCH] v850: make v850 dma-mapping.h header work when !CONFIG_PCI
    From: <miles@mcspd15.ucom.lsi.nec.co.jp> (Miles Bader)
    
    Is this something that should be done in <asm-generic/dma-mapping.h>?

--- include/asm-v850/dma-mapping.h
+++ include/asm-v850/dma-mapping.h
@@ -1,2 +1,11 @@
+macro_line|#ifndef __V850_DMA_MAPPING_H__
+DECL|macro|__V850_DMA_MAPPING_H__
+mdefine_line|#define __V850_DMA_MAPPING_H__
+macro_line|#include &lt;linux/config.h&gt;
+macro_line|#ifdef CONFIG_PCI
 macro_line|#include &lt;asm-generic/dma-mapping.h&gt;
+macro_line|#else
+macro_line|#include &lt;asm-generic/dma-mapping-broken.h&gt;
+macro_line|#endif
+macro_line|#endif /* __V850_DMA_MAPPING_H__ */
 eof
