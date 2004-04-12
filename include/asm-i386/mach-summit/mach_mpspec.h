17d3ff18ace47e37d23a209c1334e9c5b38226f7
Andrew Morton <akpm@osdl.org>
2004-04-11 23:06:32 -0700

    [PATCH] summmit: increase MAX_MP_BUSSES
    From: James Cleverdon <jamesclv@us.ibm.com>
    
    Bump up MAX_MP_BUSSES for summit/generic subarch to cope with big IBM x440
    systems.

--- include/asm-i386/mach-summit/mach_mpspec.h
+++ include/asm-i386/mach-summit/mach_mpspec.h
@@ -6,7 +6,8 @@ DECL|macro|MAX_APICS
 mdefine_line|#define MAX_APICS 256
 DECL|macro|MAX_IRQ_SOURCES
 mdefine_line|#define MAX_IRQ_SOURCES 256
+multiline_comment|/* Maximum 256 PCI busses, plus 1 ISA bus in each of 4 cabinets. */
 DECL|macro|MAX_MP_BUSSES
-mdefine_line|#define MAX_MP_BUSSES 32
+mdefine_line|#define MAX_MP_BUSSES 260
 macro_line|#endif /* __ASM_MACH_MPSPEC_H */
 eof
