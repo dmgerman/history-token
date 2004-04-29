ed6316bd490316cf575de437304091754df6eb6e
Rolf Eike Beer <eike-hotplug@sf-tec.de>
2004-04-29 02:01:34 -0700

    [PATCH] PCI Hotplug skeleton: use new style of module parameters
    Convert the PCI hotplug skeleton driver to use new style of module parameter
    handling.

--- drivers/pci/hotplug/pcihp_skeleton.c
+++ drivers/pci/hotplug/pcihp_skeleton.c
@@ -1,6 +1,7 @@
 multiline_comment|/*&n; * PCI Hot Plug Controller Skeleton Driver - 0.2&n; *&n; * Copyright (C) 2001,2003 Greg Kroah-Hartman (greg@kroah.com)&n; * Copyright (C) 2001,2003 IBM Corp.&n; *&n; * All rights reserved.&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or (at&n; * your option) any later version.&n; *&n; * This program is distributed in the hope that it will be useful, but&n; * WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or&n; * NON INFRINGEMENT.  See the GNU General Public License for more&n; * details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n; *&n; * This driver is to be used as a skeleton driver to be show how to interface&n; * with the pci hotplug core easily.&n; *&n; * Send feedback to &lt;greg@kroah.com&gt;&n; *&n; */
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
+macro_line|#include &lt;linux/moduleparam.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
@@ -40,13 +41,8 @@ c_func
 id|slot_list
 )paren
 suffix:semicolon
-macro_line|#if !defined(CONFIG_HOTPLUG_PCI_SKELETON_MODULE)
 DECL|macro|MY_NAME
 mdefine_line|#define MY_NAME&t;&quot;pcihp_skeleton&quot;
-macro_line|#else
-DECL|macro|MY_NAME
-mdefine_line|#define MY_NAME&t;THIS_MODULE-&gt;name
-macro_line|#endif
 DECL|macro|dbg
 mdefine_line|#define dbg(format, arg...)&t;&t;&t;&t;&t;&bslash;&n;&t;do {&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;&t;if (debug)&t;&t;&t;&t;&t;&bslash;&n;&t;&t;&t;printk (KERN_DEBUG &quot;%s: &quot; format &quot;&bslash;n&quot;,&t;&bslash;&n;&t;&t;&t;&t;MY_NAME , ## arg); &t;&t;&bslash;&n;&t;} while (0)
 DECL|macro|err
@@ -92,12 +88,14 @@ c_func
 l_string|&quot;GPL&quot;
 )paren
 suffix:semicolon
-id|MODULE_PARM
+id|module_param
 c_func
 (paren
 id|debug
 comma
-l_string|&quot;i&quot;
+r_bool
+comma
+l_int|644
 )paren
 suffix:semicolon
 id|MODULE_PARM_DESC
