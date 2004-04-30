c95e0f30cbfcd44fc7fd729f6753ed377927841b
Rolf Eike Beer <eike-hotplug@sf-tec.de>
2004-04-30 00:17:13 -0700

    [PATCH] PCI Hotplug Core: use new style of module parameters
    Convert PCI Hotplug Core to new style of module parameter handling.

--- drivers/pci/hotplug/pci_hotplug_core.c
+++ drivers/pci/hotplug/pci_hotplug_core.c
@@ -1,6 +1,7 @@
 multiline_comment|/*&n; * PCI HotPlug Controller Core&n; *&n; * Copyright (C) 2001-2002 Greg Kroah-Hartman (greg@kroah.com)&n; * Copyright (C) 2001-2002 IBM Corp.&n; *&n; * All rights reserved.&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2 of the License, or (at&n; * your option) any later version.&n; *&n; * This program is distributed in the hope that it will be useful, but&n; * WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, GOOD TITLE or&n; * NON INFRINGEMENT.  See the GNU General Public License for more&n; * details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n; *&n; * Send feedback to &lt;greg@kroah.com&gt;&n; *&n; * Filesystem portion based on work done by Pat Mochel on ddfs/driverfs&n; *&n; */
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
+macro_line|#include &lt;linux/moduleparam.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/list.h&gt;
@@ -15,13 +16,8 @@ macro_line|#include &lt;asm/uaccess.h&gt;
 macro_line|#include &lt;linux/kobject.h&gt;
 macro_line|#include &lt;linux/sysfs.h&gt;
 macro_line|#include &quot;pci_hotplug.h&quot;
-macro_line|#if !defined(CONFIG_HOTPLUG_PCI_MODULE)
 DECL|macro|MY_NAME
 mdefine_line|#define MY_NAME&t;&quot;pci_hotplug&quot;
-macro_line|#else
-DECL|macro|MY_NAME
-mdefine_line|#define MY_NAME&t;THIS_MODULE-&gt;name
-macro_line|#endif
 DECL|macro|dbg
 mdefine_line|#define dbg(fmt, arg...) do { if (debug) printk(KERN_DEBUG &quot;%s: %s: &quot; fmt , MY_NAME , __FUNCTION__ , ## arg); } while (0)
 DECL|macro|err
@@ -2935,12 +2931,14 @@ c_func
 l_string|&quot;GPL&quot;
 )paren
 suffix:semicolon
-id|MODULE_PARM
+id|module_param
 c_func
 (paren
 id|debug
 comma
-l_string|&quot;i&quot;
+r_bool
+comma
+l_int|644
 )paren
 suffix:semicolon
 id|MODULE_PARM_DESC
