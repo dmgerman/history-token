368b2fe1dd3a016e42932d09258cb529bbf42551
Jean Delvare <khali@linux-fr.org>
2004-05-10 23:43:55 -0700

    [PATCH] I2C: kill duplicate includes in i2c bus drivers
    Following a suggestion of Arthur Othieno, here is a trivial patch that
    kills duplicate inclusions of config.h in four i2c bus drivers.
    
    At first I was thinking of also removing inclusions of config.h wherever
    it doesn't seem to be necessary but Eugene doesn't seem to think it's a
    good idea. So I may give it a try later (in 2.7), but for now this
    simple patch will be enough.

--- drivers/i2c/busses/i2c-piix4.c
+++ drivers/i2c/busses/i2c-piix4.c
@@ -3,7 +3,6 @@ multiline_comment|/*&n;   Supports:&n;&t;Intel PIIX4, 440MX&n;&t;Serverworks OSB
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/moduleparam.h&gt;
-macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/stddef.h&gt;
