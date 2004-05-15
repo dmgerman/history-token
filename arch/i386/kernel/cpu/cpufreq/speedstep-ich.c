90e97342e5f106628b823cef04c1806a7b8c92fa
Dave Jones <davej@redhat.com>
2004-05-16 00:38:41 +0100

    [CPUFREQ] Fix an invalid comment in speedstep-ich This driver is for ICH only, not for PIIX4. Thanks to Christian Hilberg for noting this.

--- arch/i386/kernel/cpu/cpufreq/speedstep-ich.c
+++ arch/i386/kernel/cpu/cpufreq/speedstep-ich.c
@@ -463,7 +463,7 @@ r_return
 l_int|0
 suffix:semicolon
 )brace
-multiline_comment|/**&n; * speedstep_detect_chipset - detect the Southbridge which contains SpeedStep logic&n; *&n; *   Detects PIIX4, ICH2-M and ICH3-M so far. The pci_dev points to &n; * the LPC bridge / PM module which contains all power-management &n; * functions. Returns the SPEEDSTEP_CHIPSET_-number for the detected&n; * chipset, or zero on failure.&n; */
+multiline_comment|/**&n; * speedstep_detect_chipset - detect the Southbridge which contains SpeedStep logic&n; *&n; *   Detects ICH2-M, ICH3-M and ICH4-M so far. The pci_dev points to &n; * the LPC bridge / PM module which contains all power-management &n; * functions. Returns the SPEEDSTEP_CHIPSET_-number for the detected&n; * chipset, or zero on failure.&n; */
 DECL|function|speedstep_detect_chipset
 r_static
 r_int
