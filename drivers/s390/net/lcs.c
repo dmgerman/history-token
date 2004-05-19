7e1994ad754e382f6ddf1bbe2ae8cf6340769efd
Andrew Morton <akpm@osdl.org>
2004-05-19 02:42:37 -0700

    [PATCH] s390: network driver
    From: Martin Schwidefsky <schwidefsky@de.ibm.com>
    
    Network driver changes:
     - iucv: Make grab_param function SMP safe.
     - lcs: Fix null-pointer dereference after unsuccessful set_online.
     - qeth: Fix kmalloc flags in qeth_alloc_reply.
     - qeth: Show broadcase capability also in route4/6 sysfs attributes.
     - qeth: Remove debug code.
     - qeth: Add option to qetharp user space interface to strip unused
             fields from query arp records.
     - qeth: Add shortcut in outbound path for HiperSockets.
     - qeth: Add more info to qeth_perf_stats.
     - qeth: Add support for direct SNMP interface to OSA express cards.

--- drivers/s390/net/lcs.c
+++ drivers/s390/net/lcs.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; *  linux/drivers/s390/net/lcs.c&n; *&n; *  Linux for S/390 Lan Channel Station Network Driver&n; *&n; *  Copyright (C)  1999-2001 IBM Deutschland Entwicklung GmbH,&n; *&t;&t;&t;     IBM Corporation&n; *    Author(s): Original Code written by&n; *&t;&t;&t;  DJ Barrow (djbarrow@de.ibm.com,barrow_dj@yahoo.com)&n; *&t;&t; Rewritten by&n; *&t;&t;&t;  Frank Pavlic (pavlic@de.ibm.com) and&n; *&t;&t; &t;  Martin Schwidefsky &lt;schwidefsky@de.ibm.com&gt;&n; *&n; *    $Revision: 1.80 $&t; $Date: 2004/05/13 08:22:06 $&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2, or (at your option)&n; * any later version.&n; *&n; * This program is distributed in the hope that it will be useful,&n; * but WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.&t; See the&n; * GNU General Public License for more details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n; */
+multiline_comment|/*&n; *  linux/drivers/s390/net/lcs.c&n; *&n; *  Linux for S/390 Lan Channel Station Network Driver&n; *&n; *  Copyright (C)  1999-2001 IBM Deutschland Entwicklung GmbH,&n; *&t;&t;&t;     IBM Corporation&n; *    Author(s): Original Code written by&n; *&t;&t;&t;  DJ Barrow (djbarrow@de.ibm.com,barrow_dj@yahoo.com)&n; *&t;&t; Rewritten by&n; *&t;&t;&t;  Frank Pavlic (pavlic@de.ibm.com) and&n; *&t;&t; &t;  Martin Schwidefsky &lt;schwidefsky@de.ibm.com&gt;&n; *&n; *    $Revision: 1.81 $&t; $Date: 2004/05/14 13:54:33 $&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License as published by&n; * the Free Software Foundation; either version 2, or (at your option)&n; * any later version.&n; *&n; * This program is distributed in the hope that it will be useful,&n; * but WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.&t; See the&n; * GNU General Public License for more details.&n; *&n; * You should have received a copy of the GNU General Public License&n; * along with this program; if not, write to the Free Software&n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.&n; */
 macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;linux/if.h&gt;
 macro_line|#include &lt;linux/netdevice.h&gt;
@@ -22,7 +22,7 @@ macro_line|#error Cannot compile lcs.c without some net devices switched on.
 macro_line|#endif
 multiline_comment|/**&n; * initialization string for output&n; */
 DECL|macro|VERSION_LCS_C
-mdefine_line|#define VERSION_LCS_C  &quot;$Revision: 1.80 $&quot;
+mdefine_line|#define VERSION_LCS_C  &quot;$Revision: 1.81 $&quot;
 DECL|variable|__initdata
 r_static
 r_char
@@ -8345,13 +8345,28 @@ c_cond
 id|rc
 )paren
 (brace
-id|lcs_stopcard
+id|LCS_DBF_TEXT
 c_func
 (paren
-id|card
+l_int|2
+comma
+id|setup
+comma
+l_string|&quot;dtctfail&quot;
 )paren
 suffix:semicolon
-id|lcs_cleanup_card
+id|PRINT_WARN
+c_func
+(paren
+l_string|&quot;Detection of LCS card failed with return code &quot;
+l_string|&quot;%d (0x%x)&bslash;n&quot;
+comma
+id|rc
+comma
+id|rc
+)paren
+suffix:semicolon
+id|lcs_stopcard
 c_func
 (paren
 id|card
@@ -8621,12 +8636,6 @@ id|ccw_device_set_offline
 c_func
 (paren
 id|card-&gt;write.ccwdev
-)paren
-suffix:semicolon
-id|lcs_cleanup_card
-c_func
-(paren
-id|card
 )paren
 suffix:semicolon
 r_return
