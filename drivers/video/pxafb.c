524d20e6180e0d9341fa04061ae9393299f5882d
Ian Campbell <icampbell@com.rmk.(none)>
2004-06-18 19:49:27 +0100

    [ARM PATCH] 1934/2:  Consolidate code to set CKEN on PXA
    Patch from Ian Campbell
    
    I've seen comments several times that various PXA drivers
    update CKEN in an unsafe manner. This patch consolidates
    this code into a single function pxa_set_cken() and updates
    all the in tree drivers to use it.

--- drivers/video/pxafb.c
+++ drivers/video/pxafb.c
@@ -5234,10 +5234,6 @@ op_star
 id|inf
 suffix:semicolon
 r_int
-r_int
-id|flags
-suffix:semicolon
-r_int
 id|ret
 suffix:semicolon
 id|dev_dbg
@@ -5550,20 +5546,12 @@ id|failed
 suffix:semicolon
 )brace
 multiline_comment|/* enable LCD controller clock */
-id|local_irq_save
+id|pxa_set_cken
 c_func
 (paren
-id|flags
-)paren
-suffix:semicolon
-id|CKEN
-op_or_assign
 id|CKEN16_LCD
-suffix:semicolon
-id|local_irq_restore
-c_func
-(paren
-id|flags
+comma
+l_int|1
 )paren
 suffix:semicolon
 id|ret
