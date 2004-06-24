8274356190e17bed240a9655b7a31efb5291b68d
Andrew Morton <akpm@osdl.org>
2004-06-23 19:24:55 -0700

    [PATCH] sh: SH-3 On-Chip ADC support
    From: Paul Mundt <lethal@Linux-SH.ORG>
    
    This adds support for the SH-3's on-chip ADC (which is needed by things like
    the touchscreen, etc.).
    
    Signed-off-by: Andriy Skulysh <askulysh@image.kiev.ua>
    Signed-off-by: Paul Mundt <lethal@linux-sh.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ include/asm-sh/cpu-sh3/adc.h
@@ -0,0 +1,38 @@
+macro_line|#ifndef __ASM_CPU_SH3_ADC_H
+DECL|macro|__ASM_CPU_SH3_ADC_H
+mdefine_line|#define __ASM_CPU_SH3_ADC_H
+multiline_comment|/*&n; * Copyright (C) 2004  Andriy Skulysh&n; */
+DECL|macro|ADDRAH
+mdefine_line|#define ADDRAH&t;0xa4000080
+DECL|macro|ADDRAL
+mdefine_line|#define ADDRAL&t;0xa4000082
+DECL|macro|ADDRBH
+mdefine_line|#define ADDRBH&t;0xa4000084
+DECL|macro|ADDRBL
+mdefine_line|#define ADDRBL&t;0xa4000086
+DECL|macro|ADDRCH
+mdefine_line|#define ADDRCH&t;0xa4000088
+DECL|macro|ADDRCL
+mdefine_line|#define ADDRCL&t;0xa400008a
+DECL|macro|ADDRDH
+mdefine_line|#define ADDRDH&t;0xa400008c
+DECL|macro|ADDRDL
+mdefine_line|#define ADDRDL&t;0xa400008e
+DECL|macro|ADCSR
+mdefine_line|#define ADCSR&t;0xa4000090
+DECL|macro|ADCSR_ADF
+mdefine_line|#define ADCSR_ADF&t;0x80
+DECL|macro|ADCSR_ADIE
+mdefine_line|#define ADCSR_ADIE&t;0x40
+DECL|macro|ADCSR_ADST
+mdefine_line|#define ADCSR_ADST&t;0x20
+DECL|macro|ADCSR_MULTI
+mdefine_line|#define ADCSR_MULTI&t;0x10
+DECL|macro|ADCSR_CKS
+mdefine_line|#define ADCSR_CKS&t;0x08
+DECL|macro|ADCSR_CH_MASK
+mdefine_line|#define ADCSR_CH_MASK&t;0x07
+DECL|macro|ADCR
+mdefine_line|#define ADCR&t;0xa4000092
+macro_line|#endif /* __ASM_CPU_SH3_ADC_H */
+eof
