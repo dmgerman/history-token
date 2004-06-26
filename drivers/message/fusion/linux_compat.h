d6794b339184f19298fa881a9fbf4dcb05b4e6b9
Christoph Hellwig <hch@infradead.org>
2004-06-26 01:31:31 -0500

    [PATCH] MPT Fusion driver 3.01.09 update
    On Mon, Jun 21, 2004 at 12:16:08PM -0400, Moore, Eric Dean wrote:
    > We are pleased to announce the MPT Fusion release candidate for lk 2.6
    
    I've worked with Eric offline to resolve the issue we had and get some
    more changes in, but he has left for his vacation today.  He send me
    a patch though and left it to me whether we'd merged it despite only
    moderate testing.  Given 2.6.7 was just done and he'll certainly be
    back before 2.6.8 I'd go for it.  Below is the patch rediffed against
    scsi-misc-2.6:
    
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/message/fusion/linux_compat.h
+++ drivers/message/fusion/linux_compat.h
@@ -2,6 +2,27 @@ multiline_comment|/* drivers/message/fusion/linux_compat.h */
 macro_line|#ifndef FUSION_LINUX_COMPAT_H
 DECL|macro|FUSION_LINUX_COMPAT_H
 mdefine_line|#define FUSION_LINUX_COMPAT_H
+macro_line|#include &lt;linux/version.h&gt;
+macro_line|#include &lt;scsi/scsi_device.h&gt;
+macro_line|#if (LINUX_VERSION_CODE &lt; KERNEL_VERSION(2,6,6))
+DECL|function|scsi_device_online
+r_static
+r_int
+r_inline
+id|scsi_device_online
+c_func
+(paren
+r_struct
+id|scsi_device
+op_star
+id|sdev
+)paren
+(brace
+r_return
+id|sdev-&gt;online
+suffix:semicolon
+)brace
+macro_line|#endif
 multiline_comment|/*}-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 macro_line|#endif /* _LINUX_COMPAT_H */
 eof
