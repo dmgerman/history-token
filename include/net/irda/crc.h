e8706e7caf0cf04d41260baf4f2f6064a6373eaf
Andrey Panin <pazke@donpac.ru>
2004-07-10 19:36:13 -0700

    [PATCH] CRC16 renaming in IRDA drivers
    Signed-off-by: Andrey Panin <pazke@donpac.ru>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/net/irda/crc.h
+++ include/net/irda/crc.h
@@ -3,16 +3,16 @@ macro_line|#ifndef IRDA_CRC_H
 DECL|macro|IRDA_CRC_H
 mdefine_line|#define IRDA_CRC_H
 macro_line|#include &lt;linux/types.h&gt;
-macro_line|#include &lt;linux/crc16.h&gt;
+macro_line|#include &lt;linux/crc-ccitt.h&gt;
 DECL|macro|INIT_FCS
 mdefine_line|#define INIT_FCS  0xffff   /* Initial FCS value */
 DECL|macro|GOOD_FCS
 mdefine_line|#define GOOD_FCS  0xf0b8   /* Good final FCS value */
 multiline_comment|/* Recompute the FCS with one more character appended. */
 DECL|macro|irda_fcs
-mdefine_line|#define irda_fcs(fcs, c) crc16_byte(fcs, c)
+mdefine_line|#define irda_fcs(fcs, c) crc_ccitt_byte(fcs, c)
 multiline_comment|/* Recompute the FCS with len bytes appended. */
 DECL|macro|irda_calc_crc16
-mdefine_line|#define irda_calc_crc16(fcs, buf, len) crc16(fcs, buf, len)
+mdefine_line|#define irda_calc_crc16(fcs, buf, len) crc_ccitt(fcs, buf, len)
 macro_line|#endif
 eof
