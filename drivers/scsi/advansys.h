b054875f698449b3fcffb22c8992c8d16d103830
Adrian Bunk <bunk@fs.tum.de>
2004-07-16 01:08:37 -0500

    [PATCH] another small advansys cleanup
    The patch below does the following small cleanups for the advansys
    driver:
    - remove obsolete maintainer information
    - remove kernel 2.2 code from advansys.h
    
    diffstat output:
     MAINTAINERS             |    7
     drivers/scsi/advansys.c |  342 +++++++---------------------------------
     drivers/scsi/advansys.h |   26 ---
     3 files changed, 60 insertions(+), 315 deletions(-)
    
    Signed-off-by: Adrian Bunk <bunk@fs.tum.de>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/advansys.h
+++ drivers/scsi/advansys.h
@@ -1,22 +1,7 @@
-multiline_comment|/*&n; * advansys.h - Linux Host Driver for AdvanSys SCSI Adapters&n; * &n; * Copyright (c) 1995-2000 Advanced System Products, Inc.&n; * Copyright (c) 2000-2001 ConnectCom Solutions, Inc.&n; * All Rights Reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that redistributions of source&n; * code retain the above copyright notice and this comment without&n; * modification.&n; *&n; * As of March 8, 2000 Advanced System Products, Inc. (AdvanSys)&n; * changed its name to ConnectCom Solutions, Inc.&n; *&n; * There is an AdvanSys Linux WWW page at:&n; *  http://www.connectcom.net/downloads/software/os/linux.html&n; *  http://www.advansys.com/linux.html&n; *&n; * The latest released version of the AdvanSys driver is available at:&n; *  ftp://ftp.advansys.com/pub/linux/linux.tgz&n; *  ftp://ftp.connectcom.net/pub/linux/linux.tgz&n; *&n; * Please send questions, comments, bug reports to:&n; *  linux@connectcom.net or bfrey@turbolinux.com.cn&n; */
+multiline_comment|/*&n; * advansys.h - Linux Host Driver for AdvanSys SCSI Adapters&n; * &n; * Copyright (c) 1995-2000 Advanced System Products, Inc.&n; * Copyright (c) 2000-2001 ConnectCom Solutions, Inc.&n; * All Rights Reserved.&n; *&n; * Redistribution and use in source and binary forms, with or without&n; * modification, are permitted provided that redistributions of source&n; * code retain the above copyright notice and this comment without&n; * modification.&n; *&n; * As of March 8, 2000 Advanced System Products, Inc. (AdvanSys)&n; * changed its name to ConnectCom Solutions, Inc.&n; *&n; */
 macro_line|#ifndef _ADVANSYS_H
 DECL|macro|_ADVANSYS_H
 mdefine_line|#define _ADVANSYS_H
-macro_line|#include &lt;linux/config.h&gt;
-macro_line|#ifndef LINUX_VERSION_CODE
-macro_line|#include &lt;linux/version.h&gt;
-macro_line|#endif /* LINUX_VERSION_CODE */
-multiline_comment|/* Convert Linux Version, Patch-level, Sub-level to LINUX_VERSION_CODE. */
-DECL|macro|ASC_LINUX_VERSION
-mdefine_line|#define ASC_LINUX_VERSION(V, P, S)    (((V) * 65536) + ((P) * 256) + (S))
-multiline_comment|/* Driver supported only in version 2.2 and version &gt;= 2.4. */
-macro_line|#if LINUX_VERSION_CODE &lt; ASC_LINUX_VERSION(2,2,0) || &bslash;&n;    (LINUX_VERSION_CODE &gt; ASC_LINUX_VERSION(2,3,0) &amp;&amp; &bslash;&n;     LINUX_VERSION_CODE &lt; ASC_LINUX_VERSION(2,4,0))
-macro_line|#error &quot;AdvanSys driver supported only in 2.2 and 2.4 or greater kernels.&quot;
-macro_line|#endif
-DECL|macro|ASC_LINUX_KERNEL22
-mdefine_line|#define ASC_LINUX_KERNEL22 (LINUX_VERSION_CODE &lt; ASC_LINUX_VERSION(2,4,0))
-DECL|macro|ASC_LINUX_KERNEL24
-mdefine_line|#define ASC_LINUX_KERNEL24 (LINUX_VERSION_CODE &gt;= ASC_LINUX_VERSION(2,4,0))
 multiline_comment|/*&n; * Scsi_Host_Template function prototypes.&n; */
 r_int
 id|advansys_detect
