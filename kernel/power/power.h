d39205e1240f058d671948de1be39dc251763574
Patrick Mochel <mochel@digitalimplant.org>
2004-07-16 22:23:27 -0700

    [Power Mgmt] Merge swsusp and pmdisk info headers.
    - Move definition of struct pmdsik_info to power.h and rename to struct
      swsusp_info.
    - Kill struct suspend_header.
    - Move helpers from pmdisk into swsusp: init_header(), dump_info(),
      write_header(), sanity_check(), check_header().
    - Fix up calls in pmdisk to call the right ones.
    - Clean up swsusp code to use helpers; delete duplicates.

--- kernel/power/power.h
+++ kernel/power/power.h
@@ -1,8 +1,67 @@
+macro_line|#include &lt;linux/suspend.h&gt;
+macro_line|#include &lt;linux/utsname.h&gt;
 multiline_comment|/* With SUSPEND_CONSOLE defined, it suspend looks *really* cool, but&n;   we probably do not take enough locks for switching consoles, etc,&n;   so bad things might happen.&n;*/
 macro_line|#if defined(CONFIG_VT) &amp;&amp; defined(CONFIG_VT_CONSOLE)
 DECL|macro|SUSPEND_CONSOLE
 mdefine_line|#define SUSPEND_CONSOLE&t;(MAX_NR_CONSOLES-1)
 macro_line|#endif
+DECL|struct|swsusp_info
+r_struct
+id|swsusp_info
+(brace
+DECL|member|uts
+r_struct
+id|new_utsname
+id|uts
+suffix:semicolon
+DECL|member|version_code
+id|u32
+id|version_code
+suffix:semicolon
+DECL|member|num_physpages
+r_int
+r_int
+id|num_physpages
+suffix:semicolon
+DECL|member|cpus
+r_int
+id|cpus
+suffix:semicolon
+DECL|member|image_pages
+r_int
+r_int
+id|image_pages
+suffix:semicolon
+DECL|member|pagedir_pages
+r_int
+r_int
+id|pagedir_pages
+suffix:semicolon
+DECL|member|suspend_pagedir
+id|suspend_pagedir_t
+op_star
+id|suspend_pagedir
+suffix:semicolon
+DECL|member|pagedir
+id|swp_entry_t
+id|pagedir
+(braket
+l_int|768
+)braket
+suffix:semicolon
+)brace
+id|__attribute__
+c_func
+(paren
+(paren
+id|aligned
+c_func
+(paren
+id|PAGE_SIZE
+)paren
+)paren
+)paren
+suffix:semicolon
 macro_line|#ifdef CONFIG_PM_DISK
 r_extern
 r_int
