530565f13679ecfd73771fae39963b1cc75bce02
Dave Jones <davej@redhat.com>
2004-08-05 15:51:57 +0100

    [AGPGART] Sparse trivial warning fixes.
    Signed-off-by: Dave Jones <davej@redhat.com>

--- drivers/char/agp/intel-agp.c
+++ drivers/char/agp/intel-agp.c
@@ -573,7 +573,7 @@ l_int|NULL
 )paren
 (brace
 r_return
-l_int|0
+l_int|NULL
 suffix:semicolon
 )brace
 r_if
@@ -599,7 +599,7 @@ id|page
 )paren
 suffix:semicolon
 r_return
-l_int|0
+l_int|NULL
 suffix:semicolon
 )brace
 id|get_page
@@ -3306,7 +3306,7 @@ id|size-&gt;num_entries
 suffix:semicolon
 id|agp_bridge-&gt;gatt_table_real
 op_assign
-l_int|0
+l_int|NULL
 suffix:semicolon
 id|pci_read_config_dword
 c_func
