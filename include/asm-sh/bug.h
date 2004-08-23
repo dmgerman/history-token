a7063b676220f8720550fdbec390c3174524349a
Matt Mackall <mpm@selenic.com>
2004-08-22 22:55:57 -0700

    [PATCH] move duplicate BUG and WARN_ON bits to asm-generic
    This moves duplicate BUG, PAGE_BUG, BUG_ON, and WARN_ON code to asm-generic
    and makes them slightly more consistent.  This cleanup is also preparatory
    work for making BUG and WARN verbosity configurable.
    
    Signed-off-by: Matt Mackall <mpm@selenic.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-sh/bug.h
+++ include/asm-sh/bug.h
@@ -5,11 +5,8 @@ macro_line|#include &lt;linux/config.h&gt;
 multiline_comment|/*&n; * Tell the user there is some problem.&n; */
 DECL|macro|BUG
 mdefine_line|#define BUG() do { &bslash;&n;&t;printk(&quot;kernel BUG at %s:%d!&bslash;n&quot;, __FILE__, __LINE__); &bslash;&n;&t;asm volatile(&quot;nop&quot;); &bslash;&n;} while (0)
-DECL|macro|BUG_ON
-mdefine_line|#define BUG_ON(condition) do { &bslash;&n;&t;if (unlikely((condition)!=0)) &bslash;&n;&t;&t;BUG(); &bslash;&n;} while(0)
-DECL|macro|PAGE_BUG
-mdefine_line|#define PAGE_BUG(page) do { &bslash;&n;&t;BUG(); &bslash;&n;} while (0)
-DECL|macro|WARN_ON
-mdefine_line|#define WARN_ON(condition) do { &bslash;&n;&t;if (unlikely((condition)!=0)) { &bslash;&n;&t;&t;printk(&quot;Badness in %s at %s:%d&bslash;n&quot;, __FUNCTION__, __FILE__, __LINE__); &bslash;&n;&t;&t;dump_stack(); &bslash;&n;&t;} &bslash;&n;} while (0)
+DECL|macro|HAVE_ARCH_BUG
+mdefine_line|#define HAVE_ARCH_BUG
+macro_line|#include &lt;asm-generic/bug.h&gt;
 macro_line|#endif
 eof
