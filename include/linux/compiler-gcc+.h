6dfb41536759a489e8a5052f9b9c715591242031
Arnd Bergmann <arnd@arndb.de>
2004-08-22 22:45:33 -0700

    [PATCH] clean up __always_inline__ usage
    I find it somewhat annoying that the preprocessor expands every "inline" to
    "__inline__ __attribute__((always_inline)) __attribute__((always_inline))"
    in the current code.
    
    Fix that up.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/compiler-gcc+.h
+++ include/linux/compiler-gcc+.h
@@ -2,11 +2,11 @@ multiline_comment|/* Never include this file directly.  Include &lt;linux/compil
 multiline_comment|/*&n; * These definitions are for Ueber-GCC: always newer than the latest&n; * version and hence sporting everything plus a kitchen-sink.&n; */
 macro_line|#include &lt;linux/compiler-gcc.h&gt;
 DECL|macro|inline
-mdefine_line|#define inline&t;&t;&t;__inline__ __attribute__((always_inline))
+mdefine_line|#define inline&t;&t;&t;inline&t;&t;__attribute__((always_inline))
 DECL|macro|__inline__
-mdefine_line|#define __inline__&t;&t;__inline__ __attribute__((always_inline))
+mdefine_line|#define __inline__&t;&t;__inline__&t;__attribute__((always_inline))
 DECL|macro|__inline
-mdefine_line|#define __inline&t;&t;__inline__ __attribute__((always_inline))
+mdefine_line|#define __inline&t;&t;__inline&t;__attribute__((always_inline))
 DECL|macro|__deprecated
 mdefine_line|#define __deprecated&t;&t;__attribute__((deprecated))
 DECL|macro|__attribute_used__
