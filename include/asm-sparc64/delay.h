b0b2989efe52552adb144b7f8af95d8bb51554c3
David S. Miller <davem@nuts.davemloft.net>
2004-08-27 02:12:23 -0700

    [SPARC64]: Fix delay with HZ==1000.
    When I moved sparc64 over to HZ==1000 this added some
    problems to the udelay() handling.  Specifically, with
    slower cpus we could now get underflows to zero for
    things like udelay(1) due to the order of multiplies
    and shifts.
    
    Fix this, and move it out to arch/sparc64/lib/delay.c
    so it is easier to tinker with this in the future and
    also to optimize away one of the multiplies for the
    constant delay case just like other platforms do.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- include/asm-sparc64/delay.h
+++ include/asm-sparc64/delay.h
@@ -1,4 +1,4 @@
-multiline_comment|/* $Id: delay.h,v 1.13 2002/02/02 03:33:48 kanoj Exp $&n; * delay.h: Linux delay routines on the V9.&n; *&n; * Copyright (C) 1996 David S. Miller (davem@caip.rutgers.edu).&n; */
+multiline_comment|/* delay.h: Linux delay routines on sparc64.&n; *&n; * Copyright (C) 1996, 2004 David S. Miller (davem@davemloft.net).&n; *&n; * Based heavily upon x86 variant which is:&n; * Copyright (C) 1993 Linus Torvalds&n; *&n; * Delay routines calling functions in arch/sparc64/lib/delay.c&n; */
 macro_line|#ifndef __SPARC64_DELAY_H
 DECL|macro|__SPARC64_DELAY_H
 mdefine_line|#define __SPARC64_DELAY_H
@@ -6,46 +6,23 @@ macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/param.h&gt;
 macro_line|#include &lt;asm/cpudata.h&gt;
 macro_line|#ifndef __ASSEMBLY__
-DECL|function|__delay
-r_static
-id|__inline__
+r_extern
 r_void
-id|__delay
+id|__bad_udelay
 c_func
 (paren
-r_int
-r_int
-id|loops
-)paren
-(brace
-id|__asm__
-id|__volatile__
-c_func
-(paren
-l_string|&quot;&t;b,pt&t;%%xcc, 1f&bslash;n&quot;
-l_string|&quot;&t; cmp&t;%0, 0&bslash;n&quot;
-l_string|&quot;&t;.align&t;32&bslash;n&quot;
-l_string|&quot;1:&bslash;n&quot;
-l_string|&quot;&t;bne,pt&t;%%xcc, 1b&bslash;n&quot;
-l_string|&quot;&t; subcc&t;%0, 1, %0&bslash;n&quot;
-suffix:colon
-l_string|&quot;=&amp;r&quot;
-(paren
-id|loops
-)paren
-suffix:colon
-l_string|&quot;0&quot;
-(paren
-id|loops
-)paren
-suffix:colon
-l_string|&quot;cc&quot;
+r_void
 )paren
 suffix:semicolon
-)brace
-DECL|function|__udelay
-r_static
-id|__inline__
+r_extern
+r_void
+id|__bad_ndelay
+c_func
+(paren
+r_void
+)paren
+suffix:semicolon
+r_extern
 r_void
 id|__udelay
 c_func
@@ -53,108 +30,42 @@ c_func
 r_int
 r_int
 id|usecs
-comma
-r_int
-r_int
-id|lps
-)paren
-(brace
-id|usecs
-op_mul_assign
-l_int|0x00000000000010c6UL
-suffix:semicolon
-multiline_comment|/* 2**32 / 1000000 */
-id|__asm__
-id|__volatile__
-c_func
-(paren
-l_string|&quot;&t;mulx&t;%1, %2, %0&bslash;n&quot;
-l_string|&quot;&t;srlx&t;%0, 32, %0&bslash;n&quot;
-suffix:colon
-l_string|&quot;=r&quot;
-(paren
-id|usecs
-)paren
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-id|usecs
-)paren
-comma
-l_string|&quot;r&quot;
-(paren
-id|lps
-)paren
 )paren
 suffix:semicolon
-id|__delay
-c_func
-(paren
-id|usecs
-op_star
-id|HZ
-)paren
-suffix:semicolon
-)brace
-DECL|function|__ndelay
 r_extern
-id|__inline__
 r_void
 id|__ndelay
 c_func
 (paren
 r_int
 r_int
-id|usecs
-comma
-r_int
-r_int
-id|lps
+id|nsecs
 )paren
-(brace
-id|usecs
-op_mul_assign
-l_int|0x0000000000000005UL
 suffix:semicolon
-multiline_comment|/* 2**32 / 10000 */
-id|__asm__
-id|__volatile__
+r_extern
+r_void
+id|__const_udelay
 c_func
 (paren
-l_string|&quot;&t;mulx&t;%1, %2, %0&bslash;n&quot;
-l_string|&quot;&t;srlx&t;%0, 32, %0&bslash;n&quot;
-suffix:colon
-l_string|&quot;=r&quot;
-(paren
+r_int
+r_int
 id|usecs
-)paren
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-id|usecs
-)paren
-comma
-l_string|&quot;r&quot;
-(paren
-id|lps
-)paren
 )paren
 suffix:semicolon
+r_extern
+r_void
 id|__delay
 c_func
 (paren
-id|usecs
-op_star
-id|HZ
+r_int
+r_int
+id|loops
 )paren
 suffix:semicolon
-)brace
-DECL|macro|__udelay_val
-mdefine_line|#define __udelay_val cpu_data(smp_processor_id()).udelay_val
 DECL|macro|udelay
-mdefine_line|#define udelay(usecs) __udelay((usecs),__udelay_val)
+mdefine_line|#define udelay(n) (__builtin_constant_p(n) ? &bslash;&n;&t;((n) &gt; 20000 ? __bad_udelay() : __const_udelay((n) * 0x10c7ul)) : &bslash;&n;&t;__udelay(n))
 DECL|macro|ndelay
-mdefine_line|#define ndelay(usecs) __ndelay((usecs),__udelay_val)
+mdefine_line|#define ndelay(n) (__builtin_constant_p(n) ? &bslash;&n;&t;((n) &gt; 20000 ? __bad_ndelay() : __const_udelay((n) * 5ul)) : &bslash;&n;&t;__ndelay(n))
 macro_line|#endif /* !__ASSEMBLY__ */
 macro_line|#endif /* defined(__SPARC64_DELAY_H) */
 eof
