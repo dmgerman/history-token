c1f6dbf8206ba32a25fe2ca815427478edbc222b
Jason Davis <jd@rightthere.net>
2004-09-02 20:21:21 -0700

    [PATCH] ES7000: increase MAX_MP_BUSSES
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/mach-es7000/mach_mpspec.h
+++ include/asm-i386/mach-es7000/mach_mpspec.h
@@ -4,6 +4,6 @@ mdefine_line|#define __ASM_MACH_MPSPEC_H
 DECL|macro|MAX_IRQ_SOURCES
 mdefine_line|#define MAX_IRQ_SOURCES 256
 DECL|macro|MAX_MP_BUSSES
-mdefine_line|#define MAX_MP_BUSSES 32
+mdefine_line|#define MAX_MP_BUSSES 256
 macro_line|#endif /* __ASM_MACH_MPSPEC_H */
 eof
