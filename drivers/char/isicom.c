2e67454802b1b27afd1814311bec22e7d712acde
Maximilian Attems <janitor@sternwelten.at>
2004-09-02 20:34:56 -0700

    [PATCH] isicom: replace schedule_timeout() with msleep()
    Uses msleep() instead of schedule_timeout() to guarantee the task delays at
    least the desired time amount.
    
    Signed-off-by: Nishanth Aravamudan <nacc@us.ibm.com>
    Signed-off-by: Maximilian Attems <janitor@sternwelten.at>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/char/isicom.c
+++ drivers/char/isicom.c
@@ -10,6 +10,7 @@ macro_line|#include &lt;linux/mm.h&gt;
 macro_line|#include &lt;linux/miscdevice.h&gt;
 macro_line|#include &lt;linux/interrupt.h&gt;
 macro_line|#include &lt;linux/timer.h&gt;
+macro_line|#include &lt;linux/delay.h&gt;
 macro_line|#include &lt;linux/ioport.h&gt;
 macro_line|#include &lt;asm/uaccess.h&gt;
 macro_line|#include &lt;asm/io.h&gt;
@@ -9610,16 +9611,10 @@ id|re_schedule
 op_assign
 l_int|0
 suffix:semicolon
-id|set_current_state
+id|msleep
 c_func
 (paren
-id|TASK_INTERRUPTIBLE
-)paren
-suffix:semicolon
-id|schedule_timeout
-c_func
-(paren
-id|HZ
+l_int|1000
 )paren
 suffix:semicolon
 macro_line|#ifdef ISICOM_DEBUG&t;
