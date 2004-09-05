fc8d97a1e15eaf65ffb7558585b849bc7bc9d858
Margit Schubert-While <margitsw@t-online.de>
2004-09-05 07:19:38 -0400

    [PATCH] prism54 fix wpa_supplicant frequency parsing
    * This work fixes wpa_supplicant frequency parsing. iwlist eth0
    * scan will now show channel and frequency.

--- drivers/net/wireless/prism54/isl_ioctl.c
+++ drivers/net/wireless/prism54/isl_ioctl.c
@@ -2352,15 +2352,11 @@ suffix:semicolon
 multiline_comment|/* Add frequency. (short) bss-&gt;channel is the frequency in MHz */
 id|iwe.u.freq.m
 op_assign
-id|channel_of_freq
-c_func
-(paren
 id|bss-&gt;channel
-)paren
 suffix:semicolon
 id|iwe.u.freq.e
 op_assign
-l_int|0
+l_int|6
 suffix:semicolon
 id|iwe.cmd
 op_assign
