b11cdf923e13f6d354f1da1414f1508b60de79cd
Zwane Mwaikambo <zwane@fsmlabs.com>
2004-09-09 19:37:14 -0700

    [PATCH] Fix i386 SPINLOCK_MAGIC debugging
    Using a label isn't sufficent anymore for determining the location of the
    failed lock. Use __builtin_return_address instead.
    
    Signed-off-by: Zwane Mwaikambo <zwane@fsmlabs.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/spinlock.h
+++ include/asm-i386/spinlock.h
@@ -229,11 +229,6 @@ id|lock
 )paren
 (brace
 macro_line|#ifdef CONFIG_DEBUG_SPINLOCK
-id|__label__
-id|here
-suffix:semicolon
-id|here
-suffix:colon
 r_if
 c_cond
 (paren
@@ -251,8 +246,11 @@ c_func
 (paren
 l_string|&quot;eip: %p&bslash;n&quot;
 comma
-op_logical_and
-id|here
+id|__builtin_return_address
+c_func
+(paren
+l_int|0
+)paren
 )paren
 suffix:semicolon
 id|BUG
@@ -294,11 +292,6 @@ id|flags
 )paren
 (brace
 macro_line|#ifdef CONFIG_DEBUG_SPINLOCK
-id|__label__
-id|here
-suffix:semicolon
-id|here
-suffix:colon
 r_if
 c_cond
 (paren
@@ -316,8 +309,11 @@ c_func
 (paren
 l_string|&quot;eip: %p&bslash;n&quot;
 comma
-op_logical_and
-id|here
+id|__builtin_return_address
+c_func
+(paren
+l_int|0
+)paren
 )paren
 suffix:semicolon
 id|BUG
