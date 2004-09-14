8970918d8aeb23656121510e547cb5bd46b62eeb
Russell King <rmk@flint.arm.linux.org.uk>
2004-09-14 01:28:34 +0100

    [ARM] Update APM state definitions
    Move existing APM state definitions into struct apm_power_info,
    and add further definitions describing other fields.

--- include/asm-arm/apm.h
+++ include/asm-arm/apm.h
@@ -3,31 +3,6 @@ macro_line|#ifndef ARM_ASM_SA1100_APM_H
 DECL|macro|ARM_ASM_SA1100_APM_H
 mdefine_line|#define ARM_ASM_SA1100_APM_H
 macro_line|#include &lt;linux/config.h&gt;
-macro_line|#if defined(CONFIG_APM) || defined(CONFIG_APM_MODULE)
-DECL|macro|APM_AC_OFFLINE
-mdefine_line|#define APM_AC_OFFLINE 0
-DECL|macro|APM_AC_ONLINE
-mdefine_line|#define APM_AC_ONLINE 1
-DECL|macro|APM_AC_BACKUP
-mdefine_line|#define APM_AC_BACKUP 2
-DECL|macro|APM_AC_UNKNOWN
-mdefine_line|#define APM_AC_UNKNOWN 0xFF
-DECL|macro|APM_BATTERY_STATUS_HIGH
-mdefine_line|#define APM_BATTERY_STATUS_HIGH 0
-DECL|macro|APM_BATTERY_STATUS_LOW
-mdefine_line|#define APM_BATTERY_STATUS_LOW  1
-DECL|macro|APM_BATTERY_STATUS_CRITICAL
-mdefine_line|#define APM_BATTERY_STATUS_CRITICAL 2
-DECL|macro|APM_BATTERY_STATUS_CHARGING
-mdefine_line|#define APM_BATTERY_STATUS_CHARGING 3
-DECL|macro|APM_BATTERY_STATUS_UNKNOWN
-mdefine_line|#define APM_BATTERY_STATUS_UNKNOWN 0xFF
-DECL|macro|APM_BATTERY_LIFE_UNKNOWN
-mdefine_line|#define APM_BATTERY_LIFE_UNKNOWN 0xFFFF
-DECL|macro|APM_BATTERY_LIFE_MINUTES
-mdefine_line|#define APM_BATTERY_LIFE_MINUTES 0x8000
-DECL|macro|APM_BATTERY_LIFE_VALUE_MASK
-mdefine_line|#define APM_BATTERY_LIFE_VALUE_MASK 0x7FFF
 multiline_comment|/*&n; * This structure gets filled in by the machine specific &squot;get_power_status&squot;&n; * implementation.  Any fields which are not set default to a safe value.&n; */
 DECL|struct|apm_power_info
 r_struct
@@ -38,16 +13,48 @@ r_int
 r_char
 id|ac_line_status
 suffix:semicolon
+DECL|macro|APM_AC_OFFLINE
+mdefine_line|#define APM_AC_OFFLINE&t;&t;&t;0
+DECL|macro|APM_AC_ONLINE
+mdefine_line|#define APM_AC_ONLINE&t;&t;&t;1
+DECL|macro|APM_AC_BACKUP
+mdefine_line|#define APM_AC_BACKUP&t;&t;&t;2
+DECL|macro|APM_AC_UNKNOWN
+mdefine_line|#define APM_AC_UNKNOWN&t;&t;&t;0xff
 DECL|member|battery_status
 r_int
 r_char
 id|battery_status
 suffix:semicolon
+DECL|macro|APM_BATTERY_STATUS_HIGH
+mdefine_line|#define APM_BATTERY_STATUS_HIGH&t;&t;0
+DECL|macro|APM_BATTERY_STATUS_LOW
+mdefine_line|#define APM_BATTERY_STATUS_LOW&t;&t;1
+DECL|macro|APM_BATTERY_STATUS_CRITICAL
+mdefine_line|#define APM_BATTERY_STATUS_CRITICAL&t;2
+DECL|macro|APM_BATTERY_STATUS_CHARGING
+mdefine_line|#define APM_BATTERY_STATUS_CHARGING&t;3
+DECL|macro|APM_BATTERY_STATUS_NOT_PRESENT
+mdefine_line|#define APM_BATTERY_STATUS_NOT_PRESENT&t;4
+DECL|macro|APM_BATTERY_STATUS_UNKNOWN
+mdefine_line|#define APM_BATTERY_STATUS_UNKNOWN&t;0xff
 DECL|member|battery_flag
 r_int
 r_char
 id|battery_flag
 suffix:semicolon
+DECL|macro|APM_BATTERY_FLAG_HIGH
+mdefine_line|#define APM_BATTERY_FLAG_HIGH&t;&t;(1 &lt;&lt; 0)
+DECL|macro|APM_BATTERY_FLAG_LOW
+mdefine_line|#define APM_BATTERY_FLAG_LOW&t;&t;(1 &lt;&lt; 1)
+DECL|macro|APM_BATTERY_FLAG_CRITICAL
+mdefine_line|#define APM_BATTERY_FLAG_CRITICAL&t;(1 &lt;&lt; 2)
+DECL|macro|APM_BATTERY_FLAG_CHARGING
+mdefine_line|#define APM_BATTERY_FLAG_CHARGING&t;(1 &lt;&lt; 3)
+DECL|macro|APM_BATTERY_FLAG_NOT_PRESENT
+mdefine_line|#define APM_BATTERY_FLAG_NOT_PRESENT&t;(1 &lt;&lt; 7)
+DECL|macro|APM_BATTERY_FLAG_UNKNOWN
+mdefine_line|#define APM_BATTERY_FLAG_UNKNOWN&t;0xff
 DECL|member|battery_life
 r_int
 id|battery_life
@@ -60,6 +67,12 @@ DECL|member|units
 r_int
 id|units
 suffix:semicolon
+DECL|macro|APM_UNITS_MINS
+mdefine_line|#define APM_UNITS_MINS&t;&t;&t;0
+DECL|macro|APM_UNITS_SECS
+mdefine_line|#define APM_UNITS_SECS&t;&t;&t;1
+DECL|macro|APM_UNITS_UNKNOWN
+mdefine_line|#define APM_UNITS_UNKNOWN&t;&t;-1
 )brace
 suffix:semicolon
 multiline_comment|/*&n; * This allows machines to provide their own &quot;apm get power status&quot; function.&n; */
@@ -75,6 +88,5 @@ id|apm_power_info
 op_star
 )paren
 suffix:semicolon
-macro_line|#endif
 macro_line|#endif
 eof
