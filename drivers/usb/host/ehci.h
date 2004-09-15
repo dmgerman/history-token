9d2d98bb56d6eead87840528284a41aab9fbd03e
Greg Kroah-Hartman <greg@kroah.com>
2004-09-14 23:07:28 -0700

    USB: fix up __iomem warnings in the ehci driver.
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/usb/host/ehci.h
+++ drivers/usb/host/ehci.h
@@ -136,12 +136,14 @@ suffix:semicolon
 DECL|member|caps
 r_struct
 id|ehci_caps
+id|__iomem
 op_star
 id|caps
 suffix:semicolon
 DECL|member|regs
 r_struct
 id|ehci_regs
+id|__iomem
 op_star
 id|regs
 suffix:semicolon
