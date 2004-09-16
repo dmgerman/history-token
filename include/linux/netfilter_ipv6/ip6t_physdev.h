c0f4deac6630cb836f33b5c2b22ec2046d07afc6
Bart De Schuymer <bdschuym@pandora.be>
2004-09-15 23:19:01 -0700

    [NETFILTER]: port physdev to ip6tables
    Signed-off-by: Bart De Schuymer <bdschuym@pandora.be>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/netfilter_ipv6/ip6t_physdev.h
@@ -0,0 +1,62 @@
+macro_line|#ifndef _IP6T_PHYSDEV_H
+DECL|macro|_IP6T_PHYSDEV_H
+mdefine_line|#define _IP6T_PHYSDEV_H
+macro_line|#ifdef __KERNEL__
+macro_line|#include &lt;linux/if.h&gt;
+macro_line|#endif
+DECL|macro|IP6T_PHYSDEV_OP_IN
+mdefine_line|#define IP6T_PHYSDEV_OP_IN&t;&t;0x01
+DECL|macro|IP6T_PHYSDEV_OP_OUT
+mdefine_line|#define IP6T_PHYSDEV_OP_OUT&t;&t;0x02
+DECL|macro|IP6T_PHYSDEV_OP_BRIDGED
+mdefine_line|#define IP6T_PHYSDEV_OP_BRIDGED&t;&t;0x04
+DECL|macro|IP6T_PHYSDEV_OP_ISIN
+mdefine_line|#define IP6T_PHYSDEV_OP_ISIN&t;&t;0x08
+DECL|macro|IP6T_PHYSDEV_OP_ISOUT
+mdefine_line|#define IP6T_PHYSDEV_OP_ISOUT&t;&t;0x10
+DECL|macro|IP6T_PHYSDEV_OP_MASK
+mdefine_line|#define IP6T_PHYSDEV_OP_MASK&t;&t;(0x20 - 1)
+DECL|struct|ip6t_physdev_info
+r_struct
+id|ip6t_physdev_info
+(brace
+DECL|member|physindev
+r_char
+id|physindev
+(braket
+id|IFNAMSIZ
+)braket
+suffix:semicolon
+DECL|member|in_mask
+r_char
+id|in_mask
+(braket
+id|IFNAMSIZ
+)braket
+suffix:semicolon
+DECL|member|physoutdev
+r_char
+id|physoutdev
+(braket
+id|IFNAMSIZ
+)braket
+suffix:semicolon
+DECL|member|out_mask
+r_char
+id|out_mask
+(braket
+id|IFNAMSIZ
+)braket
+suffix:semicolon
+DECL|member|invert
+id|u_int8_t
+id|invert
+suffix:semicolon
+DECL|member|bitmask
+id|u_int8_t
+id|bitmask
+suffix:semicolon
+)brace
+suffix:semicolon
+macro_line|#endif /*_IP6T_PHYSDEV_H*/
+eof
