c73c2e86d6a0fa3eb7850b44247de20dddd6725d
Hirokazu Takata <takata.hirokazu@renesas.com>
2004-09-16 22:08:28 -0700

    [PATCH] m32r: upgrade include/asm-m32r/atomic.h
    Upgrade include/asm-m32r/atomic.h.
    
    * include/asm-m32r/atomic.h
    
    - Change atomic_add(), atomic_sub(), atomic_inc() and atomic_dec() from
      function to macro.
    
    - Change not to use fixed woking register in atomic_clear_mask() and
      atomic_set_mask().
    
    - Update comments: "return" to "return it", and so on.
    
    Signed-off-by: Hirokazu Takata <takata@linux-m32r.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-m32r/atomic.h
+++ include/asm-m32r/atomic.h
@@ -41,155 +41,7 @@ mdefine_line|#define atomic_read(v)&t;((v)-&gt;counter)
 multiline_comment|/**&n; * atomic_set - set atomic variable&n; * @v: pointer of type atomic_t&n; * @i: required value&n; *&n; * Atomically sets the value of @v to @i.&n; */
 DECL|macro|atomic_set
 mdefine_line|#define atomic_set(v,i)&t;(((v)-&gt;counter) = (i))
-multiline_comment|/**&n; * atomic_add - add integer to atomic variable&n; * @i: integer value to add&n; * @v: pointer of type atomic_t&n; *&n; * Atomically adds @i to @v.&n; */
-DECL|function|atomic_add
-r_static
-r_inline
-r_void
-id|atomic_add
-c_func
-(paren
-r_int
-id|i
-comma
-id|atomic_t
-op_star
-id|v
-)paren
-(brace
-r_int
-r_int
-id|flags
-suffix:semicolon
-id|local_irq_save
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-id|__asm__
-id|__volatile__
-(paren
-l_string|&quot;# atomic_add&t;&t;&t;&bslash;n&bslash;t&quot;
-id|DCACHE_CLEAR
-c_func
-(paren
-l_string|&quot;r4&quot;
-comma
-l_string|&quot;r5&quot;
-comma
-l_string|&quot;%0&quot;
-)paren
-id|LOAD
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;add&t;r4, %1;&t;&t;&t;&bslash;n&bslash;t&quot;
-id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-suffix:colon
-multiline_comment|/* no outputs */
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-op_amp
-id|v-&gt;counter
-)paren
-comma
-l_string|&quot;r&quot;
-(paren
-id|i
-)paren
-suffix:colon
-l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
-macro_line|#ifdef CONFIG_CHIP_M32700_TS1
-comma
-l_string|&quot;r5&quot;
-macro_line|#endif&t;/* CONFIG_CHIP_M32700_TS1 */
-)paren
-suffix:semicolon
-id|local_irq_restore
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-)brace
-multiline_comment|/**&n; * atomic_sub - subtract the atomic variable&n; * @i: integer value to subtract&n; * @v: pointer of type atomic_t&n; *&n; * Atomically subtracts @i from @v.&n; */
-DECL|function|atomic_sub
-r_static
-r_inline
-r_void
-id|atomic_sub
-c_func
-(paren
-r_int
-id|i
-comma
-id|atomic_t
-op_star
-id|v
-)paren
-(brace
-r_int
-r_int
-id|flags
-suffix:semicolon
-id|local_irq_save
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-id|__asm__
-id|__volatile__
-(paren
-l_string|&quot;# atomic_sub&t;&t;&t;&bslash;n&bslash;t&quot;
-id|DCACHE_CLEAR
-c_func
-(paren
-l_string|&quot;r4&quot;
-comma
-l_string|&quot;r5&quot;
-comma
-l_string|&quot;%0&quot;
-)paren
-id|LOAD
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;sub&t;r4, %1;&t;&t;&t;&bslash;n&bslash;t&quot;
-id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-suffix:colon
-multiline_comment|/* no outputs */
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-op_amp
-id|v-&gt;counter
-)paren
-comma
-l_string|&quot;r&quot;
-(paren
-id|i
-)paren
-suffix:colon
-l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
-macro_line|#ifdef CONFIG_CHIP_M32700_TS1
-comma
-l_string|&quot;r5&quot;
-macro_line|#endif&t;/* CONFIG_CHIP_M32700_TS1 */
-)paren
-suffix:semicolon
-id|local_irq_restore
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-)brace
-multiline_comment|/**&n; * atomic_add_return - add integer to atomic variable and return&n; * @i: integer value to add&n; * @v: pointer of type atomic_t&n; *&n; * Atomically adds @i to @v and return (@i + @v).&n; */
+multiline_comment|/**&n; * atomic_add_return - add integer to atomic variable and return it&n; * @i: integer value to add&n; * @v: pointer of type atomic_t&n; *&n; * Atomically adds @i to @v and return (@i + @v).&n; */
 DECL|function|atomic_add_return
 r_static
 r_inline
@@ -221,7 +73,7 @@ suffix:semicolon
 id|__asm__
 id|__volatile__
 (paren
-l_string|&quot;# atomic_add&t;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;# atomic_add_return&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
@@ -270,7 +122,7 @@ r_return
 id|result
 suffix:semicolon
 )brace
-multiline_comment|/**&n; * atomic_sub_return - subtract the atomic variable and return&n; * @i: integer value to subtract&n; * @v: pointer of type atomic_t&n; *&n; * Atomically subtracts @i from @v and return (@v - @i).&n; */
+multiline_comment|/**&n; * atomic_sub_return - subtract integer from atomic variable and return it&n; * @i: integer value to subtract&n; * @v: pointer of type atomic_t&n; *&n; * Atomically subtracts @i from @v and return (@v - @i).&n; */
 DECL|function|atomic_sub_return
 r_static
 r_inline
@@ -302,7 +154,7 @@ suffix:semicolon
 id|__asm__
 id|__volatile__
 (paren
-l_string|&quot;# atomic_sub&t;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;# atomic_sub_return&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
@@ -351,142 +203,16 @@ r_return
 id|result
 suffix:semicolon
 )brace
+multiline_comment|/**&n; * atomic_add - add integer to atomic variable&n; * @i: integer value to add&n; * @v: pointer of type atomic_t&n; *&n; * Atomically adds @i to @v.&n; */
+DECL|macro|atomic_add
+mdefine_line|#define atomic_add(i,v) ((void) atomic_add_return((i), (v)))
+multiline_comment|/**&n; * atomic_sub - subtract the atomic variable&n; * @i: integer value to subtract&n; * @v: pointer of type atomic_t&n; *&n; * Atomically subtracts @i from @v.&n; */
+DECL|macro|atomic_sub
+mdefine_line|#define atomic_sub(i,v) ((void) atomic_sub_return((i), (v)))
 multiline_comment|/**&n; * atomic_sub_and_test - subtract value from variable and test result&n; * @i: integer value to subtract&n; * @v: pointer of type atomic_t&n; *&n; * Atomically subtracts @i from @v and returns&n; * true if the result is zero, or false for all&n; * other cases.&n; */
 DECL|macro|atomic_sub_and_test
 mdefine_line|#define atomic_sub_and_test(i,v) (atomic_sub_return((i), (v)) == 0)
-multiline_comment|/**&n; * atomic_inc - increment atomic variable&n; * @v: pointer of type atomic_t&n; *&n; * Atomically increments @v by 1.&n; */
-DECL|function|atomic_inc
-r_static
-r_inline
-r_void
-id|atomic_inc
-c_func
-(paren
-id|atomic_t
-op_star
-id|v
-)paren
-(brace
-r_int
-r_int
-id|flags
-suffix:semicolon
-id|local_irq_save
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-id|__asm__
-id|__volatile__
-(paren
-l_string|&quot;# atomic_inc&t;&t;&t;&bslash;n&bslash;t&quot;
-id|DCACHE_CLEAR
-c_func
-(paren
-l_string|&quot;r4&quot;
-comma
-l_string|&quot;r5&quot;
-comma
-l_string|&quot;%0&quot;
-)paren
-id|LOAD
-l_string|&quot;&t;r4, @%0; &t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;addi&t;r4, #1;&t;&t;&t;&bslash;n&bslash;t&quot;
-id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-suffix:colon
-multiline_comment|/* no outputs */
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-op_amp
-id|v-&gt;counter
-)paren
-suffix:colon
-l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
-macro_line|#ifdef CONFIG_CHIP_M32700_TS1
-comma
-l_string|&quot;r5&quot;
-macro_line|#endif&t;/* CONFIG_CHIP_M32700_TS1 */
-)paren
-suffix:semicolon
-id|local_irq_restore
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-)brace
-multiline_comment|/**&n; * atomic_dec - decrement atomic variable&n; * @v: pointer of type atomic_t&n; *&n; * Atomically decrements @v by 1.&n; */
-DECL|function|atomic_dec
-r_static
-r_inline
-r_void
-id|atomic_dec
-c_func
-(paren
-id|atomic_t
-op_star
-id|v
-)paren
-(brace
-r_int
-r_int
-id|flags
-suffix:semicolon
-id|local_irq_save
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-id|__asm__
-id|__volatile__
-(paren
-l_string|&quot;# atomic_dec&t;&t;&t;&bslash;n&bslash;t&quot;
-id|DCACHE_CLEAR
-c_func
-(paren
-l_string|&quot;r4&quot;
-comma
-l_string|&quot;r5&quot;
-comma
-l_string|&quot;%0&quot;
-)paren
-id|LOAD
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;addi&t;r4, #-1;&t;&t;&bslash;n&bslash;t&quot;
-id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-suffix:colon
-multiline_comment|/* no outputs */
-suffix:colon
-l_string|&quot;r&quot;
-(paren
-op_amp
-id|v-&gt;counter
-)paren
-suffix:colon
-l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
-macro_line|#ifdef CONFIG_CHIP_M32700_TS1
-comma
-l_string|&quot;r5&quot;
-macro_line|#endif&t;/* CONFIG_CHIP_M32700_TS1 */
-)paren
-suffix:semicolon
-id|local_irq_restore
-c_func
-(paren
-id|flags
-)paren
-suffix:semicolon
-)brace
-multiline_comment|/**&n; * atomic_inc_return - increment atomic variable and return&n; * @v: pointer of type atomic_t&n; *&n; * Atomically increments @v by 1 and returns the result.&n; */
+multiline_comment|/**&n; * atomic_inc_return - increment atomic variable and return it&n; * @v: pointer of type atomic_t&n; *&n; * Atomically increments @v by 1 and returns the result.&n; */
 DECL|function|atomic_inc_return
 r_static
 r_inline
@@ -515,7 +241,7 @@ suffix:semicolon
 id|__asm__
 id|__volatile__
 (paren
-l_string|&quot;# atomic_dec_and_test&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;# atomic_inc_return&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
@@ -559,7 +285,7 @@ r_return
 id|result
 suffix:semicolon
 )brace
-multiline_comment|/**&n; * atomic_dec_return - decrement atomic variable and return&n; * @v: pointer of type atomic_t&n; *&n; * Atomically decrements @v by 1 and returns the result.&n; */
+multiline_comment|/**&n; * atomic_dec_return - decrement atomic variable and return it&n; * @v: pointer of type atomic_t&n; *&n; * Atomically decrements @v by 1 and returns the result.&n; */
 DECL|function|atomic_dec_return
 r_static
 r_inline
@@ -588,7 +314,7 @@ suffix:semicolon
 id|__asm__
 id|__volatile__
 (paren
-l_string|&quot;# atomic_dec_and_test&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;# atomic_dec_return&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
@@ -632,6 +358,12 @@ r_return
 id|result
 suffix:semicolon
 )brace
+multiline_comment|/**&n; * atomic_inc - increment atomic variable&n; * @v: pointer of type atomic_t&n; *&n; * Atomically increments @v by 1.&n; */
+DECL|macro|atomic_inc
+mdefine_line|#define atomic_inc(v) ((void)atomic_inc_return(v))
+multiline_comment|/**&n; * atomic_dec - decrement atomic variable&n; * @v: pointer of type atomic_t&n; *&n; * Atomically decrements @v by 1.&n; */
+DECL|macro|atomic_dec
+mdefine_line|#define atomic_dec(v) ((void)atomic_dec_return(v))
 multiline_comment|/**&n; * atomic_inc_and_test - increment and test&n; * @v: pointer of type atomic_t&n; *&n; * Atomically increments @v by 1&n; * and returns true if the result is zero, or false for all&n; * other cases.&n; */
 DECL|macro|atomic_inc_and_test
 mdefine_line|#define atomic_inc_and_test(v) (atomic_inc_return(v) == 0)
@@ -661,6 +393,10 @@ r_int
 r_int
 id|flags
 suffix:semicolon
+r_int
+r_int
+id|tmp
+suffix:semicolon
 id|local_irq_save
 c_func
 (paren
@@ -670,23 +406,26 @@ suffix:semicolon
 id|__asm__
 id|__volatile__
 (paren
-l_string|&quot;# atomic_set_mask&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;# atomic_clear_mask&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
-l_string|&quot;r4&quot;
+l_string|&quot;%0&quot;
 comma
 l_string|&quot;r5&quot;
 comma
-l_string|&quot;%0&quot;
+l_string|&quot;%1&quot;
 )paren
 id|LOAD
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;and&t;r4, %1;&t;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;&t;%0, @%1;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;and&t;%0, %2;&t;&t;&t;&bslash;n&bslash;t&quot;
 id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;&t;%0, @%1;&t;&t;&bslash;n&bslash;t&quot;
 suffix:colon
-multiline_comment|/* no outputs */
+l_string|&quot;=&amp;r&quot;
+(paren
+id|tmp
+)paren
 suffix:colon
 l_string|&quot;r&quot;
 (paren
@@ -700,8 +439,6 @@ id|mask
 )paren
 suffix:colon
 l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
 macro_line|#ifdef CONFIG_CHIP_M32700_TS1
 comma
 l_string|&quot;r5&quot;
@@ -735,6 +472,10 @@ r_int
 r_int
 id|flags
 suffix:semicolon
+r_int
+r_int
+id|tmp
+suffix:semicolon
 id|local_irq_save
 c_func
 (paren
@@ -748,19 +489,22 @@ l_string|&quot;# atomic_set_mask&t;&t;&bslash;n&bslash;t&quot;
 id|DCACHE_CLEAR
 c_func
 (paren
-l_string|&quot;r4&quot;
+l_string|&quot;%0&quot;
 comma
 l_string|&quot;r5&quot;
 comma
-l_string|&quot;%0&quot;
+l_string|&quot;%1&quot;
 )paren
 id|LOAD
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
-l_string|&quot;or&t;r4, %1;&t;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;&t;%0, @%1;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;or&t;%0, %2;&t;&t;&t;&bslash;n&bslash;t&quot;
 id|STORE
-l_string|&quot;&t;r4, @%0;&t;&t;&bslash;n&bslash;t&quot;
+l_string|&quot;&t;%0, @%1;&t;&t;&bslash;n&bslash;t&quot;
 suffix:colon
-multiline_comment|/* no outputs */
+l_string|&quot;=&amp;r&quot;
+(paren
+id|tmp
+)paren
 suffix:colon
 l_string|&quot;r&quot;
 (paren
@@ -773,8 +517,6 @@ id|mask
 )paren
 suffix:colon
 l_string|&quot;memory&quot;
-comma
-l_string|&quot;r4&quot;
 macro_line|#ifdef CONFIG_CHIP_M32700_TS1
 comma
 l_string|&quot;r5&quot;
