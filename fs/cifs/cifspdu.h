4c2d538654371851ebeda9c15aba4c4b127a6021
Alexander Viro <viro@www.linux.org.uk>
2004-09-21 02:48:38 -0700

    [PATCH] cifs: annotate RENAME_{REQ,RESP}
    Signed-off-by: Al Viro <viro@parcelfarce.linux.org.uk>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- fs/cifs/cifspdu.h
+++ fs/cifs/cifspdu.h
@@ -1907,12 +1907,12 @@ id|hdr
 suffix:semicolon
 multiline_comment|/* wct = 1 */
 DECL|member|SearchAttributes
-id|__u16
+id|__le16
 id|SearchAttributes
 suffix:semicolon
 multiline_comment|/* target file attributes */
 DECL|member|ByteCount
-id|__u16
+id|__le16
 id|ByteCount
 suffix:semicolon
 DECL|member|BufferFormat
