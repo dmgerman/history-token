6ffd6ee3295301ada5c6a4e769a65f2358ddaafc
Patrick McHardy <kaber@coreworks.de>
2004-09-23 00:52:33 -0700

    [NETFILTER]: add comment match
    2.4 version by Brad Fisher <brad@info-link.net>
    
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: David S. Miller <davem@davemloft.net>

---
+++ include/linux/netfilter_ipv4/ipt_comment.h
@@ -0,0 +1,21 @@
+macro_line|#ifndef _IPT_COMMENT_H
+DECL|macro|_IPT_COMMENT_H
+mdefine_line|#define _IPT_COMMENT_H
+DECL|macro|IPT_MAX_COMMENT_LEN
+mdefine_line|#define IPT_MAX_COMMENT_LEN 256
+DECL|struct|ipt_comment_info
+r_struct
+id|ipt_comment_info
+(brace
+DECL|member|comment
+r_int
+r_char
+id|comment
+(braket
+id|IPT_MAX_COMMENT_LEN
+)braket
+suffix:semicolon
+)brace
+suffix:semicolon
+macro_line|#endif /* _IPT_COMMENT_H */
+eof
