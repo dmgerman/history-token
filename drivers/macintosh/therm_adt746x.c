28e01ccc3df287630ea736c11483bec031b6334b
Colin Leroy <colin@colino.net>
2004-10-09 01:03:50 -0700

    [PATCH] therm_adt746x: various fixes
    This one removes the other occurences of "°C"; fixes displayed fan speed so
    that it uses the same scale than other occurences of this parameter instead
    of RPM only; fixes the RPM reading of the fan so that it shows zero instead
    of 82 when it is effectively stopped.
    
    Signed-off-by: Colin Leroy <colin@colino.net>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/macintosh/therm_adt746x.c
+++ drivers/macintosh/therm_adt746x.c
@@ -197,7 +197,7 @@ c_func
 (paren
 id|limit_adjust
 comma
-l_string|&quot;Adjust maximum temperatures (50&#xfffd;C cpu, 70&#xfffd;C gpu) by N &#xfffd;C.&quot;
+l_string|&quot;Adjust maximum temperatures (50 cpu, 70 gpu) by N degrees.&quot;
 )paren
 suffix:semicolon
 id|MODULE_PARM
@@ -658,7 +658,7 @@ c_func
 (paren
 id|KERN_INFO
 l_string|&quot;adt746x: Putting max temperatures back from %d, %d, %d,&quot;
-l_string|&quot; to %d, %d, %d, (&#xfffd;C)&bslash;n&quot;
+l_string|&quot; to %d, %d, %d&bslash;n&quot;
 comma
 id|th-&gt;limits
 (braket
@@ -856,7 +856,16 @@ op_lshift
 l_int|8
 )paren
 suffix:semicolon
+multiline_comment|/* &quot;a value of 0xffff means that the fan has stopped&quot; */
 r_return
+(paren
+id|res
+op_eq
+l_int|0xffff
+ques
+c_cond
+l_int|0
+suffix:colon
 (paren
 l_int|90000
 op_star
@@ -864,6 +873,7 @@ l_int|60
 )paren
 op_div
 id|res
+)paren
 suffix:semicolon
 )brace
 DECL|function|write_both_fan_speed
@@ -1360,7 +1370,7 @@ id|printk
 c_func
 (paren
 id|KERN_INFO
-l_string|&quot;adt746x: Limit exceeded by %d&#xfffd;C, overriding specified fan speed for %s.&bslash;n&quot;
+l_string|&quot;adt746x: Limit exceeded by %d, overriding specified fan speed for %s.&bslash;n&quot;
 comma
 id|var
 comma
@@ -1429,7 +1439,7 @@ id|printk
 c_func
 (paren
 id|KERN_INFO
-l_string|&quot;adt746x: Limit exceeded by %d&#xfffd;C, setting speed to specified for %s.&bslash;n&quot;
+l_string|&quot;adt746x: Limit exceeded by %d, setting speed to specified for %s.&bslash;n&quot;
 comma
 id|var
 comma
@@ -1599,8 +1609,8 @@ c_func
 (paren
 id|KERN_INFO
 l_string|&quot;adt746x: Temperature infos:&quot;
-l_string|&quot; thermostats: %d,%d,%d &#xfffd;C;&quot;
-l_string|&quot; limits: %d,%d,%d &#xfffd;C;&quot;
+l_string|&quot; thermostats: %d,%d,%d;&quot;
+l_string|&quot; limits: %d,%d,%d;&quot;
 l_string|&quot; fan speed: %d RPM&bslash;n&quot;
 comma
 id|temps
@@ -1967,7 +1977,7 @@ c_func
 (paren
 id|KERN_INFO
 l_string|&quot;adt746x: Lowering max temperatures from %d, %d, %d&quot;
-l_string|&quot; to %d, %d, %d (&#xfffd;C)&bslash;n&quot;
+l_string|&quot; to %d, %d, %d&bslash;n&quot;
 comma
 id|th-&gt;initial_limits
 (braket
@@ -2132,10 +2142,12 @@ suffix:semicolon
 multiline_comment|/* &n; * Now, unfortunately, sysfs doesn&squot;t give us a nice void * we could&n; * pass around to the attribute functions, so we don&squot;t really have&n; * choice but implement a bunch of them...&n; *&n; */
 DECL|macro|BUILD_SHOW_FUNC_INT
 mdefine_line|#define BUILD_SHOW_FUNC_INT(name, data)&t;&t;&t;&t;&bslash;&n;static ssize_t show_##name(struct device *dev, char *buf)&t;&bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;return sprintf(buf, &quot;%d&bslash;n&quot;, data);&t;&t;&t;&bslash;&n;}
+DECL|macro|BUILD_SHOW_FUNC_FAN
+mdefine_line|#define BUILD_SHOW_FUNC_FAN(name, data)&t;&t;&t;&t;&bslash;&n;static ssize_t show_##name(struct device *dev, char *buf)       &bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;return sprintf(buf, &quot;%d (%d rpm)&bslash;n&quot;, &t;&t;&t;&bslash;&n;&t;&t;thermostat-&gt;last_speed[data],&t;&t;&t;&bslash;&n;&t;&t;read_fan_speed(thermostat, FAN_SPEED[data])&t;&bslash;&n;&t;&t;);&t;&t;&t;&t;&t;&t;&bslash;&n;}
 DECL|macro|BUILD_STORE_FUNC_DEG
 mdefine_line|#define BUILD_STORE_FUNC_DEG(name, data)&t;&t;&t;&bslash;&n;static ssize_t store_##name(struct device *dev, const char *buf, size_t n) &bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;int val;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;int i;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;val = simple_strtol(buf, NULL, 10);&t;&t;&t;&bslash;&n;&t;printk(KERN_INFO &quot;Adjusting limits by %d&#xfffd;C&bslash;n&quot;, val);&t;&bslash;&n;&t;limit_adjust = val;&t;&t;&t;&t;&t;&bslash;&n;&t;for (i=0; i &lt; 3; i++)&t;&t;&t;&t;&t;&bslash;&n;&t;&t;set_limit(thermostat, i);&t;&t;&t;&bslash;&n;&t;return n;&t;&t;&t;&t;&t;&t;&bslash;&n;}
 DECL|macro|BUILD_STORE_FUNC_INT
-mdefine_line|#define BUILD_STORE_FUNC_INT(name, data)&t;&t;&t;&bslash;&n;static ssize_t store_##name(struct device *dev, const char *buf, size_t n) &bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;u32 val;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;val = simple_strtoul(buf, NULL, 10);&t;&t;&t;&bslash;&n;&t;if (val &lt; 0 || val &gt; 255)&t;&t;&t;&t;&bslash;&n;&t;&t;return -EINVAL;&t;&t;&t;&t;&t;&bslash;&n;&t;printk(KERN_INFO &quot;Setting fan speed to %d&bslash;n&quot;, val);&t;&bslash;&n;&t;data = val;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;return n;&t;&t;&t;&t;&t;&t;&bslash;&n;}
+mdefine_line|#define BUILD_STORE_FUNC_INT(name, data)&t;&t;&t;&bslash;&n;static ssize_t store_##name(struct device *dev, const char *buf, size_t n) &bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;u32 val;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;val = simple_strtoul(buf, NULL, 10);&t;&t;&t;&bslash;&n;&t;if (val &lt; 0 || val &gt; 255)&t;&t;&t;&t;&bslash;&n;&t;&t;return -EINVAL;&t;&t;&t;&t;&t;&bslash;&n;&t;printk(KERN_INFO &quot;Setting specified fan speed to %d&bslash;n&quot;, val);&t;&bslash;&n;&t;data = val;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;return n;&t;&t;&t;&t;&t;&t;&bslash;&n;}
 id|BUILD_SHOW_FUNC_INT
 c_func
 (paren
@@ -2199,41 +2211,19 @@ id|specified_fan_speed
 comma
 id|fan_speed
 )paren
-id|BUILD_SHOW_FUNC_INT
+id|BUILD_SHOW_FUNC_FAN
 c_func
 (paren
 id|cpu_fan_speed
 comma
-(paren
-id|read_fan_speed
-c_func
-(paren
-id|thermostat
-comma
-id|FAN_SPEED
-(braket
 l_int|0
-)braket
 )paren
-)paren
-)paren
-id|BUILD_SHOW_FUNC_INT
+id|BUILD_SHOW_FUNC_FAN
 c_func
 (paren
 id|gpu_fan_speed
 comma
-(paren
-id|read_fan_speed
-c_func
-(paren
-id|thermostat
-comma
-id|FAN_SPEED
-(braket
 l_int|1
-)braket
-)paren
-)paren
 )paren
 id|BUILD_STORE_FUNC_INT
 c_func
