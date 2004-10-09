27ed56b74cafd2ca139bf0e0b83351d7590c8bdf
Sascha Hauer <sascha@de.rmk.(none)>
2004-10-10 00:05:21 +0100

    [ARM PATCH] 2073/3: Hynix h720x architecture support
    Patch from Sascha Hauer
    
    This patch supports the Hynix developer boards h7201 and h7202 for the
    HMS30C7201 and HMS30C7202 CPUs.
    
    Changes to previous patch:
    - reorganized file structure. We now have common.c which shares stuff
      common to all h720x processors and cpu-h720[12].c files for the cpu
      specific stuff
    - fixed timer handling for timers 1/2
    - cleaned up h720x register definitions and splitted in files specific
      to each cpu and one generic part
    
    Signed-off-by: Thomas Gleixner
    Signed-off-by: Robert Schwebel
    Signed-off-by: Sascha Hauer

---
+++ include/asm-arm/arch-h720x/io.h
@@ -0,0 +1,13 @@
+multiline_comment|/*&n; * linux/include/asm-arm/arch-h720x/io.h&n; *&n; * Copyright (C) 2000 Steve Hill (sjhill@cotw.com)&n; *&n; * Changelog:&n; *&n; *  09-19-2001&t;JJKIM&n; *  &t;&t;Created from linux/include/asm-arm/arch-l7200/io.h&n; *&n; *  03-27-2003  Robert Schwebel &lt;r.schwebel@pengutronix.de&gt;:&n; *  &t;&t;re-unified header files for h720x&n; */
+macro_line|#ifndef __ASM_ARM_ARCH_IO_H
+DECL|macro|__ASM_ARM_ARCH_IO_H
+mdefine_line|#define __ASM_ARM_ARCH_IO_H
+macro_line|#include &lt;asm/arch/hardware.h&gt;
+DECL|macro|IO_SPACE_LIMIT
+mdefine_line|#define IO_SPACE_LIMIT 0xffffffff
+DECL|macro|__io
+mdefine_line|#define __io(a) (a)
+DECL|macro|__mem_pci
+mdefine_line|#define __mem_pci(a) (a)
+macro_line|#endif
+eof
