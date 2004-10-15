278f5102957b155c9b19c7a348427027b663b410
Stephen Hemminger <shemminger@osdl.org>
2004-10-15 11:43:15 -0400

    [PATCH] ns83820: use module_param
    Replace MODULE_PARM with module_param
    
    Signed-off-by: Stephen Hemminger <shemminger@osdl.org>

--- drivers/net/ns83820.c
+++ drivers/net/ns83820.c
@@ -5,6 +5,7 @@ singleline_comment|//#define dprintk&t;&t;printk
 DECL|macro|dprintk
 mdefine_line|#define dprintk(x...)&t;&t;do { } while (0)
 macro_line|#include &lt;linux/module.h&gt;
+macro_line|#include &lt;linux/moduleparam.h&gt;
 macro_line|#include &lt;linux/types.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
 macro_line|#include &lt;linux/netdevice.h&gt;
@@ -25,7 +26,7 @@ macro_line|#include &lt;asm/uaccess.h&gt;
 macro_line|#include &lt;asm/system.h&gt;
 DECL|macro|DRV_NAME
 mdefine_line|#define DRV_NAME &quot;ns83820&quot;
-multiline_comment|/* Global parameters.  See MODULE_PARM near the bottom. */
+multiline_comment|/* Global parameters.  See module_param near the bottom. */
 DECL|variable|ihr
 r_static
 r_int
@@ -9033,12 +9034,14 @@ comma
 id|ns83820_pci_tbl
 )paren
 suffix:semicolon
-id|MODULE_PARM
+id|module_param
 c_func
 (paren
 id|lnksts
 comma
-l_string|&quot;i&quot;
+r_int
+comma
+l_int|0
 )paren
 suffix:semicolon
 id|MODULE_PARM_DESC
@@ -9049,12 +9052,14 @@ comma
 l_string|&quot;Polarity of LNKSTS bit&quot;
 )paren
 suffix:semicolon
-id|MODULE_PARM
+id|module_param
 c_func
 (paren
 id|ihr
 comma
-l_string|&quot;i&quot;
+r_int
+comma
+l_int|0
 )paren
 suffix:semicolon
 id|MODULE_PARM_DESC
@@ -9065,12 +9070,14 @@ comma
 l_string|&quot;Time in 100 us increments to delay interrupts (range 0-127)&quot;
 )paren
 suffix:semicolon
-id|MODULE_PARM
+id|module_param
 c_func
 (paren
 id|reset_phy
 comma
-l_string|&quot;i&quot;
+r_int
+comma
+l_int|0
 )paren
 suffix:semicolon
 id|MODULE_PARM_DESC
