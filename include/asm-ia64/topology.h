04a2a426731ef3710711af11e3df1e8fa0420022
Matthew Dobson <colpatch@us.ibm.com>
2004-10-18 09:11:27 -0700

    [PATCH] sched_domains: Make SD_NODE_INIT per-arch #2
    Here's yet another version of a patch to implement per-arch SD_*_INITs.
    This follows the same basic idea of my last patch, but
    
    1) defines an arch-specific SD_NODE_INIT for the 4 NUMA arches (i386,
       x86_64, IA64 & PPC64),
    
    2) defines *default* SD_CPU_INIT & SD_SIBLING_INIT for *all* arches,
       with the possibility of them being overridden by simply defining an
       arch-specific version in include/asm/topology.h.
    
    The motivation behind the third version of this patch is that Martin feels
    that there should be no "default" NUMA initializer because NUMA
    characteristics are *very* arch/platform specific, and hence a "default"
    NUMA initializer can only lead to confusion.  I agree with most of that,
    but don't quite see as much harm in having a default as he does.
    Nevertheless, to keep him quiet, I've run up this version of the patch.
    Martin, please run this through your magic test suite and make sure I
    didn't break anything trivial.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ia64/topology.h
+++ include/asm-ia64/topology.h
@@ -28,6 +28,9 @@ c_func
 r_void
 )paren
 suffix:semicolon
+multiline_comment|/* sched_domains SD_NODE_INIT for IA64 NUMA machines */
+DECL|macro|SD_NODE_INIT
+mdefine_line|#define SD_NODE_INIT (struct sched_domain) {&t;&t;&bslash;&n;&t;.span&t;&t;&t;= CPU_MASK_NONE,&t;&bslash;&n;&t;.parent&t;&t;&t;= NULL,&t;&t;&t;&bslash;&n;&t;.groups&t;&t;&t;= NULL,&t;&t;&t;&bslash;&n;&t;.min_interval&t;&t;= 80,&t;&t;&t;&bslash;&n;&t;.max_interval&t;&t;= 320,&t;&t;&t;&bslash;&n;&t;.busy_factor&t;&t;= 320,&t;&t;&t;&bslash;&n;&t;.imbalance_pct&t;&t;= 125,&t;&t;&t;&bslash;&n;&t;.cache_hot_time&t;&t;= (10*1000000),&t;&t;&bslash;&n;&t;.cache_nice_tries&t;= 1,&t;&t;&t;&bslash;&n;&t;.per_cpu_gain&t;&t;= 100,&t;&t;&t;&bslash;&n;&t;.flags&t;&t;&t;= SD_LOAD_BALANCE&t;&bslash;&n;&t;&t;&t;&t;| SD_BALANCE_EXEC&t;&bslash;&n;&t;&t;&t;&t;| SD_WAKE_BALANCE,&t;&bslash;&n;&t;.last_balance&t;&t;= jiffies,&t;&t;&bslash;&n;&t;.balance_interval&t;= 10,&t;&t;&t;&bslash;&n;&t;.nr_balance_failed&t;= 0,&t;&t;&t;&bslash;&n;}
 macro_line|#endif /* CONFIG_NUMA */
 macro_line|#include &lt;asm-generic/topology.h&gt;
 macro_line|#endif /* _ASM_IA64_TOPOLOGY_H */
