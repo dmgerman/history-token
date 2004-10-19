63cf71574088aeca49af5975020dc022e135beb6
David Howells <dhowells@redhat.com>
2004-10-18 17:58:25 -0700

    [PATCH] Add some key management specific error codes
    Here's a patch to add some new error codes specific to key management.
    
    Signed-Off-By: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-generic/errno.h
+++ include/asm-generic/errno.h
@@ -182,5 +182,13 @@ DECL|macro|ENOMEDIUM
 mdefine_line|#define&t;ENOMEDIUM&t;123&t;/* No medium found */
 DECL|macro|EMEDIUMTYPE
 mdefine_line|#define&t;EMEDIUMTYPE&t;124&t;/* Wrong medium type */
+DECL|macro|ENOKEY
+mdefine_line|#define&t;ENOKEY&t;&t;125&t;/* Required key not available */
+DECL|macro|EKEYEXPIRED
+mdefine_line|#define&t;EKEYEXPIRED&t;126&t;/* Key has expired */
+DECL|macro|EKEYREVOKED
+mdefine_line|#define&t;EKEYREVOKED&t;127&t;/* Key has been revoked */
+DECL|macro|EKEYREJECTED
+mdefine_line|#define&t;EKEYREJECTED&t;128&t;/* Key was rejected by service */
 macro_line|#endif
 eof
