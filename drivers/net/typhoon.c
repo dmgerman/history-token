3d7c31599dcc3e6a0a99ac07f9da5ab3421fc888
Andrew Morton <akpm@osdl.org>
2004-10-19 18:46:19 -0700

    [PATCH] typhoon build fix
    Fix incorrect attempt to doubly-initialise the ethtool ops.
    
    Cc: <jgarzik@redhat.com>
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/net/typhoon.c
+++ drivers/net/typhoon.c
@@ -10938,11 +10938,6 @@ id|dev-&gt;poll
 op_assign
 id|typhoon_poll
 suffix:semicolon
-id|dev-&gt;ethtool_ops
-op_assign
-op_amp
-id|typhoon_ethtool_ops
-suffix:semicolon
 id|dev-&gt;weight
 op_assign
 l_int|16
@@ -10973,7 +10968,7 @@ c_func
 id|dev
 comma
 op_amp
-id|ops
+id|typhoon_ethtool_ops
 )paren
 suffix:semicolon
 multiline_comment|/* We can handle scatter gather, up to 16 entries, and&n;&t; * we can do IP checksumming (only version 4, doh...)&n;&t; */
