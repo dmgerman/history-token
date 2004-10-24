6bba7afbf92c9be057d446d3c14dd9f9a2c50952
Bartlomiej Zolnierkiewicz <bzolnier@trik.(none)>
2004-10-24 02:49:23 +0200

    [ide] cs5520: kill /proc/ide/cs5520
    Signed-off-by: Bartlomiej Zolnierkiewicz <bzolnier@gmail.com>

--- drivers/ide/pci/cs5520.c
+++ drivers/ide/pci/cs5520.c
@@ -15,267 +15,6 @@ macro_line|#include &lt;linux/pci.h&gt;
 macro_line|#include &lt;linux/ide.h&gt;
 macro_line|#include &lt;asm/io.h&gt;
 macro_line|#include &lt;asm/irq.h&gt;
-DECL|macro|DISPLAY_CS5520_TIMINGS
-mdefine_line|#define DISPLAY_CS5520_TIMINGS
-macro_line|#if defined(DISPLAY_CS5520_TIMINGS) &amp;&amp; defined(CONFIG_PROC_FS)
-macro_line|#include &lt;linux/stat.h&gt;
-macro_line|#include &lt;linux/proc_fs.h&gt;
-DECL|variable|cs5520_proc
-r_static
-id|u8
-id|cs5520_proc
-op_assign
-l_int|0
-suffix:semicolon
-DECL|variable|bmide_dev
-r_static
-r_struct
-id|pci_dev
-op_star
-id|bmide_dev
-suffix:semicolon
-DECL|function|cs5520_get_info
-r_static
-r_int
-id|cs5520_get_info
-c_func
-(paren
-r_char
-op_star
-id|buffer
-comma
-r_char
-op_star
-op_star
-id|addr
-comma
-id|off_t
-id|offset
-comma
-r_int
-id|count
-)paren
-(brace
-r_char
-op_star
-id|p
-op_assign
-id|buffer
-suffix:semicolon
-r_int
-r_int
-id|bmiba
-op_assign
-id|pci_resource_start
-c_func
-(paren
-id|bmide_dev
-comma
-l_int|2
-)paren
-suffix:semicolon
-r_int
-id|len
-suffix:semicolon
-id|u8
-id|c0
-op_assign
-l_int|0
-comma
-id|c1
-op_assign
-l_int|0
-suffix:semicolon
-id|u16
-id|reg16
-suffix:semicolon
-id|u32
-id|reg32
-suffix:semicolon
-multiline_comment|/*&n;&t; * at that point bibma+0x2 et bibma+0xa are byte registers&n;&t; * to investigate:&n;&t; */
-id|c0
-op_assign
-id|inb
-c_func
-(paren
-id|bmiba
-op_plus
-l_int|0x02
-)paren
-suffix:semicolon
-id|c1
-op_assign
-id|inb
-c_func
-(paren
-id|bmiba
-op_plus
-l_int|0x0a
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;&bslash;nCyrix CS55x0 IDE&bslash;n&quot;
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;--------------- Primary Channel &quot;
-l_string|&quot;---------------- Secondary Channel &quot;
-l_string|&quot;-------------&bslash;n&quot;
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;                %sabled &quot;
-l_string|&quot;                        %sabled&bslash;n&quot;
-comma
-(paren
-id|c0
-op_amp
-l_int|0x80
-)paren
-ques
-c_cond
-l_string|&quot;dis&quot;
-suffix:colon
-l_string|&quot; en&quot;
-comma
-(paren
-id|c1
-op_amp
-l_int|0x80
-)paren
-ques
-c_cond
-l_string|&quot;dis&quot;
-suffix:colon
-l_string|&quot; en&quot;
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;&bslash;n&bslash;nTimings: &bslash;n&quot;
-)paren
-suffix:semicolon
-id|pci_read_config_word
-c_func
-(paren
-id|bmide_dev
-comma
-l_int|0x62
-comma
-op_amp
-id|reg16
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;8bit CAT/CRT   : %04x&bslash;n&quot;
-comma
-id|reg16
-)paren
-suffix:semicolon
-id|pci_read_config_dword
-c_func
-(paren
-id|bmide_dev
-comma
-l_int|0x64
-comma
-op_amp
-id|reg32
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;16bit Primary  : %08x&bslash;n&quot;
-comma
-id|reg32
-)paren
-suffix:semicolon
-id|pci_read_config_dword
-c_func
-(paren
-id|bmide_dev
-comma
-l_int|0x68
-comma
-op_amp
-id|reg32
-)paren
-suffix:semicolon
-id|p
-op_add_assign
-id|sprintf
-c_func
-(paren
-id|p
-comma
-l_string|&quot;16bit Secondary: %08x&bslash;n&quot;
-comma
-id|reg32
-)paren
-suffix:semicolon
-id|len
-op_assign
-(paren
-id|p
-op_minus
-id|buffer
-)paren
-op_minus
-id|offset
-suffix:semicolon
-op_star
-id|addr
-op_assign
-id|buffer
-op_plus
-id|offset
-suffix:semicolon
-r_return
-id|len
-OG
-id|count
-ques
-c_cond
-id|count
-suffix:colon
-id|len
-suffix:semicolon
-)brace
-macro_line|#endif
 DECL|struct|pio_clocks
 r_struct
 id|pio_clocks
@@ -421,7 +160,7 @@ id|pio
 )paren
 suffix:semicolon
 multiline_comment|/* FIXME: if DMA = 1 do we need to set the DMA bit here ? */
-multiline_comment|/* 8bit command timing for channel */
+multiline_comment|/* 8bit CAT/CRT - 8bit command timing for channel */
 id|pci_write_config_byte
 c_func
 (paren
@@ -452,6 +191,7 @@ m_assert
 )paren
 )paren
 suffix:semicolon
+multiline_comment|/* 0x64 - 16bit Primary, 0x68 - 16bit Secondary */
 multiline_comment|/* FIXME: should these use address ? */
 multiline_comment|/* Data read timing */
 id|pci_write_config_byte
@@ -693,55 +433,6 @@ id|drive
 )paren
 suffix:semicolon
 )brace
-DECL|function|init_chipset_cs5520
-r_static
-r_int
-r_int
-id|__devinit
-id|init_chipset_cs5520
-c_func
-(paren
-r_struct
-id|pci_dev
-op_star
-id|dev
-comma
-r_const
-r_char
-op_star
-id|name
-)paren
-(brace
-macro_line|#if defined(DISPLAY_CS5520_TIMINGS) &amp;&amp; defined(CONFIG_PROC_FS)
-r_if
-c_cond
-(paren
-op_logical_neg
-id|cs5520_proc
-)paren
-(brace
-id|cs5520_proc
-op_assign
-l_int|1
-suffix:semicolon
-id|bmide_dev
-op_assign
-id|dev
-suffix:semicolon
-id|ide_pci_create_host_proc
-c_func
-(paren
-l_string|&quot;cs5520&quot;
-comma
-id|cs5520_get_info
-)paren
-suffix:semicolon
-)brace
-macro_line|#endif /* DISPLAY_CS5520_TIMINGS &amp;&amp; CONFIG_PROC_FS */
-r_return
-l_int|0
-suffix:semicolon
-)brace
 multiline_comment|/*&n; *&t;We provide a callback for our nonstandard DMA location&n; */
 DECL|function|cs5520_init_setup_dma
 r_static
@@ -930,7 +621,7 @@ id|hwif-&gt;autodma
 suffix:semicolon
 )brace
 DECL|macro|DECLARE_CS_DEV
-mdefine_line|#define DECLARE_CS_DEV(name_str)&t;&t;&t;&t;&bslash;&n;&t;{&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;&t;.name&t;&t;= name_str,&t;&t;&t;&bslash;&n;&t;&t;.init_chipset&t;= init_chipset_cs5520,&t;&t;&bslash;&n;&t;&t;.init_setup_dma = cs5520_init_setup_dma,&t;&bslash;&n;&t;&t;.init_hwif&t;= init_hwif_cs5520,&t;&t;&bslash;&n;&t;&t;.channels&t;= 2,&t;&t;&t;&t;&bslash;&n;&t;&t;.autodma&t;= AUTODMA,&t;&t;&t;&bslash;&n;&t;&t;.bootable&t;= ON_BOARD,&t;&t;&t;&bslash;&n;&t;&t;.flags&t;&t;= IDEPCI_FLAG_ISA_PORTS,&t;&bslash;&n;&t;}
+mdefine_line|#define DECLARE_CS_DEV(name_str)&t;&t;&t;&t;&bslash;&n;&t;{&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;&t;.name&t;&t;= name_str,&t;&t;&t;&bslash;&n;&t;&t;.init_setup_dma = cs5520_init_setup_dma,&t;&bslash;&n;&t;&t;.init_hwif&t;= init_hwif_cs5520,&t;&t;&bslash;&n;&t;&t;.channels&t;= 2,&t;&t;&t;&t;&bslash;&n;&t;&t;.autodma&t;= AUTODMA,&t;&t;&t;&bslash;&n;&t;&t;.bootable&t;= ON_BOARD,&t;&t;&t;&bslash;&n;&t;&t;.flags&t;&t;= IDEPCI_FLAG_ISA_PORTS,&t;&bslash;&n;&t;}
 DECL|variable|__devinitdata
 r_static
 id|ide_pci_device_t
@@ -1054,14 +745,6 @@ op_minus
 id|ENODEV
 suffix:semicolon
 )brace
-id|init_chipset_cs5520
-c_func
-(paren
-id|dev
-comma
-id|d-&gt;name
-)paren
-suffix:semicolon
 id|index.all
 op_assign
 l_int|0xf0f0
