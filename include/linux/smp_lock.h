9345a969cc71191b6a70f231773bdffdc11865d6
Linus Torvalds <torvalds@ppc970.osdl.org>
2004-10-25 01:21:01 -0700

    Allow BKL re-acquire to fail, causing us to re-schedule.
    This allows for low-latency BKL contention even with
    preemption. Previously, since preemption is disabled
    over context switches, re-acquiring the kernel lock when
    resuming a process would be non-preemtible.

--- include/linux/smp_lock.h
+++ include/linux/smp_lock.h
@@ -8,7 +8,7 @@ macro_line|#ifdef CONFIG_LOCK_KERNEL
 DECL|macro|kernel_locked
 mdefine_line|#define kernel_locked()&t;&t;(current-&gt;lock_depth &gt;= 0)
 r_extern
-r_void
+r_int
 id|__lockfunc
 id|get_kernel_lock
 c_func
@@ -28,8 +28,48 @@ suffix:semicolon
 multiline_comment|/*&n; * Release/re-acquire global kernel lock for the scheduler&n; */
 DECL|macro|release_kernel_lock
 mdefine_line|#define release_kernel_lock(tsk) do { &t;&t;&bslash;&n;&t;if (unlikely((tsk)-&gt;lock_depth &gt;= 0))&t;&bslash;&n;&t;&t;put_kernel_lock();&t;&t;&bslash;&n;} while (0)
-DECL|macro|reacquire_kernel_lock
-mdefine_line|#define reacquire_kernel_lock(tsk) do {&t;&bslash;&n;&t;if (unlikely((tsk)-&gt;lock_depth &gt;= 0))&t;&bslash;&n;&t;&t;get_kernel_lock();&t;&t;&bslash;&n;} while (0)
+multiline_comment|/*&n; * Non-SMP kernels will never block on the kernel lock,&n; * so we are better off returning a constant zero from&n; * reacquire_kernel_lock() so that the compiler can see&n; * it at compile-time.&n; */
+macro_line|#ifdef CONFIG_SMP
+DECL|macro|return_value_on_smp
+mdefine_line|#define return_value_on_smp return
+macro_line|#else
+DECL|macro|return_value_on_smp
+mdefine_line|#define return_value_on_smp
+macro_line|#endif
+DECL|function|reacquire_kernel_lock
+r_static
+r_inline
+r_int
+id|reacquire_kernel_lock
+c_func
+(paren
+r_struct
+id|task_struct
+op_star
+id|task
+)paren
+(brace
+r_if
+c_cond
+(paren
+id|unlikely
+c_func
+(paren
+id|task-&gt;lock_depth
+op_ge
+l_int|0
+)paren
+)paren
+id|return_value_on_smp
+id|get_kernel_lock
+c_func
+(paren
+)paren
+suffix:semicolon
+r_return
+l_int|0
+suffix:semicolon
+)brace
 DECL|variable|kernel_lock
 r_extern
 r_void
