219539e39309ca258774c0ed96e0ea0cad432766
David Howells <dhowells@redhat.com>
2004-10-25 04:05:11 -0700

    [PATCH] Shift key-related error codes up and insert ECANCELED
    This patch shifts the key-related error codes up by one and inserts an
    ECANCELED error code where not already defined.  It seems that has been
    defined in glibc without passing it back to the kernel:-/
    
    Signed-Off-By: David Howells <dhowells@redhat.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-sparc/errno.h
+++ include/asm-sparc/errno.h
@@ -189,13 +189,15 @@ DECL|macro|ENOMEDIUM
 mdefine_line|#define&t;ENOMEDIUM&t;125&t;/* No medium found */
 DECL|macro|EMEDIUMTYPE
 mdefine_line|#define&t;EMEDIUMTYPE&t;126&t;/* Wrong medium type */
+DECL|macro|ECANCELED
+mdefine_line|#define&t;ECANCELED&t;127&t;/* Operation Cancelled */
 DECL|macro|ENOKEY
-mdefine_line|#define&t;ENOKEY&t;&t;127&t;/* Required key not available */
+mdefine_line|#define&t;ENOKEY&t;&t;128&t;/* Required key not available */
 DECL|macro|EKEYEXPIRED
-mdefine_line|#define&t;EKEYEXPIRED&t;128&t;/* Key has expired */
+mdefine_line|#define&t;EKEYEXPIRED&t;129&t;/* Key has expired */
 DECL|macro|EKEYREVOKED
-mdefine_line|#define&t;EKEYREVOKED&t;129&t;/* Key has been revoked */
+mdefine_line|#define&t;EKEYREVOKED&t;130&t;/* Key has been revoked */
 DECL|macro|EKEYREJECTED
-mdefine_line|#define&t;EKEYREJECTED&t;130&t;/* Key was rejected by service */
+mdefine_line|#define&t;EKEYREJECTED&t;131&t;/* Key was rejected by service */
 macro_line|#endif
 eof
