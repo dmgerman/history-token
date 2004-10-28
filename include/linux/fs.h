d7cc4c68e49500d79fb40148d0f732282e042cf3
Hugh Dickins <hugh@veritas.com>
2004-10-27 18:18:13 -0700

    [PATCH] anon cris align address_space
    CRIS does not demand alignment, so PageAnon's PAGE_MAPPING_ANON bit got
    mixed up with the low bit of the struct address_space *mapping pointer.
    
    Patch based on that from Mikael Starvik, but moved the alignment to the
    declaration of struct address_space itself, and align to sizeof(long) so
    it's well-aligned on all architectures.
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -866,7 +866,22 @@ id|assoc_mapping
 suffix:semicolon
 multiline_comment|/* ditto */
 )brace
+id|__attribute__
+c_func
+(paren
+(paren
+id|aligned
+c_func
+(paren
+r_sizeof
+(paren
+r_int
+)paren
+)paren
+)paren
+)paren
 suffix:semicolon
+multiline_comment|/*&n;&t; * On most architectures that alignment is already the case; but&n;&t; * must be enforced here for CRIS, to let the least signficant bit&n;&t; * of struct page&squot;s &quot;mapping&quot; pointer be used for PAGE_MAPPING_ANON.&n;&t; */
 DECL|struct|block_device
 r_struct
 id|block_device
