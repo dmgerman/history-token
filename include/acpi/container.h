425a3ebc36fa813a67df128639e4d294cae36b54
Len Brown <len.brown@intel.com>
2004-10-27 20:01:25 -0400

    [ACPI] Initial container driver to support hotplug notifications on ACPI0004, PNP0A05 and PNP0A06 devices.
    Signed-off-by: Anil S Keshavamurthy <anil.s.keshavamurthy@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>

---
+++ include/acpi/container.h
@@ -0,0 +1,25 @@
+macro_line|#ifndef __ACPI_CONTAINER_H
+DECL|macro|__ACPI_CONTAINER_H
+mdefine_line|#define __ACPI_CONTAINER_H
+macro_line|#include &lt;linux/kernel.h&gt;
+DECL|struct|acpi_container
+r_struct
+id|acpi_container
+(brace
+DECL|member|handle
+id|acpi_handle
+id|handle
+suffix:semicolon
+DECL|member|sun
+r_int
+r_int
+id|sun
+suffix:semicolon
+DECL|member|state
+r_int
+id|state
+suffix:semicolon
+)brace
+suffix:semicolon
+macro_line|#endif /* __ACPI_CONTAINER_H */
+eof
