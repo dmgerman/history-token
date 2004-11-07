71ec5602cccd6d96a4c97779a271bafadbc46615
Jeff Dike <jdike@addtoit.com>
2004-11-07 03:57:39 -0800

    [PATCH] uml: update atomic.h so UML builds cleanly
    The comment should be self-explanatory.  This works around a pile of
    nasty-looking build output.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-um/atomic.h
+++ include/asm-um/atomic.h
@@ -1,6 +1,8 @@
 macro_line|#ifndef __UM_ATOMIC_H
 DECL|macro|__UM_ATOMIC_H
 mdefine_line|#define __UM_ATOMIC_H
+multiline_comment|/* The i386 atomic.h calls printk, but doesn&squot;t include kernel.h, so we&n; * include it here.&n; */
+macro_line|#include &quot;linux/kernel.h&quot;
 macro_line|#include &quot;asm/arch/atomic.h&quot;
 macro_line|#endif
 eof
