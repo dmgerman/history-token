bfaf4c1ef5170938d06a87124f5f065033e0fd14
David Brownell <david-b@pacbell.net>
2004-11-11 03:02:33 -0800

    [PATCH] USB: EHCI and APM suspend
    I've been having some problems with the EHCI driver on my Thinkpad T40,
    running 2.6.9 with APM instead of ACPI.  On resume after suspend-to-RAM
    the EHCI hardware keeps the interrupt line high, but the EHCI driver is
    not clearing the interrupt status register, causing Linux to eventually
    disable the interrupt altogether.
    
    The attached patch for ehci-hub.c does the obvious thing: delay enabling
    interrupts until hcd->state is set to USB_STATE_RUNNING.
    
    From: Nickolai Zeldovich <nickolai@cs.stanford.edu>
    Signed-off-by: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Greg Kroah-Hartman <greg@kroah.com>

--- drivers/usb/host/ehci-hub.c
+++ drivers/usb/host/ehci-hub.c
@@ -303,6 +303,9 @@ suffix:semicolon
 r_int
 id|i
 suffix:semicolon
+r_int
+id|intr_enable
+suffix:semicolon
 r_if
 c_cond
 (paren
@@ -339,17 +342,10 @@ op_eq
 l_int|0
 )paren
 (brace
-id|temp
+multiline_comment|/* at least some APM implementations will try to deliver&n;&t;&t; * IRQs right away, so delay them until we&squot;re ready.&n; &t;&t; */
+id|intr_enable
 op_assign
 l_int|1
-suffix:semicolon
-id|writel
-(paren
-id|INTR_MASK
-comma
-op_amp
-id|ehci-&gt;regs-&gt;intr_enable
-)paren
 suffix:semicolon
 id|writel
 (paren
@@ -378,10 +374,9 @@ op_amp
 id|ehci-&gt;regs-&gt;async_next
 )paren
 suffix:semicolon
-multiline_comment|/* FIXME will this work even if (pci) vAUX was lost? */
 )brace
 r_else
-id|temp
+id|intr_enable
 op_assign
 l_int|0
 suffix:semicolon
@@ -392,7 +387,7 @@ id|ehci
 comma
 l_string|&quot;resume root hub%s&bslash;n&quot;
 comma
-id|temp
+id|intr_enable
 ques
 c_cond
 l_string|&quot; after power loss&quot;
@@ -622,6 +617,20 @@ suffix:semicolon
 id|ehci-&gt;hcd.state
 op_assign
 id|USB_STATE_RUNNING
+suffix:semicolon
+multiline_comment|/* Now we can safely re-enable irqs */
+r_if
+c_cond
+(paren
+id|intr_enable
+)paren
+id|writel
+(paren
+id|INTR_MASK
+comma
+op_amp
+id|ehci-&gt;regs-&gt;intr_enable
+)paren
 suffix:semicolon
 r_return
 l_int|0
