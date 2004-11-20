067820a9c56b966e026aa289e15ab42d471f57a1
Andrew Morton <akpm@osdl.org>
2004-11-19 22:26:34 -0800

    [PATCH] x86_64: duplicated patch
    We ended up adding this patch twice.
    
    Cc: <ak@muc.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-x86_64/smp.h
+++ include/asm-x86_64/smp.h
@@ -304,20 +304,6 @@ id|BAD_APICID
 r_return
 l_int|0
 suffix:semicolon
-multiline_comment|/* No entries in x86_cpu_to_apicid?  Either no MPS|ACPI,&n;          * or called too early.  Either way, we must be CPU 0. */
-r_if
-c_cond
-(paren
-id|x86_cpu_to_apicid
-(braket
-l_int|0
-)braket
-op_eq
-id|BAD_APICID
-)paren
-r_return
-l_int|0
-suffix:semicolon
 r_return
 op_minus
 l_int|1
