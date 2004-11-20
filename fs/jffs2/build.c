6af4eca7d5a04bda1b45e32716a0a9fcec81394f
David Woodhouse <dwmw2@shinybook.infradead.org>
2004-11-20 09:08:57 +0000

    JFFS2: Discard dirents which point to non-existent inodes.
    If a directory entry refers to an inode which doesn't actually exist,
    we weren't marking it obsolete, so it was still visible in the file
    system, and would give EIO if you ever tried to stat it. Once upon
    a time, perl -e 'unlink' and rm -f would manage to unlink such things
    but nowadays they both try to stat it first and stupidly refuse to
    even attempt the unlink if the stat fails, and this is more of a
    problem. So we throw it away ourselves.
    
    Signed-off-by: Artem Bityuckiy <dedekind@infradead.org>
    Signed-off-by: David Woodhouse <dwmw2@infradead.org>

--- fs/jffs2/build.c
+++ fs/jffs2/build.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * JFFS2 -- Journalling Flash File System, Version 2.&n; *&n; * Copyright (C) 2001-2003 Red Hat, Inc.&n; *&n; * Created by David Woodhouse &lt;dwmw2@infradead.org&gt;&n; *&n; * For licensing information, see the file &squot;LICENCE&squot; in this directory.&n; *&n; * $Id: build.c,v 1.57 2004/11/16 20:36:11 dwmw2 Exp $&n; *&n; */
+multiline_comment|/*&n; * JFFS2 -- Journalling Flash File System, Version 2.&n; *&n; * Copyright (C) 2001-2003 Red Hat, Inc.&n; *&n; * Created by David Woodhouse &lt;dwmw2@infradead.org&gt;&n; *&n; * For licensing information, see the file &squot;LICENCE&squot; in this directory.&n; *&n; * $Id: build.c,v 1.60 2004/11/17 17:13:13 dedekind Exp $&n; *&n; */
 macro_line|#include &lt;linux/kernel.h&gt;
 macro_line|#include &lt;linux/sched.h&gt;
 macro_line|#include &lt;linux/slab.h&gt;
@@ -224,6 +224,14 @@ comma
 id|ic-&gt;ino
 )paren
 suffix:semicolon
+id|jffs2_mark_node_obsolete
+c_func
+(paren
+id|c
+comma
+id|fd-&gt;raw
+)paren
+suffix:semicolon
 r_continue
 suffix:semicolon
 )brace
@@ -342,11 +350,6 @@ c_func
 id|c
 )paren
 suffix:semicolon
-id|c-&gt;flags
-op_and_assign
-op_complement
-id|JFFS2_SB_FLAG_MOUNTING
-suffix:semicolon
 r_if
 c_cond
 (paren
@@ -433,6 +436,11 @@ c_func
 suffix:semicolon
 )brace
 )brace
+id|c-&gt;flags
+op_and_assign
+op_complement
+id|JFFS2_SB_FLAG_MOUNTING
+suffix:semicolon
 id|D1
 c_func
 (paren
