5fa92ce4ae3dcc0abd2fbf520a011fd0645a5492
Deepak Saxena <dsaxena@net.rmk.(none)>
2004-11-26 20:42:00 +0000

    [ARM PATCH] 2259/1: Rip out ixp2000 IRQ_ERR_STATUS demultiplexing
    Patch from Lennert Buytenhek
    
    There are thirteen different IRQs chained off IRQ_ERR_STATUS, one for
    each possible error class that the IXP can signal an interrupt for, but
    there are no in-tree users of these interrupts, and it doesn't make much
    sense to treat them as separate interrupts if we can just have one
    handler checking each of the thirteen errors in one go instead.
    
    Besides that, the error interrupt handling can't even have been working
    properly in the first place as the chained handler was testing the wrong
    bits in the IRQ_ERR_STATUS register.
    
    So this patch rips it all out.
    
    Signed-off-by: Lennert Buytenhek
    Signed-off-by: Deepak Saxena
    Signed-off-by: Russell King

--- include/asm-arm/arch-ixp2000/irqs.h
+++ include/asm-arm/arch-ixp2000/irqs.h
@@ -74,35 +74,8 @@ DECL|macro|IRQ_IXP2000_PCIA
 mdefine_line|#define IRQ_IXP2000_PCIA&t;&t;40
 DECL|macro|IRQ_IXP2000_PCIB
 mdefine_line|#define IRQ_IXP2000_PCIB&t;&t;41
-multiline_comment|/* Int sources from IRQ_ERROR_STATUS */
-DECL|macro|IRQ_IXP2000_DRAM0_MIN_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM0_MIN_ERR&t;42
-DECL|macro|IRQ_IXP2000_DRAM0_MAJ_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM0_MAJ_ERR&t;43
-DECL|macro|IRQ_IXP2000_DRAM1_MIN_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM1_MIN_ERR&t;44
-DECL|macro|IRQ_IXP2000_DRAM1_MAJ_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM1_MAJ_ERR&t;45
-DECL|macro|IRQ_IXP2000_DRAM2_MIN_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM2_MIN_ERR&t;46
-DECL|macro|IRQ_IXP2000_DRAM2_MAJ_ERR
-mdefine_line|#define IRQ_IXP2000_DRAM2_MAJ_ERR&t;47
-DECL|macro|IRQ_IXP2000_SRAM0_ERR
-mdefine_line|#define IRQ_IXP2000_SRAM0_ERR&t;&t;48
-DECL|macro|IRQ_IXP2000_SRAM1_ERR
-mdefine_line|#define IRQ_IXP2000_SRAM1_ERR&t;&t;49
-DECL|macro|IRQ_IXP2000_SRAM2_ERR
-mdefine_line|#define IRQ_IXP2000_SRAM2_ERR           50
-DECL|macro|IRQ_IXP2000_SRAM3_ERR
-mdefine_line|#define IRQ_IXP2000_SRAM3_ERR&t;&t;51
-DECL|macro|IRQ_IXP2000_MEDIA_ERR
-mdefine_line|#define IRQ_IXP2000_MEDIA_ERR&t;&t;52
-DECL|macro|IRQ_IXP2000_PCI_ERR
-mdefine_line|#define IRQ_IXP2000_PCI_ERR&t;&t;53
-DECL|macro|IRQ_IXP2000_SP_INT
-mdefine_line|#define IRQ_IXP2000_SP_INT&t;&t;54
 DECL|macro|NR_IXP2000_IRQS
-mdefine_line|#define NR_IXP2000_IRQS                 55
+mdefine_line|#define NR_IXP2000_IRQS                 42
 DECL|macro|IXP2000_BOARD_IRQ
 mdefine_line|#define&t;IXP2000_BOARD_IRQ(x)&t;&t;(NR_IXP2000_IRQS + (x))
 DECL|macro|IXP2000_BOARD_IRQ_MASK
