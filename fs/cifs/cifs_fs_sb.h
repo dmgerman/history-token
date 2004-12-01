c7d79ed4a274e8796b76e7066f82bee4c2d7f70d
Steve French <stevef@smfhome.smfdom>
2004-12-01 11:34:34 -0600

    add direct mount options to optionally bypass the page cache on reads and writes

--- fs/cifs/cifs_fs_sb.h
+++ fs/cifs/cifs_fs_sb.h
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; *   fs/cifs/cifs_fs_sb.h&n; *&n; *   Copyright (c) International Business Machines  Corp., 2002&n; *   Author(s): Steve French (sfrench@us.ibm.com)&n; *&n; *   This library is free software; you can redistribute it and/or modify&n; *   it under the terms of the GNU Lesser General Public License as published&n; *   by the Free Software Foundation; either version 2.1 of the License, or&n; *   (at your option) any later version.&n; *&n; *   This library is distributed in the hope that it will be useful,&n; *   but WITHOUT ANY WARRANTY; without even the implied warranty of&n; *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See&n; *   the GNU Lesser General Public License for more details.&n; *&n; */
+multiline_comment|/*&n; *   fs/cifs/cifs_fs_sb.h&n; *&n; *   Copyright (c) International Business Machines  Corp., 2002,2004&n; *   Author(s): Steve French (sfrench@us.ibm.com)&n; *&n; *   This library is free software; you can redistribute it and/or modify&n; *   it under the terms of the GNU Lesser General Public License as published&n; *   by the Free Software Foundation; either version 2.1 of the License, or&n; *   (at your option) any later version.&n; *&n; *   This library is distributed in the hope that it will be useful,&n; *   but WITHOUT ANY WARRANTY; without even the implied warranty of&n; *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See&n; *   the GNU Lesser General Public License for more details.&n; *&n; */
 macro_line|#ifndef _CIFS_FS_SB_H
 DECL|macro|_CIFS_FS_SB_H
 mdefine_line|#define _CIFS_FS_SB_H
@@ -8,6 +8,8 @@ DECL|macro|CIFS_MOUNT_SET_UID
 mdefine_line|#define CIFS_MOUNT_SET_UID      2 /* set current-&gt;euid in create etc. */
 DECL|macro|CIFS_MOUNT_SERVER_INUM
 mdefine_line|#define CIFS_MOUNT_SERVER_INUM  4 /* inode numbers from uniqueid from server */
+DECL|macro|CIFS_MOUNT_DIRECT_IO
+mdefine_line|#define CIFS_MOUNT_DIRECT_IO          8 /* do not write nor read through page cache */
 DECL|struct|cifs_sb_info
 r_struct
 id|cifs_sb_info
