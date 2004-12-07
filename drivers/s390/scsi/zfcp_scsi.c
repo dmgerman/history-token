a867c7933222ea8cbdb4a8f40354c5d52466e78f
Andreas Herrmann <aherrman@de.ibm.com>
2004-12-06 21:15:15 -0600

    [PATCH] zfcp: act enhancements corrections
    - corrects errors introduced in open_unit_handler
      with last patch submission for act enhancements,
    - changes formatting and some log-messages in act
      enhancments code,
    - removes junk lines
    - corrected Kconfig dependency for zfcp
    - do not wait for SBALs when creating ELS requests
    
    Signed-off-by: Andreas Herrmann <aherrman@de.ibm.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/s390/scsi/zfcp_scsi.c
+++ drivers/s390/scsi/zfcp_scsi.c
@@ -1,9 +1,8 @@
 multiline_comment|/* &n; * &n; * linux/drivers/s390/scsi/zfcp_scsi.c&n; * &n; * FCP adapter driver for IBM eServer zSeries &n; * &n; * (C) Copyright IBM Corp. 2002, 2004&n; *&n; * Author(s): Martin Peschke &lt;mpeschke@de.ibm.com&gt; &n; *            Raimund Schroeder &lt;raimund.schroeder@de.ibm.com&gt; &n; *            Aron Zeh&n; *            Wolfgang Taphorn&n; *            Stefan Bader &lt;stefan.bader@de.ibm.com&gt; &n; *            Heiko Carstens &lt;heiko.carstens@de.ibm.com&gt; &n; *            Andreas Herrmann &lt;aherrman@de.ibm.com&gt;&n; * &n; * This program is free software; you can redistribute it and/or modify &n; * it under the terms of the GNU General Public License as published by &n; * the Free Software Foundation; either version 2, or (at your option) &n; * any later version. &n; * &n; * This program is distributed in the hope that it will be useful, &n; * but WITHOUT ANY WARRANTY; without even the implied warranty of &n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the &n; * GNU General Public License for more details. &n; * &n; * You should have received a copy of the GNU General Public License &n; * along with this program; if not, write to the Free Software &n; * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. &n; */
 DECL|macro|ZFCP_LOG_AREA
 mdefine_line|#define ZFCP_LOG_AREA&t;&t;&t;ZFCP_LOG_AREA_SCSI
-multiline_comment|/* this drivers version (do not edit !!! generated and updated by cvs) */
 DECL|macro|ZFCP_SCSI_REVISION
-mdefine_line|#define ZFCP_SCSI_REVISION &quot;$Revision: 1.73 $&quot;
+mdefine_line|#define ZFCP_SCSI_REVISION &quot;$Revision: 1.74 $&quot;
 macro_line|#include &quot;zfcp_ext.h&quot;
 r_static
 r_void
