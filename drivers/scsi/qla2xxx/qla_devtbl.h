25a6eaf834476ee1a971df8ceedb24d4619fb7a4
Andrew Vasquez <andrew.vasquez@qlogic.com>
2004-12-19 00:12:20 -0600

    qla2xxx: NVRAM id-list updates
    Resync with latest NVRAM subsystem ID list.
    
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla2xxx/qla_devtbl.h
+++ drivers/scsi/qla2xxx/qla_devtbl.h
@@ -1,5 +1,5 @@
 DECL|macro|QLA_MODEL_NAMES
-mdefine_line|#define QLA_MODEL_NAMES         0x21
+mdefine_line|#define QLA_MODEL_NAMES         0x32
 multiline_comment|/*&n; * Adapter model names.&n; */
 DECL|variable|qla2x00_model_name
 r_static
@@ -56,22 +56,22 @@ multiline_comment|/* 0x10d */
 l_string|&quot;QLA2352&quot;
 comma
 multiline_comment|/* 0x10e */
-l_string|&quot;HPQSVS &quot;
+l_string|&quot;HPQ SVS&quot;
 comma
 multiline_comment|/* 0x10f */
-l_string|&quot;HPQSVS &quot;
+l_string|&quot;HPQ SVS&quot;
 comma
 multiline_comment|/* 0x110 */
-l_string|&quot;QLA4010&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x111 */
-l_string|&quot;QLA4010&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x112 */
-l_string|&quot;QLA4010C&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x113 */
-l_string|&quot;QLA4010C&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x114 */
 l_string|&quot;QLA2360&quot;
@@ -80,29 +80,86 @@ multiline_comment|/* 0x115 */
 l_string|&quot;QLA2362&quot;
 comma
 multiline_comment|/* 0x116 */
-l_string|&quot; &quot;
+l_string|&quot;QLE2360&quot;
 comma
 multiline_comment|/* 0x117 */
-l_string|&quot; &quot;
+l_string|&quot;QLE2362&quot;
 comma
 multiline_comment|/* 0x118 */
 l_string|&quot;QLA200&quot;
 comma
 multiline_comment|/* 0x119 */
 l_string|&quot;QLA200C&quot;
-multiline_comment|/* 0x11A */
+comma
+multiline_comment|/* 0x11a */
 l_string|&quot;QLA200P&quot;
-multiline_comment|/* 0x11B */
+comma
+multiline_comment|/* 0x11b */
 l_string|&quot;QLA200P&quot;
-multiline_comment|/* 0x11C */
-l_string|&quot;QLA4040&quot;
-multiline_comment|/* 0x11D */
-l_string|&quot;QLA4040&quot;
-multiline_comment|/* 0x11E */
-l_string|&quot;QLA4040C&quot;
-multiline_comment|/* 0x11F */
-l_string|&quot;QLA4040C&quot;
+comma
+multiline_comment|/* 0x11c */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x11d */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x11e */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x11f */
+l_string|&quot; &quot;
+comma
 multiline_comment|/* 0x120 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x121 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x122 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x123 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x124 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x125 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x126 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x127 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x128 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x129 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12a */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12b */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12c */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12d */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12e */
+l_string|&quot;QLA210&quot;
+comma
+multiline_comment|/* 0x12f */
+l_string|&quot;EMC 250&quot;
+comma
+multiline_comment|/* 0x130 */
+l_string|&quot;HP A7538A&quot;
+multiline_comment|/* 0x131 */
 )brace
 suffix:semicolon
 DECL|variable|qla2x00_model_desc
@@ -166,16 +223,16 @@ multiline_comment|/* 0x10f */
 l_string|&quot;HPQ SVS HBA- Target device&quot;
 comma
 multiline_comment|/* 0x110 */
-l_string|&quot;Optical- 133MHz to 1Gb iSCSI- networking&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x111 */
-l_string|&quot;Optical- 133MHz to 1Gb iSCSI- storage&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x112 */
-l_string|&quot;Copper- 133MHz to 1Gb iSCSI- networking&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x113 */
-l_string|&quot;Copper- 133MHz to 1Gb iSCSI- storage&quot;
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x114 */
 l_string|&quot;133MHz PCI-X to 2Gb FC Single Channel&quot;
@@ -184,33 +241,86 @@ multiline_comment|/* 0x115 */
 l_string|&quot;133MHz PCI-X to 2Gb FC Dual Channel&quot;
 comma
 multiline_comment|/* 0x116 */
-l_string|&quot; &quot;
+l_string|&quot;PCI-Express to 2Gb FC, Single Channel&quot;
 comma
 multiline_comment|/* 0x117 */
-l_string|&quot; &quot;
+l_string|&quot;PCI-Express to 2Gb FC, Dual Channel&quot;
 comma
 multiline_comment|/* 0x118 */
 l_string|&quot;133MHz PCI-X to 2Gb FC Optical&quot;
 comma
 multiline_comment|/* 0x119 */
 l_string|&quot;133MHz PCI-X to 2Gb FC Copper&quot;
-multiline_comment|/* 0x11A */
+comma
+multiline_comment|/* 0x11a */
 l_string|&quot;133MHz PCI-X to 2Gb FC SFP&quot;
-multiline_comment|/* 0x11B */
+comma
+multiline_comment|/* 0x11b */
 l_string|&quot;133MHz PCI-X to 2Gb FC SFP&quot;
-multiline_comment|/* 0x11C */
-l_string|&quot;Optical- 133MHz to 1Gb NIC with IPSEC&quot;
 comma
-multiline_comment|/* 0x11D */
-l_string|&quot;Optical- 133MHz to 1Gb iSCSI with IPSEC&quot;
+multiline_comment|/* 0x11c */
+l_string|&quot; &quot;
 comma
-multiline_comment|/* 0x11E */
-l_string|&quot;Copper- 133MHz to 1Gb NIC with IPSEC&quot;
+multiline_comment|/* 0x11d */
+l_string|&quot; &quot;
 comma
-multiline_comment|/* 0x11F */
-l_string|&quot;Copper- 133MHz to 1Gb iSCSI with IPSEC&quot;
+multiline_comment|/* 0x11e */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x11f */
+l_string|&quot; &quot;
 comma
 multiline_comment|/* 0x120 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x121 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x122 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x123 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x124 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x125 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x126 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x127 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x128 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x129 */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12a */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12b */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12c */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12d */
+l_string|&quot; &quot;
+comma
+multiline_comment|/* 0x12e */
+l_string|&quot;133MHz PCI-X to 2Gb FC SFF&quot;
+comma
+multiline_comment|/* 0x12f */
+l_string|&quot;133MHz PCI-X to 2Gb FC SFF&quot;
+comma
+multiline_comment|/* 0x130 */
+l_string|&quot;HP 1p2g QLA2340&quot;
+multiline_comment|/* 0x131 */
 )brace
 suffix:semicolon
 eof
