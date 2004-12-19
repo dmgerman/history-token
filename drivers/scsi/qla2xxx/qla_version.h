c1350d99d817272ed8cc456b44e5af8955cd9951
Andrew Vasquez <andrew.vasquez@qlogic.com>
2004-12-19 00:43:44 -0600

    qla2xxx: Update driver version
    Update version number to 8.00.02b4-k.
    
    Signed-off-by: Andrew Vasquez <andrew.vasquez@qlogic.com>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/qla2xxx/qla_version.h
+++ drivers/scsi/qla2xxx/qla_version.h
@@ -1,13 +1,13 @@
 multiline_comment|/******************************************************************************&n; *                  QLOGIC LINUX SOFTWARE&n; *&n; * QLogic ISP2x00 device driver for Linux 2.6.x&n; * Copyright (C) 2003-2004 QLogic Corporation&n; * (www.qlogic.com)&n; *&n; * This program is free software; you can redistribute it and/or modify it&n; * under the terms of the GNU General Public License as published by the&n; * Free Software Foundation; either version 2, or (at your option) any&n; * later version.&n; *&n; * This program is distributed in the hope that it will be useful, but&n; * WITHOUT ANY WARRANTY; without even the implied warranty of&n; * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU&n; * General Public License for more details.&n; *&n; ******************************************************************************/
 multiline_comment|/*&n; * Driver version &n; */
 DECL|macro|QLA2XXX_VERSION
-mdefine_line|#define QLA2XXX_VERSION      &quot;8.00.00b21-k&quot;
+mdefine_line|#define QLA2XXX_VERSION      &quot;8.00.02b4-k&quot;
 DECL|macro|QLA_DRIVER_MAJOR_VER
 mdefine_line|#define QLA_DRIVER_MAJOR_VER&t;8
 DECL|macro|QLA_DRIVER_MINOR_VER
 mdefine_line|#define QLA_DRIVER_MINOR_VER&t;0
 DECL|macro|QLA_DRIVER_PATCH_VER
-mdefine_line|#define QLA_DRIVER_PATCH_VER&t;0
+mdefine_line|#define QLA_DRIVER_PATCH_VER&t;2
 DECL|macro|QLA_DRIVER_BETA_VER
-mdefine_line|#define QLA_DRIVER_BETA_VER&t;21
+mdefine_line|#define QLA_DRIVER_BETA_VER&t;4
 eof
