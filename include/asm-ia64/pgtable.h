e2bd528f1afd8118a398edc260e638dcfdc9ad12
Andrew Morton <akpm@osdl.org>
2005-01-01 17:46:03 -0800

    [PATCH] ia64 PTRS_PER_PGD build fix
    mm/memory.c:1944:6: missing binary operator before token "long"
    
    The preprocessor doesn't like the typecast....
    
    PTRS_PER_PGD isn't used in assembly code, so this looks to be safe enough..
    
    This patch helps, but the ia64 build is still broken.  There is no
    implementation of __pmd_alloc().
    
    Cc: Nick Piggin <nickpiggin@yahoo.com.au>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-ia64/pgtable.h
+++ include/asm-ia64/pgtable.h
@@ -117,7 +117,7 @@ mdefine_line|#define PGDIR_SIZE&t;&t;(__IA64_UL(1) &lt;&lt; PGDIR_SHIFT)
 DECL|macro|PGDIR_MASK
 mdefine_line|#define PGDIR_MASK&t;&t;(~(PGDIR_SIZE-1))
 DECL|macro|PTRS_PER_PGD
-mdefine_line|#define PTRS_PER_PGD&t;&t;(__IA64_UL(1) &lt;&lt; (PAGE_SHIFT-3))
+mdefine_line|#define PTRS_PER_PGD&t;&t;(1UL &lt;&lt; (PAGE_SHIFT-3))
 DECL|macro|USER_PTRS_PER_PGD
 mdefine_line|#define USER_PTRS_PER_PGD&t;(5*PTRS_PER_PGD/8)&t;/* regions 0-4 are user regions */
 DECL|macro|FIRST_USER_PGD_NR
