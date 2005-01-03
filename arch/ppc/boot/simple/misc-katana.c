0a327d5479dd54f2b2d26ff023088dba3c65d9bb
Mark A. Greer <mgreer@mvista.com>
2005-01-03 04:33:18 -0800

    [PATCH] ppc32: support for Artesyn Katana cPCI boards
    This patch adds support for the Artesyn Katana 750i, 752i, and 3750.
    
    Signed-off-by: Mark A. Greer <mgreer@mvista.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ arch/ppc/boot/simple/misc-katana.c
@@ -0,0 +1,9 @@
+multiline_comment|/*&n; * arch/ppc/boot/simple/misc-katana.c&n; *&n; * Add birec data for Artesyn KATANA board.&n; *&n; * Author: Mark A. Greer &lt;source@mvista.com&gt;&n; *&n; * 2004 (c) MontaVista Software, Inc. This file is licensed under&n; * the terms of the GNU General Public License version 2. This program&n; * is licensed &quot;as is&quot; without any warranty of any kind, whether express&n; * or implied.&n; */
+macro_line|#include &lt;linux/types.h&gt;
+DECL|variable|mv64x60_mpsc_clk_freq
+r_int
+id|mv64x60_mpsc_clk_freq
+op_assign
+l_int|133333333
+suffix:semicolon
+eof
