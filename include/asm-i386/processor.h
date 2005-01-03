cde1555e4f9bea1fe41580f87f5f3678ec22df44
Zwane Mwaikambo <zwane@linuxpower.ca>
2005-01-03 04:41:13 -0800

    [PATCH] Cyrix MII cpuid returns stale %ecx
    This patch is for the following bug, thanks to Ondrej Zary for reporting,
    testing and submitting a patch.
    
    http://bugzilla.kernel.org/show_bug.cgi?id=3767
    
    It appears that the Cyrix MII won't touch %ecx at all resulting in stale
    data being returned as extended attributes, so clear ecx before issuing the
    cpuid.  I have also made the capability print code display all the
    capability words for easier debugging in future.
    
    Signed-off-by: Zwane Mwaikambo <zwane@linuxpower.ca>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-i386/processor.h
+++ include/asm-i386/processor.h
@@ -291,7 +291,7 @@ DECL|macro|X86_EFLAGS_VIP
 mdefine_line|#define X86_EFLAGS_VIP&t;0x00100000 /* Virtual Interrupt Pending */
 DECL|macro|X86_EFLAGS_ID
 mdefine_line|#define X86_EFLAGS_ID&t;0x00200000 /* CPUID detection flag */
-multiline_comment|/*&n; * Generic CPUID function&n; */
+multiline_comment|/*&n; * Generic CPUID function&n; * clear %ecx since some cpus (Cyrix MII) do not set or clear %ecx&n; * resulting in stale register contents being returned.&n; */
 DECL|function|cpuid
 r_static
 r_inline
@@ -351,6 +351,11 @@ suffix:colon
 l_string|&quot;0&quot;
 (paren
 id|op
+)paren
+comma
+l_string|&quot;c&quot;
+(paren
+l_int|0
 )paren
 )paren
 suffix:semicolon
