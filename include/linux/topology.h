990d49404f148dff3085f5b86f7ccfbe092032cd
Andi Kleen <ak@suse.de>
2005-01-03 04:48:54 -0800

    [PATCH] x86_64: Add SLIT (inter node distance) information to sysfs.
    Add SLIT (inter node distance) information to sysfs.
    
    [This is Jack's patch that he submitted on l-k. I'm submitting
    it for him because I need it for my x86-64 followon SLIT patch.
    Hope I don't stomp onto his toes with that one. If you already
    merged it please ignore]
    
    From: Jack Steiner
    
    Here is an update patch to externalize the SLIT information. I think I have
    encorporated all the comments that were posted previously)
    
    For example:
    
            # cd /sys/devices/system
            # find .
            ./node
            ./node/node5
            ./node/node5/distance
            ./node/node5/numastat
            ./node/node5/meminfo
            ./node/node5/cpumap
    
            # cat ./node/node0/distance
            10 20 64 42 42 22
    
            # cat node/*/distance
            10 20 64 42 42 22
            20 10 42 22 64 84
            64 42 10 20 22 42
            42 22 20 10 42 62
            42 64 22 42 10 20
            22 84 42 62 20 10
    
    Signed-off-by: Jack Steiner <steiner@sgi.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/linux/topology.h
+++ include/linux/topology.h
@@ -48,8 +48,13 @@ suffix:semicolon
 DECL|macro|for_each_node_with_cpus
 mdefine_line|#define for_each_node_with_cpus(node) &bslash;&n;&t;for (node = 0; node &lt; numnodes; node = __next_node_with_cpus(node))
 macro_line|#ifndef node_distance
+multiline_comment|/* Conform to ACPI 2.0 SLIT distance definitions */
+DECL|macro|LOCAL_DISTANCE
+mdefine_line|#define LOCAL_DISTANCE&t;&t;10
+DECL|macro|REMOTE_DISTANCE
+mdefine_line|#define REMOTE_DISTANCE&t;&t;20
 DECL|macro|node_distance
-mdefine_line|#define node_distance(from,to)&t;((from) != (to))
+mdefine_line|#define node_distance(from,to)&t;((from) == (to) ? LOCAL_DISTANCE : REMOTE_DISTANCE)
 macro_line|#endif
 macro_line|#ifndef PENALTY_FOR_NODE_WITH_CPUS
 DECL|macro|PENALTY_FOR_NODE_WITH_CPUS
