fa8286d3257c9c93c759c1704a6713208c2ac589
William Lee Irwin III <wli@holomorphy.com>
2005-01-04 05:19:42 -0800

    [PATCH] convert FRV to use remap_pfn_range
    This patch converts FRV to use remap_pfn_range() in its
    io_remap_page_range() function.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-frv/pgtable.h
+++ include/asm-frv/pgtable.h
@@ -1046,7 +1046,7 @@ mdefine_line|#define PageSkip(page)&t;&t;(0)
 DECL|macro|kern_addr_valid
 mdefine_line|#define kern_addr_valid(addr)&t;(1)
 DECL|macro|io_remap_page_range
-mdefine_line|#define io_remap_page_range&t;remap_page_range
+mdefine_line|#define io_remap_page_range(vma, vaddr, paddr, size, prot)&t;&t;&bslash;&n;&t;&t;remap_pfn_range(vma, vaddr, (paddr) &gt;&gt; PAGE_SHIFT, size, prot)
 DECL|macro|__HAVE_ARCH_PTEP_TEST_AND_CLEAR_YOUNG
 mdefine_line|#define __HAVE_ARCH_PTEP_TEST_AND_CLEAR_YOUNG
 DECL|macro|__HAVE_ARCH_PTEP_TEST_AND_CLEAR_DIRTY
