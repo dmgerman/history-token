4933eaaf265148d6c597a5a1b13fb081b7e26e7b
Christoph Hellwig <hch@lst.de>
2005-01-04 05:26:32 -0800

    [PATCH] remove unused irq_cpustat fields
    The only common field in irq_cpustat is __softirq_pending, i386 and ppc
    have some of their own.
    
    Remove all unused obsolete fields from various architectures.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-parisc/hardirq.h
+++ include/asm-parisc/hardirq.h
@@ -14,22 +14,6 @@ r_int
 id|__softirq_pending
 suffix:semicolon
 multiline_comment|/* set_bit is used on this */
-DECL|member|__syscall_count
-r_int
-r_int
-id|__syscall_count
-suffix:semicolon
-DECL|member|__ksoftirqd_task
-r_struct
-id|task_struct
-op_star
-id|__ksoftirqd_task
-suffix:semicolon
-DECL|member|idle_timestamp
-r_int
-r_int
-id|idle_timestamp
-suffix:semicolon
 DECL|typedef|irq_cpustat_t
 )brace
 id|____cacheline_aligned
