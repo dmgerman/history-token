4cf88307aaf14efaa344160f47b7cad74e86c3c0
Greg Ungerer <gerg@snapgear.com>
2005-01-09 01:34:30 -0800

    [PATCH] m68knommu: export lib udelay symbol for modules
    Export the udelay symbol so modules get access to it as well.
    
    Signed-off-by: Greg Ungerer <gerg@snapgear.com>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/m68knommu/lib/delay.c
+++ arch/m68knommu/lib/delay.c
@@ -1,6 +1,14 @@
 multiline_comment|/*&n; *&t;arch/m68knommu/lib/delay.c&n; *&n; *&t;(C) Copyright 2004, Greg Ungerer &lt;gerg@snapgear.com&gt;&n; *&n; * This program is free software; you can redistribute it and/or modify&n; * it under the terms of the GNU General Public License version 2 as&n; * published by the Free Software Foundation.&n; */
+macro_line|#include &lt;linux/module.h&gt;
 macro_line|#include &lt;asm/param.h&gt;
 macro_line|#include &lt;asm/delay.h&gt;
+DECL|variable|udelay
+id|EXPORT_SYMBOL
+c_func
+(paren
+id|udelay
+)paren
+suffix:semicolon
 DECL|function|udelay
 r_void
 id|udelay
