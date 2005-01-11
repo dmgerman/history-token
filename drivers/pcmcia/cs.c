7ac807a64216b406de426c5ecc18aa1a0a218136
Dominik Brodowski <linux@dominikbrodowski.de>
2005-01-11 03:22:33 -0800

    [PATCH] pcmcia: use pcmcia_device in send_event
    Use a struct pcmcia_device-based approach to inform "clients" of events.  It
    needs to be done using bus_for_each_device() so that we don't need to take the
    device_list spinlock.
    
    Signed-off-by: Dominik Brodowski <linux@brodo.de>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/pcmcia/cs.c
+++ drivers/pcmcia/cs.c
@@ -1391,7 +1391,7 @@ suffix:semicolon
 )brace
 )brace
 multiline_comment|/* shutdown_socket */
-multiline_comment|/*======================================================================&n;&n;    The central event handler.  Send_event() sends an event to all&n;    valid clients.  Parse_events() interprets the event bits from&n;    a card status change report.  Do_shutdown() handles the high&n;    priority stuff associated with a card removal.&n;    &n;======================================================================*/
+multiline_comment|/*======================================================================&n;&n;    The central event handler.  Send_event() sends an event to the&n;    16-bit subsystem, which then calls the relevant device drivers.&n;    Parse_events() interprets the event bits from&n;    a card status change report.  Do_shutdown() handles the high&n;    priority stuff associated with a card removal.&n;    &n;======================================================================*/
 multiline_comment|/* NOTE: send_event needs to be called with skt-&gt;sem held. */
 DECL|function|send_event
 r_static
