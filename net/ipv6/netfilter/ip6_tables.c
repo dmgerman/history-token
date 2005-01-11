368324e8c7907bd4fd859ebfe3d9126e3834689a
Patrick McHardy <kaber@coreworks.de>
2005-01-11 05:42:12 +0100

    [NETFILTER]: Remove skb_linearize in ip6tables
    Signed-off-by: Patrick McHardy <kaber@trash.net>

--- net/ipv6/netfilter/ip6_tables.c
+++ net/ipv6/netfilter/ip6_tables.c
@@ -1266,31 +1266,6 @@ comma
 op_star
 id|back
 suffix:semicolon
-multiline_comment|/* FIXME: Push down to extensions --RR */
-r_if
-c_cond
-(paren
-id|skb_is_nonlinear
-c_func
-(paren
-op_star
-id|pskb
-)paren
-op_logical_and
-id|skb_linearize
-c_func
-(paren
-op_star
-id|pskb
-comma
-id|GFP_ATOMIC
-)paren
-op_ne
-l_int|0
-)paren
-r_return
-id|NF_DROP
-suffix:semicolon
 multiline_comment|/* Initialization */
 id|indev
 op_assign
--- net/ipv6/netfilter/ip6table_mangle.c
+++ net/ipv6/netfilter/ip6table_mangle.c
@@ -1112,31 +1112,6 @@ id|NF_ACCEPT
 suffix:semicolon
 )brace
 macro_line|#endif
-multiline_comment|/* FIXME: Push down to extensions --RR */
-r_if
-c_cond
-(paren
-id|skb_is_nonlinear
-c_func
-(paren
-op_star
-id|pskb
-)paren
-op_logical_and
-id|skb_linearize
-c_func
-(paren
-op_star
-id|pskb
-comma
-id|GFP_ATOMIC
-)paren
-op_ne
-l_int|0
-)paren
-r_return
-id|NF_DROP
-suffix:semicolon
 multiline_comment|/* save source/dest address, nfmark, hoplimit, flowlabel, priority,  */
 id|memcpy
 c_func
