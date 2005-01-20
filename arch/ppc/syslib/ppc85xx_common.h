147946ed59bc925b6a166866622163884b6e0de7
Kumar Gala <galak@freescale.com>
2005-01-20 15:53:12 -0800

    [PATCH] ppc32: Infrastructure changes to MPC85xx sub-arch from OCP to platform_device
    - Described all devices available on current MPC85xx CPUs (mem & irq)
    
    - Added cpu descriptions for the MPC8540, MPC8541, MPC8541, MPC8555,
      MPC8555E, and MPC8560.
    
    * Removed OCP usage from MPC85xx sub-arch
    
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/syslib/ppc85xx_common.h
+++ arch/ppc/syslib/ppc85xx_common.h
@@ -4,28 +4,12 @@ DECL|macro|__PPC_SYSLIB_PPC85XX_COMMON_H
 mdefine_line|#define __PPC_SYSLIB_PPC85XX_COMMON_H
 macro_line|#include &lt;linux/config.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
-macro_line|#include &lt;asm/ocp.h&gt;
 multiline_comment|/* Provide access to ccsrbar for any modules, etc */
 id|phys_addr_t
 id|get_ccsrbar
 c_func
 (paren
 r_void
-)paren
-suffix:semicolon
-multiline_comment|/* Update the 85xx OCP tables paddr field */
-r_void
-id|mpc85xx_update_paddr_ocp
-c_func
-(paren
-r_struct
-id|ocp_device
-op_star
-id|dev
-comma
-r_void
-op_star
-id|ccsrbar
 )paren
 suffix:semicolon
 macro_line|#endif /* __PPC_SYSLIB_PPC85XX_COMMON_H */
