ff21fa2540479b763f844b180bbf1df3c6311fa8
Jeff Dike <jdike@addtoit.com>
2005-01-20 16:01:54 -0800

    [PATCH] uml: provide some initcall definitions for userspace code
    Provide definitions of __initcall, __exitcall, and __init_call for userspace
    code.
    
    Signed-off-by: Jeff Dike <jdike@addtoit.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/um/include/init.h
+++ arch/um/include/init.h
@@ -126,6 +126,14 @@ DECL|macro|__uml_postsetup_call
 mdefine_line|#define __uml_postsetup_call&t;__attribute_used__ __attribute__ ((__section__ (&quot;.uml.postsetup.init&quot;)))
 DECL|macro|__uml_exit_call
 mdefine_line|#define __uml_exit_call&t;&t;__attribute_used__ __attribute__ ((__section__ (&quot;.uml.exitcall.exit&quot;)))
+macro_line|#ifndef __KERNEL__
+DECL|macro|__initcall
+mdefine_line|#define __initcall(fn) static initcall_t __initcall_##fn __init_call = fn
+DECL|macro|__exitcall
+mdefine_line|#define __exitcall(fn) static exitcall_t __exitcall_##fn __exit_call = fn
+DECL|macro|__init_call
+mdefine_line|#define __init_call __attribute__ ((unused,__section__ (&quot;.initcall.init&quot;)))
+macro_line|#endif
 macro_line|#endif /* _LINUX_UML_INIT_H */
 multiline_comment|/*&n; * Overrides for Emacs so that we follow Linus&squot;s tabbing style.&n; * Emacs will notice this stuff at the end of the file and automatically&n; * adjust the settings for this buffer only.  This must remain at the end&n; * of the file.&n; * ---------------------------------------------------------------------------&n; * Local variables:&n; * c-file-style: &quot;linux&quot;&n; * End:&n; */
 eof
