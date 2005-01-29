58b44049af92195e6e1957081576d17c5d9c8f81
Marcel Holtmann <marcel@holtmann.org>
2005-01-29 05:34:45 +0100

    [Bluetooth] Use raw mode for the CSR sniffer device
    The CSR sniffer device looks like a normal H:2 Bluetooth device,
    but it is not and so mark it as raw mode device.
    
    Signed-off-by: Marcel Holtmann <marcel@holtmann.org>

--- drivers/bluetooth/hci_usb.h
+++ drivers/bluetooth/hci_usb.h
@@ -12,8 +12,10 @@ DECL|macro|HCI_RESET
 mdefine_line|#define HCI_RESET&t;&t;0x02
 DECL|macro|HCI_DIGIANSWER
 mdefine_line|#define HCI_DIGIANSWER&t;&t;0x04
+DECL|macro|HCI_SNIFFER
+mdefine_line|#define HCI_SNIFFER&t;&t;0x08
 DECL|macro|HCI_BROKEN_ISOC
-mdefine_line|#define HCI_BROKEN_ISOC&t;&t;0x08
+mdefine_line|#define HCI_BROKEN_ISOC&t;&t;0x10
 DECL|macro|HCI_MAX_IFACE_NUM
 mdefine_line|#define HCI_MAX_IFACE_NUM&t;3
 DECL|macro|HCI_MAX_BULK_TX
