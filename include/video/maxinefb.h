7f2ef7d460bb0d97483ff0f768854190fddbe26d
Ralf Bächle <ralf@linux-mips.org>
2005-02-01 16:44:34 -0800

    [PATCH] mips: DECstation updates
    Update DECstation code.  This still isn't usable but updates the code from
    rotten to less rotten and most importantly eleminates the diff between
    kernel.org and linux-mips.org tree to easy future work.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/video/maxinefb.h
+++ include/video/maxinefb.h
@@ -1,10 +1,11 @@
 multiline_comment|/*&n; *      linux/drivers/video/maxinefb.h&n; *&n; *      DECstation 5000/xx onboard framebuffer support, Copyright (C) 1999 by&n; *      Michael Engel &lt;engel@unix-ag.org&gt; and Karsten Merker &lt;merker@guug.de&gt;&n; *      This file is subject to the terms and conditions of the GNU General&n; *      Public License.  See the file COPYING in the main directory of this&n; *      archive for more details.&n; */
+macro_line|#include &lt;asm/addrspace.h&gt;
 multiline_comment|/*&n; * IMS332 video controller register base address&n; */
 DECL|macro|MAXINEFB_IMS332_ADDRESS
-mdefine_line|#define MAXINEFB_IMS332_ADDRESS                  0xbc140000
+mdefine_line|#define MAXINEFB_IMS332_ADDRESS&t;&t;KSEG1ADDR(0x1c140000)
 multiline_comment|/*&n; * Begin of DECstation 5000/xx onboard framebuffer memory, default resolution&n; * is 1024x768x8&n; */
 DECL|macro|DS5000_xx_ONBOARD_FBMEM_START
-mdefine_line|#define DS5000_xx_ONBOARD_FBMEM_START&t;0xaa000000
+mdefine_line|#define DS5000_xx_ONBOARD_FBMEM_START&t;KSEG1ADDR(0x0a000000)
 multiline_comment|/*&n; *      The IMS 332 video controller used in the DECstation 5000/xx series&n; *      uses 32 bits wide registers; the following defines declare the&n; *      register numbers, to get the real offset, these have to be multiplied&n; *      by four.&n; */
 DECL|macro|IMS332_REG_CURSOR_RAM
 mdefine_line|#define IMS332_REG_CURSOR_RAM           0x200&t;/* hardware cursor bitmap */
