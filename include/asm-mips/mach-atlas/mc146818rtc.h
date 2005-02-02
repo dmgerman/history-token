e333891a7261c27b9af022001d77cec9b0c242f6
Ralf Bächle <ralf@linux-mips.org>
2005-02-01 16:45:19 -0800

    [PATCH] mips: mIPS Technologies board updates
    Update the code for the three MIPS Technologies evaluation platforms.
    Untangle the support for the three platforms, keep up with changes elsewhere
    in the kernel and getting Atlas back to work even with the most esotheric
    configurations.
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-mips/mach-atlas/mc146818rtc.h
+++ include/asm-mips/mach-atlas/mc146818rtc.h
@@ -1,25 +1,19 @@
-multiline_comment|/*&n; * Carsten Langgaard, carstenl@mips.com&n; * Copyright (C) 1999,2000 MIPS Technologies, Inc.  All rights reserved.&n; * Copyright (C) 2003 by Ralf Baechle&n; *&n; * This program is free software; you can distribute it and/or modify it&n; * under the terms of the GNU General Public License (Version 2) as&n; * published by the Free Software Foundation.&n; *&n; * This program is distributed in the hope it will be useful, but WITHOUT&n; * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or&n; * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License&n; * for more details.&n; *&n; * You should have received a copy of the GNU General Public License along&n; * with this program; if not, write to the Free Software Foundation, Inc.,&n; * 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.&n; */
+multiline_comment|/*&n; * Copyright (C) 1999, 2000, 2005  MIPS Technologies, Inc.&n; *&t;All rights reserved.&n; *&t;Authors: Carsten Langgaard &lt;carstenl@mips.com&gt;&n; *&t;&t; Maciej W. Rozycki &lt;macro@mips.com&gt;&n; * Copyright (C) 2003, 05 Ralf Baechle (ralf@linux-mips.org)&n; *&n; * This program is free software; you can distribute it and/or modify it&n; * under the terms of the GNU General Public License (Version 2) as&n; * published by the Free Software Foundation.&n; *&n; * This program is distributed in the hope it will be useful, but WITHOUT&n; * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or&n; * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License&n; * for more details.&n; *&n; * You should have received a copy of the GNU General Public License along&n; * with this program; if not, write to the Free Software Foundation, Inc.,&n; * 59 Temple Place - Suite 330, Boston MA 02111-1307, USA.&n; */
 macro_line|#ifndef __ASM_MACH_ATLAS_MC146818RTC_H
 DECL|macro|__ASM_MACH_ATLAS_MC146818RTC_H
 mdefine_line|#define __ASM_MACH_ATLAS_MC146818RTC_H
-macro_line|#include &lt;asm/io.h&gt;
+macro_line|#include &lt;linux/types.h&gt;
+macro_line|#include &lt;asm/addrspace.h&gt;
 macro_line|#include &lt;asm/mips-boards/atlas.h&gt;
 macro_line|#include &lt;asm/mips-boards/atlasint.h&gt;
 DECL|macro|RTC_PORT
-mdefine_line|#define RTC_PORT(x)&t;(ATLAS_RTC_ADR_REG + (x)*8)
+mdefine_line|#define RTC_PORT(x)&t;(ATLAS_RTC_ADR_REG + (x) * 8)
+DECL|macro|RTC_IO_EXTENT
+mdefine_line|#define RTC_IO_EXTENT&t;0x100
 DECL|macro|RTC_IOMAPPED
-mdefine_line|#define RTC_IOMAPPED&t;1
-DECL|macro|RTC_EXTENT
-mdefine_line|#define RTC_EXTENT&t;16
+mdefine_line|#define RTC_IOMAPPED&t;0
 DECL|macro|RTC_IRQ
 mdefine_line|#define RTC_IRQ&t;&t;ATLASINT_RTC
-macro_line|#ifdef CONFIG_CPU_LITTLE_ENDIAN
-DECL|macro|ATLAS_RTC_PORT
-mdefine_line|#define ATLAS_RTC_PORT(x) (RTC_PORT(x) + 0)
-macro_line|#else
-DECL|macro|ATLAS_RTC_PORT
-mdefine_line|#define ATLAS_RTC_PORT(x) (RTC_PORT(x) + 3)
-macro_line|#endif
 DECL|function|CMOS_READ
 r_static
 r_inline
@@ -33,28 +27,52 @@ r_int
 id|addr
 )paren
 (brace
-id|outb
+r_volatile
+id|u32
+op_star
+id|ireg
+op_assign
+(paren
+r_void
+op_star
+)paren
+id|CKSEG1ADDR
 c_func
 (paren
-id|addr
-comma
-id|ATLAS_RTC_PORT
+id|RTC_PORT
 c_func
 (paren
 l_int|0
 )paren
 )paren
 suffix:semicolon
-r_return
-id|inb
+r_volatile
+id|u32
+op_star
+id|dreg
+op_assign
+(paren
+r_void
+op_star
+)paren
+id|CKSEG1ADDR
 c_func
 (paren
-id|ATLAS_RTC_PORT
+id|RTC_PORT
 c_func
 (paren
 l_int|1
 )paren
 )paren
+suffix:semicolon
+op_star
+id|ireg
+op_assign
+id|addr
+suffix:semicolon
+r_return
+op_star
+id|dreg
 suffix:semicolon
 )brace
 DECL|function|CMOS_WRITE
@@ -73,29 +91,53 @@ r_int
 id|addr
 )paren
 (brace
-id|outb
+r_volatile
+id|u32
+op_star
+id|ireg
+op_assign
+(paren
+r_void
+op_star
+)paren
+id|CKSEG1ADDR
 c_func
 (paren
-id|addr
-comma
-id|ATLAS_RTC_PORT
+id|RTC_PORT
 c_func
 (paren
 l_int|0
 )paren
 )paren
 suffix:semicolon
-id|outb
+r_volatile
+id|u32
+op_star
+id|dreg
+op_assign
+(paren
+r_void
+op_star
+)paren
+id|CKSEG1ADDR
 c_func
 (paren
-id|data
-comma
-id|ATLAS_RTC_PORT
+id|RTC_PORT
 c_func
 (paren
 l_int|1
 )paren
 )paren
+suffix:semicolon
+op_star
+id|ireg
+op_assign
+id|addr
+suffix:semicolon
+op_star
+id|dreg
+op_assign
+id|data
 suffix:semicolon
 )brace
 DECL|macro|RTC_ALWAYS_BCD
