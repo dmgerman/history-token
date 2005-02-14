f227e10ddde06a2f05c184392322df9802a67ab3
Jaroslav Kysela <perex@suse.cz>
2005-02-14 18:24:30 +0100

    [ALSA] Add ac97_quirk for Dell machine
    Intel8x0 driver
    Added ac97_quirk option for Dell machine.
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- sound/pci/intel8x0.c
+++ sound/pci/intel8x0.c
@@ -8214,6 +8214,29 @@ comma
 dot
 id|vendor
 op_assign
+l_int|0x1028
+comma
+dot
+id|device
+op_assign
+l_int|0x0163
+comma
+dot
+id|name
+op_assign
+l_string|&quot;Dell Unknown&quot;
+comma
+multiline_comment|/* STAC9750/51 */
+dot
+id|type
+op_assign
+id|AC97_TUNE_HP_ONLY
+)brace
+comma
+(brace
+dot
+id|vendor
+op_assign
 l_int|0x103c
 comma
 dot
