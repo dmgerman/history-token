9e5e897b7f36fde14cc35a680a1ad10ef30b5feb
Ingo Molnar <mingo@elte.hu>
2005-03-07 17:53:09 -0800

    [PATCH] clean up and unify asm-*/resource.h files
    This patch does the final consolidation of asm-*/resource.h file, without
    changing any of the rlimit definitions on any architecture.  Primarily it
    removes the __ARCH_RLIMIT_ORDER method and replaces it with a more compact
    and isolated one that allows architectures to define only the offending
    rlimits.
    
    This method has the positive effect that adding a new rlimit can now be
    purely done via changing asm-generic/resource.h alone.  Previously one
    would have to patch 4 other (sparc, sparc64, alpha and mips) resource.h
    files.
    
    The patch also does style unification, whitespace cleanups and
    simplification of resource.h files and cleans up the asm-generic/resource.h
    file as well.  I've added more comments too.
    
    This patch should have no effect on any code on any architecture.  (i.e.
    it's a pure identity patch.)
    
    Tested on x86 and carefully reviewed to make sure that Sparc, Sparc64,
    MIPS and Alpha rlimits are still the same as required by the ABI.
    
    Signed-off-by: Ingo Molnar <mingo@elte.hu>
    Acked-by: Chris Wright <chrisw@osdl.org>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- include/asm-generic/resource.h
+++ include/asm-generic/resource.h
@@ -1,9 +1,7 @@
 macro_line|#ifndef _ASM_GENERIC_RESOURCE_H
 DECL|macro|_ASM_GENERIC_RESOURCE_H
 mdefine_line|#define _ASM_GENERIC_RESOURCE_H
-multiline_comment|/*&n; * Resource limits&n; */
-multiline_comment|/* Allow arch to control resource order */
-macro_line|#ifndef __ARCH_RLIMIT_ORDER
+multiline_comment|/*&n; * Resource limit IDs&n; *&n; * ( Compatibility detail: there are architectures that have&n; *   a different rlimit ID order in the 5-9 range and want&n; *   to keep that order for binary compatibility. The reasons&n; *   are historic and all new rlimits are identical across all&n; *   arches. If an arch has such special order for some rlimits&n; *   then it defines them prior including asm-generic/resource.h. )&n; */
 DECL|macro|RLIMIT_CPU
 mdefine_line|#define RLIMIT_CPU&t;&t;0&t;/* CPU time in ms */
 DECL|macro|RLIMIT_FSIZE
@@ -14,16 +12,26 @@ DECL|macro|RLIMIT_STACK
 mdefine_line|#define RLIMIT_STACK&t;&t;3&t;/* max stack size */
 DECL|macro|RLIMIT_CORE
 mdefine_line|#define RLIMIT_CORE&t;&t;4&t;/* max core file size */
+macro_line|#ifndef RLIMIT_RSS
 DECL|macro|RLIMIT_RSS
-mdefine_line|#define RLIMIT_RSS&t;&t;5&t;/* max resident set size */
+macro_line|# define RLIMIT_RSS&t;&t;5&t;/* max resident set size */
+macro_line|#endif
+macro_line|#ifndef RLIMIT_NPROC
 DECL|macro|RLIMIT_NPROC
-mdefine_line|#define RLIMIT_NPROC&t;&t;6&t;/* max number of processes */
+macro_line|# define RLIMIT_NPROC&t;&t;6&t;/* max number of processes */
+macro_line|#endif
+macro_line|#ifndef RLIMIT_NOFILE
 DECL|macro|RLIMIT_NOFILE
-mdefine_line|#define RLIMIT_NOFILE&t;&t;7&t;/* max number of open files */
+macro_line|# define RLIMIT_NOFILE&t;&t;7&t;/* max number of open files */
+macro_line|#endif
+macro_line|#ifndef RLIMIT_MEMLOCK
 DECL|macro|RLIMIT_MEMLOCK
-mdefine_line|#define RLIMIT_MEMLOCK&t;&t;8&t;/* max locked-in-memory address space */
+macro_line|# define RLIMIT_MEMLOCK&t;&t;8&t;/* max locked-in-memory address space */
+macro_line|#endif
+macro_line|#ifndef RLIMIT_AS
 DECL|macro|RLIMIT_AS
-mdefine_line|#define RLIMIT_AS&t;&t;9&t;/* address space limit */
+macro_line|# define RLIMIT_AS&t;&t;9&t;/* address space limit */
+macro_line|#endif
 DECL|macro|RLIMIT_LOCKS
 mdefine_line|#define RLIMIT_LOCKS&t;&t;10&t;/* maximum file locks held */
 DECL|macro|RLIMIT_SIGPENDING
@@ -32,19 +40,20 @@ DECL|macro|RLIMIT_MSGQUEUE
 mdefine_line|#define RLIMIT_MSGQUEUE&t;&t;12&t;/* maximum bytes in POSIX mqueues */
 DECL|macro|RLIM_NLIMITS
 mdefine_line|#define RLIM_NLIMITS&t;&t;13
-macro_line|#endif
-multiline_comment|/*&n; * SuS says limits have to be unsigned.&n; * Which makes a ton more sense anyway.&n; */
+multiline_comment|/*&n; * SuS says limits have to be unsigned.&n; * Which makes a ton more sense anyway.&n; *&n; * Some architectures override this (for compatibility reasons):&n; */
 macro_line|#ifndef RLIM_INFINITY
 DECL|macro|RLIM_INFINITY
-mdefine_line|#define RLIM_INFINITY&t;(~0UL)
+macro_line|# define RLIM_INFINITY&t;&t;(~0UL)
 macro_line|#endif
+multiline_comment|/*&n; * RLIMIT_STACK default maximum - some architectures override it:&n; */
 macro_line|#ifndef _STK_LIM_MAX
 DECL|macro|_STK_LIM_MAX
-mdefine_line|#define _STK_LIM_MAX&t;RLIM_INFINITY
+macro_line|# define _STK_LIM_MAX&t;&t;RLIM_INFINITY
 macro_line|#endif
 macro_line|#ifdef __KERNEL__
+multiline_comment|/*&n; * boot-time rlimit defaults for the init task:&n; */
 DECL|macro|INIT_RLIMITS
-mdefine_line|#define INIT_RLIMITS&t;&t;&t;&t;&t;&t;&t;&bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;[RLIMIT_CPU]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_FSIZE]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_DATA]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_STACK]&t;&t;= {      _STK_LIM, _STK_LIM_MAX  },&t;&bslash;&n;&t;[RLIMIT_CORE]&t;&t;= {             0, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_RSS]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_NPROC]&t;&t;= {             0,             0 },&t;&bslash;&n;&t;[RLIMIT_NOFILE]&t;&t;= {      INR_OPEN,     INR_OPEN  },&t;&bslash;&n;&t;[RLIMIT_MEMLOCK]&t;= {   MLOCK_LIMIT,   MLOCK_LIMIT },&t;&bslash;&n;&t;[RLIMIT_AS]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_LOCKS]&t;&t;= { RLIM_INFINITY, RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_SIGPENDING]&t;= { MAX_SIGPENDING, MAX_SIGPENDING },&t;&bslash;&n;&t;[RLIMIT_MSGQUEUE]&t;= { MQ_BYTES_MAX, MQ_BYTES_MAX },&t;&bslash;&n;}
+mdefine_line|#define INIT_RLIMITS&t;&t;&t;&t;&t;&t;&t;&bslash;&n;{&t;&t;&t;&t;&t;&t;&t;&t;&t;&bslash;&n;&t;[RLIMIT_CPU]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_FSIZE]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_DATA]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_STACK]&t;&t;= {       _STK_LIM,   _STK_LIM_MAX },&t;&bslash;&n;&t;[RLIMIT_CORE]&t;&t;= {              0,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_RSS]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_NPROC]&t;&t;= {              0,              0 },&t;&bslash;&n;&t;[RLIMIT_NOFILE]&t;&t;= {       INR_OPEN,       INR_OPEN },&t;&bslash;&n;&t;[RLIMIT_MEMLOCK]&t;= {    MLOCK_LIMIT,    MLOCK_LIMIT },&t;&bslash;&n;&t;[RLIMIT_AS]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_LOCKS]&t;&t;= {  RLIM_INFINITY,  RLIM_INFINITY },&t;&bslash;&n;&t;[RLIMIT_SIGPENDING]&t;= { MAX_SIGPENDING, MAX_SIGPENDING },&t;&bslash;&n;&t;[RLIMIT_MSGQUEUE]&t;= {   MQ_BYTES_MAX,   MQ_BYTES_MAX },&t;&bslash;&n;}
 macro_line|#endif&t;/* __KERNEL__ */
 macro_line|#endif
 eof
