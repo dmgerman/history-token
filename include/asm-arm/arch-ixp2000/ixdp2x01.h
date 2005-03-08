4bbe86b8917e3d3709379a9c1cffc9cda4ccb590
Lennert Buytenhek <buytenh@org.rmk.(none)>
2005-03-08 22:37:20 +0000

    [ARM PATCH] 2517/1: more ixp2000 typo fixes
    Patch from Lennert Buytenhek
    
    Another round of ixp2000 typo fixes.
    
    Signed-off-by: Lennert Buytenhek
    Signed-off-by: Russell King

--- include/asm-arm/arch-ixp2000/ixdp2x01.h
+++ include/asm-arm/arch-ixp2000/ixdp2x01.h
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * include/asm/arch/ixdp2x01.h&n; *&n; * Platform definitions for IXDP2X01 &amp;&amp; IXDP2801 systems&n; *&n; * Author: Deepak Saxena &lt;dsaxena@plexity.net&gt;&n; *&n; * Copyright 2004 (c) MontaVista Software, Inc. &n; *&n; * Based on original code Copyright (c) 2002-2003 Intel Corporation&n; * &n; * This file is licensed under  the terms of the GNU General Public &n; * License version 2. This program is licensed &quot;as is&quot; without any &n; * warranty of any kind, whether express or implied.&n; */
+multiline_comment|/*&n; * include/asm-arm/arch-ixp2000/ixdp2x01.h&n; *&n; * Platform definitions for IXDP2X01 &amp;&amp; IXDP2801 systems&n; *&n; * Author: Deepak Saxena &lt;dsaxena@plexity.net&gt;&n; *&n; * Copyright 2004 (c) MontaVista Software, Inc. &n; *&n; * Based on original code Copyright (c) 2002-2003 Intel Corporation&n; * &n; * This file is licensed under  the terms of the GNU General Public &n; * License version 2. This program is licensed &quot;as is&quot; without any &n; * warranty of any kind, whether express or implied.&n; */
 macro_line|#ifndef __IXDP2X01_H__
 DECL|macro|__IXDP2X01_H__
 mdefine_line|#define __IXDP2X01_H__
