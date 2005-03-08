89868f159ce4e77f9d8ddc6e2e4c81af46b3343a
Steve French <stevef@stevef95.austin.ibm.com>
2005-03-08 06:14:21 -0600

    [CIFS] Add support for setting timestaps by handle for NT4 servers
    Signed-off-by: Steve French (sfrench@us.ibm.com)

--- fs/cifs/cifsproto.h
+++ fs/cifs/cifsproto.h
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; *   fs/cifs/cifsproto.h&n; *&n; *   Copyright (c) International Business Machines  Corp., 2002,2004&n; *   Author(s): Steve French (sfrench@us.ibm.com)&n; *&n; *   This library is free software; you can redistribute it and/or modify&n; *   it under the terms of the GNU Lesser General Public License as published&n; *   by the Free Software Foundation; either version 2.1 of the License, or&n; *   (at your option) any later version.&n; *&n; *   This library is distributed in the hope that it will be useful,&n; *   but WITHOUT ANY WARRANTY; without even the implied warranty of&n; *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See&n; *   the GNU Lesser General Public License for more details.&n; *&n; *   You should have received a copy of the GNU Lesser General Public License&n; *   along with this library; if not, write to the Free Software&n; *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA &n; */
+multiline_comment|/*&n; *   fs/cifs/cifsproto.h&n; *&n; *   Copyright (c) International Business Machines  Corp., 2002,2005&n; *   Author(s): Steve French (sfrench@us.ibm.com)&n; *&n; *   This library is free software; you can redistribute it and/or modify&n; *   it under the terms of the GNU Lesser General Public License as published&n; *   by the Free Software Foundation; either version 2.1 of the License, or&n; *   (at your option) any later version.&n; *&n; *   This library is distributed in the hope that it will be useful,&n; *   but WITHOUT ANY WARRANTY; without even the implied warranty of&n; *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See&n; *   the GNU Lesser General Public License for more details.&n; *&n; *   You should have received a copy of the GNU Lesser General Public License&n; *   along with this library; if not, write to the Free Software&n; *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA &n; */
 macro_line|#ifndef _CIFSPROTO_H
 DECL|macro|_CIFSPROTO_H
 mdefine_line|#define _CIFSPROTO_H
@@ -810,6 +810,29 @@ r_struct
 id|nls_table
 op_star
 id|nls_codepage
+)paren
+suffix:semicolon
+r_extern
+r_int
+id|CIFSSMBSetFileTimes
+c_func
+(paren
+r_const
+r_int
+id|xid
+comma
+r_struct
+id|cifsTconInfo
+op_star
+id|tcon
+comma
+r_const
+id|FILE_BASIC_INFO
+op_star
+id|data
+comma
+id|__u16
+id|fid
 )paren
 suffix:semicolon
 r_extern
