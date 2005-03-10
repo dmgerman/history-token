67931904b16fe0a3d4c12f753cbfddd4ff578c02
Andrew Morton <akpm@osdl.org>
2005-03-09 17:09:04 -0800

    [PATCH] sis warning fix
    drivers/video/sis/sis_main.c: In function <BACKTICK>sisfb_compat_ioctl':
    drivers/video/sis/sis_main.c:2204: warning: implicit declaration of function <BACKTICK>lock_kernel'
    
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- drivers/video/sis/sis_main.c
+++ drivers/video/sis/sis_main.c
@@ -17,6 +17,7 @@ macro_line|#include &lt;linux/delay.h&gt;
 macro_line|#include &lt;linux/fb.h&gt;
 macro_line|#include &lt;linux/console.h&gt;
 macro_line|#include &lt;linux/selection.h&gt;
+macro_line|#include &lt;linux/smp_lock.h&gt;
 macro_line|#include &lt;linux/ioport.h&gt;
 macro_line|#include &lt;linux/init.h&gt;
 macro_line|#include &lt;linux/pci.h&gt;
