573eb9a4eadb53c0f68bd1b3ea5ec32e82ba16f7
Adrian Bunk <bunk@stusta.de>
2005-03-22 22:59:06 -0600

    [PATCH] drivers/scsi/osst.c: make code static
    This patch makes needlessly global code static.
    
    Signed-off-by: Adrian Bunk <bunk@stusta.de>
    Signed-off-by: Willem Riede <osst@riede.org>
    Signed-off-by: James Bottomley <James.Bottomley@SteelEye.com>

--- drivers/scsi/osst.c
+++ drivers/scsi/osst.c
@@ -9,6 +9,7 @@ op_assign
 l_string|&quot;$Id: osst.c,v 1.73 2005/01/01 21:13:34 wriede Exp $&quot;
 suffix:semicolon
 DECL|variable|osst_version
+r_static
 r_const
 r_char
 op_star
@@ -447,6 +448,7 @@ op_star
 )paren
 suffix:semicolon
 DECL|variable|osst_template
+r_static
 r_struct
 id|scsi_driver
 id|osst_template
