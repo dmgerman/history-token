b39e9e05efcb5ef682441b3d9e513f571dd137a3
Kumar Gala <galak@freescale.com>
2005-03-30 16:30:30 -0800

    [PATCH] ppc32: CPM2 PIC cleanup
    Cleaned up the CPM2 interrupt controller code:
    * Added the ability to offset the IRQs
    * Refactored common PIC init code out of platform files
    * Fixed IRQ offsets on MPC85xx so it can handle properly handled multiple
    interrupt controllers (i8259, CPM2 PIC, and OpenPIC)
    
    Signed-off-by: Jason McMullan <jason.mcmullan@timesys.com>
    Signed-off-by: Kumar Gala <kumar.gala@freescale.com>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

--- arch/ppc/syslib/cpm2_pic.h
+++ arch/ppc/syslib/cpm2_pic.h
@@ -2,11 +2,6 @@ macro_line|#ifndef _PPC_KERNEL_CPM2_H
 DECL|macro|_PPC_KERNEL_CPM2_H
 mdefine_line|#define _PPC_KERNEL_CPM2_H
 r_extern
-r_struct
-id|hw_interrupt_type
-id|cpm2_pic
-suffix:semicolon
-r_extern
 r_int
 id|cpm2_get_irq
 c_func
@@ -15,6 +10,14 @@ r_struct
 id|pt_regs
 op_star
 id|regs
+)paren
+suffix:semicolon
+r_extern
+r_void
+id|cpm2_init_IRQ
+c_func
+(paren
+r_void
 )paren
 suffix:semicolon
 macro_line|#endif /* _PPC_KERNEL_CPM2_H */
